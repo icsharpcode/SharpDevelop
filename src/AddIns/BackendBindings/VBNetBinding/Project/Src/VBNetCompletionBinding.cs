1d89d058003f19701cdd3b652cd60c072b18fc89
Markus Palme <MarkusPalme@gmx.de>
2005-07-22 21:03:51 +0000

    added vb.net keyword completition. todo: add completition for special statements, show insight
    git-svn-id: svn://svn.sharpdevelop.net/sharpdevelop/trunk@232 1ccf3a8d-04fe-1044-b7c0-cef0b8235c61

--- src/AddIns/BackendBindings/VBNetBinding/Project/Src/VBNetCompletionBinding.cs
+++ src/AddIns/BackendBindings/VBNetBinding/Project/Src/VBNetCompletionBinding.cs
@@ -6,7 +6,10 @@
 // </file>
 
 using System;
+
+using ICSharpCode.SharpDevelop.Dom;
 using ICSharpCode.SharpDevelop.DefaultEditor.Gui.Editor;
+using ICSharpCode.SharpDevelop.Dom.NRefactoryResolver;
 
 namespace VBNetBinding
 {
@@ -14,6 +17,29 @@ namespace VBNetBinding
 	{
 		public VBNetCompletionBinding() : base(".vb")
 		{
+			this.EnableXmlCommentCompletion = true;
+		}
+		
+		public override bool HandleKeyword(SharpDevelopTextAreaControl editor, string word)
+		{
+			// TODO: Assistance writing Methods/Fields/Properties/Events:
+			// use public/static/etc. as keywords to display a list with other modifiers
+			// and possible return types.
+			switch (word.ToLower()) {
+				case "imports":
+					// TODO: check if we are inside class/namespace
+					editor.ShowCompletionWindow(new CtrlSpaceCompletionDataProvider(ExpressionContext.Namespace), ' ');
+					return true;
+				case "as":
+					System.Windows.Forms.MessageBox.Show("as");
+					editor.ShowCompletionWindow(new CtrlSpaceCompletionDataProvider(ExpressionContext.Type), ' ');
+					return true;
+				case "new":
+					editor.ShowCompletionWindow(new CtrlSpaceCompletionDataProvider(ExpressionContext.ObjectCreation), ' ');
+					return true;
+				default:
+					return base.HandleKeyword(editor, word);
+			}
 		}
 	}
 }
