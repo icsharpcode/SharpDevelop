3a0a26c1b1c0b0f5710068d24ff9232e9dc2443c
David Srbecký <dsrbecky@gmail.com>
2005-07-23 13:43:14 +0000

    Updating my headers
    git-svn-id: svn://svn.sharpdevelop.net/sharpdevelop/trunk@236 1ccf3a8d-04fe-1044-b7c0-cef0b8235c61

--- src/AddIns/Misc/Debugger/Debugger.AddIn/Project/Src/Pads/CallStackPad.cs
+++ src/AddIns/Misc/Debugger/Debugger.AddIn/Project/Src/Pads/CallStackPad.cs
@@ -1,7 +1,7 @@
 ﻿// <file>
 //     <copyright see="prj:///doc/copyright.txt">2002-2005 AlphaSierraPapa</copyright>
 //     <license see="prj:///doc/license.txt">GNU General Public License</license>
-//     <owner name="Mike Krüger" email="mike@icsharpcode.net"/>
+//     <owner name="David Srbecký" email="dsrbecky@gmail.com"/>
 //     <version>$Revision$</version>
 // </file>
 
--- src/AddIns/Misc/Debugger/Debugger.AddIn/Project/Src/Pads/ViewMenuItemBuilder.cs
+++ src/AddIns/Misc/Debugger/Debugger.AddIn/Project/Src/Pads/ViewMenuItemBuilder.cs
@@ -1,7 +1,7 @@
-// <file>
+﻿// <file>
 //     <copyright see="prj:///doc/copyright.txt">2002-2005 AlphaSierraPapa</copyright>
 //     <license see="prj:///doc/license.txt">GNU General Public License</license>
-//     <owner name="Daniel Grunwald" email="daniel@danielgrunwald.de"/>
+//     <owner name="David Srbecký" email="dsrbecky@gmail.com"/>
 //     <version>$Revision$</version>
 // </file>
 
