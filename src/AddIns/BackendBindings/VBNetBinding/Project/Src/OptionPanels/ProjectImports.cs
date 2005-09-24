64c1f4f6a851776651a751a850edaa3a8b417cdf
Markus Palme <MarkusPalme@gmx.de>
2005-09-24 17:55:53 +0000

    added vb.net projectlevel-imports option panel
    git-svn-id: svn://svn.sharpdevelop.net/sharpdevelop/trunk@504 1ccf3a8d-04fe-1044-b7c0-cef0b8235c61

---
+++ src/AddIns/BackendBindings/VBNetBinding/Project/Src/OptionPanels/ProjectImports.cs
@@ -0,0 +1,97 @@
+// <file>
+//     <copyright see="prj:///doc/copyright.txt">2002-2005 AlphaSierraPapa</copyright>
+//     <license see="prj:///doc/license.txt">GNU General Public License</license>
+//     <owner name="Daniel Grunwald" email="daniel@danielgrunwald.de"/>
+//     <version>$Revision: 434 $</version>
+// </file>
+
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+using ICSharpCode.SharpDevelop.Project;
+using ICSharpCode.Core;
+using ICSharpCode.SharpDevelop;
+using ICSharpCode.SharpDevelop.Gui;
+using ICSharpCode.SharpDevelop.Gui.XmlForms;
+using ICSharpCode.SharpDevelop.Gui.OptionPanels;
+
+using VBNetBinding;
+
+namespace VBNetBinding.OptionPanels
+{
+	public class ProjectImports : AbstractProjectOptionPanel
+	{
+		public override void LoadPanelContents()
+		{
+			SetupFromXmlResource("ProjectImports.xfrm");
+			InitializeHelper();
+			
+			Get<Button>("addImport").Click += new EventHandler(addImportButton_Click);
+			Get<Button>("removeImport").Click += new EventHandler(removeImportButton_Click);
+			Get<ComboBox>("namespaces").TextChanged += new EventHandler(namespacesComboBox_TextCanged);
+			Get<ListBox>("imports").SelectedIndexChanged += new EventHandler(importsListBox_SelectedIndexChanged);
+			
+			Get<ComboBox>("namespaces").Items.Clear();
+			foreach(ProjectItem item in project.Items)
+			{
+				if(item.ItemType == ItemType.Import) {
+					Get<ListBox>("imports").Items.Add(item.Include);
+				}
+			}
+			
+			namespacesComboBox_TextCanged(null, EventArgs.Empty);
+			importsListBox_SelectedIndexChanged(null, EventArgs.Empty);
+		}
+		
+		private void namespacesComboBox_TextCanged(object sender, EventArgs e)
+		{
+			Get<Button>("addImport").Enabled = Get<ComboBox>("namespaces").Text != "" &&
+				! Get<ListBox>("imports").Items.Contains(Get<ComboBox>("namespaces").Text);
+		}
+		
+		private void importsListBox_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			Get<Button>("removeImport").Enabled = Get<ListBox>("imports").SelectedIndex != -1;
+		}
+		
+		private void removeImportButton_Click(object sender, EventArgs e)
+		{
+			Get<ListBox>("imports").Items.RemoveAt(Get<ListBox>("imports").SelectedIndex);
+			IsDirty = true;
+		}
+		
+		private void addImportButton_Click(object sender, EventArgs e)
+		{
+			Get<ListBox>("imports").Items.Add(Get<ComboBox>("namespaces").Text);
+			Get<ComboBox>("namespaces").Text = "";
+			IsDirty = true;
+		}
+		
+		public override bool StorePanelContents()
+		{
+			List<ProjectItem> imports = new List<ProjectItem>();
+			foreach(ProjectItem item in project.Items)
+			{
+				if(item.ItemType == ItemType.Import)
+				{
+					imports.Add(item);
+				}
+			}
+			
+			foreach(ImportProjectItem item in imports)
+			{
+				project.Items.Remove(item);
+			}
+			
+			foreach(string importedNamespace in Get<ListBox>("imports").Items)
+			{
+				ImportProjectItem importItem = new ImportProjectItem(project);
+				importItem.Include = importedNamespace;
+				project.Items.Add(importItem);
+				
+			}
+			return base.StorePanelContents();
+		}
+	}
+}
