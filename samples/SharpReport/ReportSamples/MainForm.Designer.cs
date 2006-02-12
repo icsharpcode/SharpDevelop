b6a1b8d84cb0b9b53ff045304a564fc960dbfb32
Peter Forstmeier <peter.forstmeier@t-online.de>
2006-02-12 11:00:21 +0000

    SharpReport Exampels
    git-svn-id: svn://svn.sharpdevelop.net/sharpdevelop/trunk@1126 1ccf3a8d-04fe-1044-b7c0-cef0b8235c61

---
+++ samples/SharpReport/ReportSamples/MainForm.Designer.cs
@@ -0,0 +1,148 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Forstmeier Helmut
+ * Date: 08.02.2006
+ * Time: 15:24
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace ReportSamples
+{
+	partial class MainForm : System.Windows.Forms.Form
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.formSheetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.simpleFormsSheetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.pullMpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.customersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.employeeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.missingConnectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.pushModelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.emlpoyeesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+									this.formSheetToolStripMenuItem,
+									this.pullMpToolStripMenuItem,
+									this.pushModelToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(518, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// formSheetToolStripMenuItem
+			// 
+			this.formSheetToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+									this.simpleFormsSheetToolStripMenuItem});
+			this.formSheetToolStripMenuItem.Name = "formSheetToolStripMenuItem";
+			this.formSheetToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+			this.formSheetToolStripMenuItem.Text = "FormSheet";
+			// 
+			// simpleFormsSheetToolStripMenuItem
+			// 
+			this.simpleFormsSheetToolStripMenuItem.Name = "simpleFormsSheetToolStripMenuItem";
+			this.simpleFormsSheetToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+			this.simpleFormsSheetToolStripMenuItem.Text = "SimpleFormsSheet";
+			this.simpleFormsSheetToolStripMenuItem.Click += new System.EventHandler(this.SimpleFormsSheetClick);
+			// 
+			// pullMpToolStripMenuItem
+			// 
+			this.pullMpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+									this.customersToolStripMenuItem,
+									this.employeeToolStripMenuItem,
+									this.missingConnectionToolStripMenuItem});
+			this.pullMpToolStripMenuItem.Name = "pullMpToolStripMenuItem";
+			this.pullMpToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+			this.pullMpToolStripMenuItem.Text = "PullModell";
+			// 
+			// customersToolStripMenuItem
+			// 
+			this.customersToolStripMenuItem.Name = "customersToolStripMenuItem";
+			this.customersToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
+			this.customersToolStripMenuItem.Text = "Customers";
+			this.customersToolStripMenuItem.Click += new System.EventHandler(this.CustomersClick);
+			// 
+			// employeeToolStripMenuItem
+			// 
+			this.employeeToolStripMenuItem.Name = "employeeToolStripMenuItem";
+			this.employeeToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
+			this.employeeToolStripMenuItem.Text = "Employee";
+			this.employeeToolStripMenuItem.Click += new System.EventHandler(this.EmployeeClick);
+			// 
+			// missingConnectionToolStripMenuItem
+			// 
+			this.missingConnectionToolStripMenuItem.Name = "missingConnectionToolStripMenuItem";
+			this.missingConnectionToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
+			this.missingConnectionToolStripMenuItem.Text = "MissingConnection";
+			this.missingConnectionToolStripMenuItem.Click += new System.EventHandler(this.MissingConnectionClick);
+			// 
+			// pushModelToolStripMenuItem
+			// 
+			this.pushModelToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+									this.emlpoyeesToolStripMenuItem});
+			this.pushModelToolStripMenuItem.Name = "pushModelToolStripMenuItem";
+			this.pushModelToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+			this.pushModelToolStripMenuItem.Text = "PushModel";
+			// 
+			// emlpoyeesToolStripMenuItem
+			// 
+			this.emlpoyeesToolStripMenuItem.Name = "emlpoyeesToolStripMenuItem";
+			this.emlpoyeesToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
+			this.emlpoyeesToolStripMenuItem.Text = "Employees-Push";
+			this.emlpoyeesToolStripMenuItem.Click += new System.EventHandler(this.EmployeesPushClick);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(518, 273);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "MainForm";
+			this.Text = "ReportSamples";
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.ToolStripMenuItem emlpoyeesToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem missingConnectionToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem employeeToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem customersToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem simpleFormsSheetToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem pushModelToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem pullMpToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem formSheetToolStripMenuItem;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+	}
+}
---
+++ samples/SharpReport/ReportSamples/MainForm.cs
@@ -0,0 +1,197 @@
+/*
+ * Created by SharpDevelop.
+ * User: Forstmeier Helmut
+ * Date: 08.02.2006
+ * Time: 15:24
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+using System;
+using System.Data;
+using System.Data.Common;
+using System.Drawing;
+using System.Windows.Forms;
+
+using SharpReportCore;
+
+namespace ReportSamples
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm
+	{
+		[STAThread]
+		public static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	
+		
+		private void MissingConnection () {
+			try
+			{
+				string connectionString="";
+				OpenFileDialog dg = new OpenFileDialog();
+				dg.Filter = "SharpReport files|*.srd";
+				dg.Title = "Select a report file: ";
+				if (dg.ShowDialog() == DialogResult.OK)
+				{
+					SharpReportCore.SharpReportEngine mn = new SharpReportCore.SharpReportEngine();
+					SharpReportCore.ReportParameters pn = new SharpReportCore.ReportParameters();
+
+//					sql = "Provider=SQLOLEDB.1;Password=xxx;Persist Security Info=True;User ID=xxx;Initial Catalog=Northwind;Data Source=WS161";
+					connectionString = this.ConnectionStringFromMsDataLink();
+		
+
+//					pn.ConnectionObject = new ConnectionObject(sql);
+					
+					pn.ConnectionObject = this.ConnectionFromConnectionBuilder(connectionString);
+
+//					pn.SqlParameters.Clear();
+//					pn.SqlParameters.Add(new SharpReportCore.SqlParameter("@OrderID",System.Data.DbType.Int32,10248));
+//					pn.SortColumnCollection.Add(new SortColumn("UnitPrice",System.ComponentModel.ListSortDirection.Descending));
+					mn.PreviewStandartReport(dg.FileName.ToString(),pn);
+//					mn.PrintStandartReport(dg.FileName.ToString(),pn);
+				}
+			}
+			catch(Exception er)
+			{
+				MessageBox.Show(er.ToString(),": MainForm");
+			}
+		}
+		
+		private void OpenPull()
+		{
+			try
+			{
+				OpenFileDialog dg = new OpenFileDialog();
+				dg.Filter = "SharpReport files|*.srd";
+				dg.Title = "Select a report file: ";
+				if (dg.ShowDialog() == DialogResult.OK){
+					SharpReportCore.SharpReportEngine mn = new SharpReportCore.SharpReportEngine();
+					mn.PreviewStandartReport(dg.FileName.ToString());
+//					mn.PrintStandartReport(dg.FileName.ToString());
+					
+				}
+			}
+			catch(Exception er)
+			{
+				MessageBox.Show(er.ToString(),"MainForm");
+			}
+		}
+		///<summary>Preferd Method to initialise the <see cref="SharpReportCore.ConnectionObject"></see>
+		/// hav a look to
+		/// <http://msdn2.microsoft.com/en-us/library/system.data.oledb.oledbconnectionstringbuilder(VS.80).aspx>
+		/// <example><code>
+		/// public void OleDbConnectionBuilderForSqlServer () {
+		///			OleDbConnectionStringBuilder builder = new OleDbConnectionStringBuilder();
+		///			builder["Provider"] = "SQLOLEDB.1";
+		///			builder["Integrated Security"] = "SSPI";
+		///			builder["Initial Catalog"] = "NorthWind";
+		///			System.Console.WriteLine("{0}",builder.ConnectionString);
+		///			ConnectionObject obj = new ConnectionObject(builder);
+		///			Assert.IsTrue(obj.Connection.State == ConnectionState.Closed,"Connection should be closed");
+		///			obj.Connection.Open();
+		///			Assert.IsTrue(obj.Connection.State == ConnectionState.Open,"Connection should be opend");
+		///}
+		/// </code></example>
+		/// </summary>
+		/// 
+	
+		private SharpReportCore.ConnectionObject ConnectionFromConnectionBuilder (string sql) {
+			System.Data.Common.DbConnectionStringBuilder builder = new System.Data.OleDb.OleDbConnectionStringBuilder(sql);
+			return new SharpReportCore.ConnectionObject (builder.ConnectionString);
+		}
+	
+		
+		private string ConnectionStringFromMsDataLink() {
+			ADODB._Connection AdoConnection;
+			MSDASC.DataLinks dataLink = new MSDASC.DataLinks();
+			
+			AdoConnection = null;
+			AdoConnection = (ADODB._Connection) dataLink.PromptNew();
+			return AdoConnection.ConnectionString;
+		}
+		
+		///<summary>This Report is send directly to the Printer, PrintDialog is
+		///  showing when UseStandartPrinter to 'false'</summary>
+		
+		private void OpenPushModell()
+		{
+			string reportFileName;
+			try
+			{
+				OpenFileDialog dg = new OpenFileDialog();
+				dg.Filter = "SharpReport files|*.srd";
+				dg.Title = "Select a report file: ";
+				if (dg.ShowDialog() == DialogResult.OK){
+					SharpReportCore.SharpReportEngine mn = new SharpReportCore.SharpReportEngine();
+					reportFileName = dg.FileName.ToString();
+					DataTable table = SelectData();
+					if (table != null) {
+//						mn.PreviewPushDataReport(reportFileName,table);
+						mn.PrintPushDataReport(reportFileName,table);
+					}
+				}
+			}
+			catch (Exception){
+				
+			}			
+		}
+		
+		private DataTable SelectData()
+		{
+			OpenFileDialog dg = new OpenFileDialog();
+			dg.Filter = "SharpReport files|*.xsd";
+			dg.Title = "Select a '.xsdfile: ";
+			if (dg.ShowDialog() == DialogResult.OK){
+				DataSet ds = new DataSet();
+				ds.ReadXml(dg.FileName);
+				return ds.Tables[0];
+			}
+			return null;
+		}
+		
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void SimpleFormsSheetClick(object sender, System.EventArgs e)
+		{
+			OpenPull();
+		}
+		
+		void CustomersClick(object sender, System.EventArgs e)
+		{
+			OpenPull();
+		}
+		
+		void EmployeeClick(object sender, System.EventArgs e)
+		{
+			OpenPull();
+		}
+		
+		void MissingConnectionClick(object sender, System.EventArgs e)
+		{
+			MissingConnection();
+		}
+		
+		void EmployeesPushClick(object sender, System.EventArgs e)
+		{
+			OpenPushModell();
+		}
+	}
+}
