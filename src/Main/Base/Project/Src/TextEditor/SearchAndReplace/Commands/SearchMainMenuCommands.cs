1c2d6baba182eecfc918f72262f53a0c1496bbda
Matt Ward <ward.matt@gmail.com>
2006-06-29 21:11:25 +0000

    Added 'Find Next Selected' search menu option which will find the next occurrence of the selected text in the document.
    git-svn-id: svn://svn.sharpdevelop.net/sharpdevelop/trunk@1530 1ccf3a8d-04fe-1044-b7c0-cef0b8235c61

--- src/Main/Base/Project/Src/TextEditor/SearchAndReplace/Commands/SearchMainMenuCommands.cs
+++ src/Main/Base/Project/Src/TextEditor/SearchAndReplace/Commands/SearchMainMenuCommands.cs
@@ -47,7 +47,7 @@ namespace SearchAndReplace
 			SearchAndReplaceDialog.ShowSingleInstance(SearchAndReplaceMode.Search);
 		}
 		
-		static bool IsMultipleLines(string text)
+		public static bool IsMultipleLines(string text)
 		{
 			return text.IndexOf('\n') != -1;
 		}
@@ -62,6 +62,56 @@ namespace SearchAndReplace
 			} else {
 				Find find = new Find();
 				find.Run();
+			}
+		}
+	}
+	
+	/// <summary>
+	/// Finds the next text match based on the text currently
+	/// selected. It uses the currently active search options and only changes
+	/// the current find pattern. If the currently active search is inside the 
+	/// current text selection then the quick find will change the search so it is
+	/// across the active document, otherwise it will not change the current setting.
+	/// </summary>
+	/// <remarks>
+	/// If there is a piece of text selected on a single line then the quick
+	/// find will search for that. If multiple lines of text are selected then
+	/// the word at the start of the selection is determined and searche for.
+	/// If no text is selected then the word next to the caret is used. If
+	/// no text is selected, but the caret is immediately surrounded by whitespace
+	/// then quick find does nothing.
+	/// </remarks>
+	public class FindNextSelected : AbstractMenuCommand
+	{
+		public override void Run()
+		{
+			TextEditorControl textArea = SearchReplaceUtilities.GetActiveTextEditor();
+			if (textArea == null) {
+				return;
+			}
+			
+			// Determine what text we should search for.
+			string textToFind;
+			
+			string selectedText = textArea.ActiveTextAreaControl.TextArea.SelectionManager.SelectedText;
+			if (selectedText.Length > 0) {
+				if (Find.IsMultipleLines(selectedText)) {
+					// Locate the nearest word at the selection start.
+					textToFind = TextUtilities.GetWordAt(textArea.Document, textArea.ActiveTextAreaControl.TextArea.SelectionManager.SelectionCollection[0].Offset);
+				} else {
+					// Search for selected text.
+					textToFind = selectedText;		
+				}
+			} else {
+				textToFind = TextUtilities.GetWordAt(textArea.Document, textArea.ActiveTextAreaControl.Caret.Offset);
+			}
+			
+			if (textToFind != null && textToFind.Length > 0) {
+				SearchOptions.CurrentFindPattern = textToFind;
+				if (SearchOptions.DocumentIteratorType == DocumentIteratorType.CurrentSelection) {
+					SearchOptions.DocumentIteratorType = DocumentIteratorType.CurrentDocument;
+				}
+				SearchReplaceManager.FindNext();
 			}
 		}
 	}
