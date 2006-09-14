2c1b544aa76fd4f817c0f5a5bf048ba5f6b2b906
Dickon Field <dickon.field@stalkingcat.co.uk>
2006-09-14 22:46:22 +0000

    added SharpDbTools back as a subproject of SharpServerTools
    git-svn-id: svn://svn.sharpdevelop.net/sharpdevelop/trunk@1798 1ccf3a8d-04fe-1044-b7c0-cef0b8235c61

---
+++ src/AddIns/Misc/SharpServerTools/SharpDbTools/Src/Data/DbModelInfo.cs
@@ -0,0 +1,181 @@
+// <file>
+//     <copyright see="prj:///doc/copyright.txt"/>
+//     <license see="prj:///doc/license.txt"/>
+//     <owner name="Dickon Field" email=""/>
+//     <version>$Revision: 1784 $</version>
+// </file>
+
+/*
+ * User: Dickon Field
+ * Date: 10/07/2006
+ * Time: 09:12
+ * 
+ */
+
+using System;
+using System.Data;
+using System.Data.Common;
+using System.Collections.Generic;
+
+using ICSharpCode.Core;
+
+namespace SharpDbTools.Data
+{
+	/// <summary>
+	/// DbModel is a DataSet containing the metadata tables returned from a DbConnection.
+	/// It adds methods designed specifically to facilitate access to the data in the
+	/// DataTables contained by the DbModel.
+	/// 
+	/// The DbModel class is intended to be usable in a fully disconnected mode - that is,
+	/// it requires a DbConnection to populate it, but it may then be locally persisted and subsequently
+	/// retrieved from its persisted data. This is intended to allow work to progress against the
+	/// DbModel without requiring a connection to its RDB server.
+	/// </summary>
+	/// 
+	public class DbModelInfo: DataSet
+	{
+		public const string METADATACOLLECTIONS = "MetaDataCollections";
+		
+		public DbModelInfo() : base()
+		{
+		}
+		
+		public DbModelInfo(string name) : base()
+		{
+			DataTable table = CreateConnectionInfoTable();
+			// add the first and only column of this table;
+			table.Rows.Add(new object[] {name, null, null});		
+		}
+		
+		public DbModelInfo(string name, string invariantName, string connectionString): base()
+		{
+			DataTable table = CreateConnectionInfoTable();
+			// add the first and only column of this table;
+			table.Rows.Add(new object[] {name, invariantName, connectionString});
+		}
+		
+		public string Name {
+			get {
+				DataTable table = this.Tables[TableNames.ConnectionInfo];
+				string name = table.Rows[0][ColumnNames.Name] as string;
+				return name;
+			}
+		}
+		
+		public string InvariantName {
+			get {
+				DataTable table = this.Tables[TableNames.ConnectionInfo];
+				string invariantName = null;
+				try {
+					invariantName = table.Rows[0][ColumnNames.InvariantName] as string;	
+				}
+				catch(ArgumentException) {
+					// see comment below - it is correct to bury this exception
+					//LoggingService.Info("InvariantName property was accessed while undefined" + e);
+				}
+				return invariantName;
+			}
+			set {
+				DataTable table = this.Tables[TableNames.ConnectionInfo];
+				string invariantName = table.Rows[0][ColumnNames.InvariantName] as string;
+				string name = this.Name;
+				string connectionString = this.ConnectionString;
+				
+				if (invariantName == null) {
+					table.Rows[0][ColumnNames.InvariantName] = value;	
+				}
+				
+				// if invariant has changed must clear any existing metadata
+				else if (!(invariantName.Equals(value))) {
+					// clear tables
+					this.Clear();
+					DataTable newTable = CreateConnectionInfoTable();
+					// add the first and only column of this table;
+					newTable.Rows.Add(new object[] {name, invariantName, connectionString});
+				}				
+			}
+		}
+		
+		public string ConnectionString {
+			get {
+				DataTable table = this.Tables[TableNames.ConnectionInfo];
+				string connectionString = null;
+				try {
+					connectionString = table.Rows[0][ColumnNames.ConnectionString] as string;	
+				}
+				catch(ArgumentException) {
+					// this simply indicates that this attribute was not defined when the
+					// DbModelInfo was saved, returning null makes sense here - so it is
+					// correct to bury this exception
+					//LoggingService.Info("InvariantName property was accessed while undefined" + e);
+				}
+				return connectionString;
+			}
+			set {
+				DataTable table = this.Tables[TableNames.ConnectionInfo];
+				string connectionString = this.ConnectionString;
+				if (connectionString == null) {
+					table.Rows[0][ColumnNames.ConnectionString] = value;	
+				}
+				else if (!(connectionString.Equals(value))) {
+					string invariantName = this.InvariantName;
+					string name = this.Name;
+					this.Clear();
+					// add the first and only column of this table;
+					table.Rows.Add(new object[] {name, invariantName, value});
+				}
+			}
+		}
+		
+		public void SetConnectionInfo(string invariantName, string connectionString)
+		{
+			string name = this.Name;
+			SetConnectionInfo(name, invariantName, connectionString);
+		}
+		
+		public void SetConnectionInfo(string name, string invariantName, 
+		                              string connectionString)
+		{
+			this.Clear();
+			DataTable table = CreateConnectionInfoTable();
+			// add the first and only column of this table;
+			table.Rows.Add(new object[] {name, invariantName, connectionString});			
+
+		}
+		
+		private DataTable CreateConnectionInfoTable()
+		{
+			// create a table in the DbModelInfo to hold this initial info.
+			// this creates a consistent representation of the data and makes
+			// it easier to serialise it
+			DataTable table = this.Tables.Add(TableNames.ConnectionInfo);
+			table.Columns.Add(ColumnNames.Name, typeof(string));
+			table.Columns.Add(ColumnNames.InvariantName, typeof(string));
+			table.Columns.Add(ColumnNames.ConnectionString, typeof(string));
+			return table;
+		}
+		
+		public void ClearMetaData()
+		{
+			DataTable metadataCollectionsTable = this.MetaDataCollections;
+			if (metadataCollectionsTable != null) {
+				foreach (DataRow collectionRow in metadataCollectionsTable.Rows) {
+					String collectionName = (string)collectionRow[0];
+					this.Tables.Remove(collectionName);
+				}
+			}
+		}
+		
+		public DataTable MetaDataCollections {
+			get {
+				return this.Tables[METADATACOLLECTIONS];
+			}
+		}
+		
+		public DataTable this[string collectionName] {
+			get {
+				return this.Tables[collectionName];
+			}
+		}
+	}
+}
---
+++ src/AddIns/Misc/SharpServerTools/SharpDbTools/Src/Data/DbModelInfoService.cs
@@ -0,0 +1,283 @@
+// <file>
+//     <copyright see="prj:///doc/copyright.txt"/>
+//     <license see="prj:///doc/license.txt"/>
+//     <owner name="Dickon Field" email=""/>
+//     <version>$Revision: 1784 $</version>
+// </file>
+
+/*
+ * User: dickon
+ * Date: 28/07/2006
+ * Time: 21:55
+ * 
+ */
+
+using System;
+using System.Data;
+using System.Data.Common;
+using System.Collections.Generic;
+using System.IO;
+using ICSharpCode.Core;
+
+namespace SharpDbTools.Data
+{
+	/// <summary>
+	/// Manages a collection of DbModelInfo:
+	/// - retrieval from files
+	/// - opening (essentially refreshing) from a database connection
+	/// - adding for new connection data (name, invariant name, connection string)
+	/// - saving to files
+	/// </summary>
+	public static class DbModelInfoService
+	{
+		static string saveLocation = null;
+		static object lockObject = new Object();
+		const string dbFilesDir = "DbTools";	
+		static SortedList<string, DbModelInfo> cache = new SortedList<string, DbModelInfo>();
+		
+		public static IList<string> Names {
+			get {
+				return cache.Keys;
+			}
+		}
+		
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="name">The user readable name of the provider</param>
+		/// <param name="invariantName">the identifying name of the provider</param>
+		/// <param name="connectionString">the connection string for this connection</param>
+		/// <returns></returns>
+		public static DbModelInfo Add(string name, string invariantName, string connectionString)
+		{
+			// TODO: add validation on name; invariant name
+			// assume that connection string is valid - if it fails an exception will be thrown later
+			// this allows partially defined connection strings at least to be saved and worked on later
+			DbModelInfo dbModel = new DbModelInfo(name, invariantName, connectionString);
+			
+			// add to cache
+			cache.Add(name, dbModel);
+			return dbModel;
+		}
+		
+		public static void Remove(string name)
+		{
+			cache.Remove(name);
+		}
+		
+		public static DbModelInfo GetDbModelInfo(string name) {
+				DbModelInfo modelInfo = null;
+				bool exists = cache.TryGetValue(name, out modelInfo);
+				return modelInfo;
+		}
+		
+		public static DataTable GetTableInfo(string modelName, string tableName)
+		{
+			LoggingService.Debug("-->GetTableInfo");
+			DbModelInfo modelInfo = GetDbModelInfo(modelName);
+			DataTable columnTable = modelInfo.Tables[TableNames.Columns];
+			DataRow[] columnsMetadata = columnTable.Select(ColumnNames.TableName + "='" + tableName + "'");
+			LoggingService.Debug("found: " + columnsMetadata.Length + " columns belonging to table: " + tableName);
+			DataTable tableInfo = new DataTable();
+			DataColumnCollection cols = columnTable.Columns;
+			foreach (DataColumn c in cols) {
+				DataColumn nc = new DataColumn(c.ColumnName, c.DataType);	
+				tableInfo.Columns.Add(nc);
+			}
+			foreach (DataRow r in columnsMetadata) {
+				DataRow newRow = tableInfo.NewRow();
+				newRow.ItemArray = r.ItemArray;
+				tableInfo.Rows.Add(newRow);
+				tableInfo.AcceptChanges();
+			}
+			return tableInfo;
+
+		}
+				
+		public static DbModelInfo LoadMetadataFromConnection(string name)
+		{
+			// get the DbModelInfo
+			
+			DbModelInfo modelInfo = null;
+			bool exists = cache.TryGetValue(name, out modelInfo);
+			if (!exists)
+			{
+				// TODO: more detail...
+				throw new KeyNotFoundException();
+			}
+			
+			// get the invariant name and connection string
+			
+			string invariantName = modelInfo.InvariantName;
+			string connectionString = modelInfo.ConnectionString;				
+			
+			// get a connection - wait until a connection has been successfully made
+			// before clearing the DbModelInfo
+			
+			DbProvidersService factoryService = DbProvidersService.GetDbProvidersService();
+			DbConnection connection = null;
+			try {
+				
+				DbProviderFactory factory = factoryService.GetFactoryByInvariantName(invariantName);
+				connection = factory.CreateConnection();
+				
+				
+				modelInfo.ClearMetaData();
+				
+				// reload the metadata from the connection
+				// get the Schema table
+				connection.ConnectionString = connectionString;
+	
+				connection.Open();
+				DataTable schemaInfo = connection.GetSchema();
+				
+				// clear the DbModelInfo prior to refreshing from the connection
+				modelInfo.ClearMetaData();
+	
+				// iterate through the rows in it - the first column of each is a
+				// schema info collection name that can be retrieved as a DbTable
+				// Add each one to the DbModel DataSet
+				
+				foreach (DataRow collectionRow in schemaInfo.Rows) {
+					String collectionName = (string)collectionRow[0];
+					DataTable nextMetaData = connection.GetSchema(collectionName);
+					modelInfo.Merge(nextMetaData);
+				}
+				return modelInfo;
+			}
+			catch(Exception e) {
+				LoggingService.Fatal("Exception caught while trying to retrieve database metadata: " + e);
+				throw e;
+			}
+			finally {
+				connection.Close();
+			}
+		}
+		
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="name">the logical name of the DbModelInfo to save to a file</param>
+		/// <param name="overwriteExistingFile">if true, any existing file will be overwritten</param>
+		/// <returns>true if the DbModelInfo was saved, false if not. It will not be saved if
+		/// either overwriteExistingFile is set to true, and there is an existing file</returns>
+		public static bool SaveToFile(string name, bool overwriteExistingFile)
+		{
+			string path = GetSaveLocation();
+			DbModelInfo modelInfo = null;
+			cache.TryGetValue(name, out modelInfo);
+			if (modelInfo != null) {
+				string modelName = modelInfo.Name;
+				// write to a file in 'path' called <name>.metadata
+				// TODO: may want to consider ways of making this more resilient
+				string filePath = path + @"\" + name + ".metadata";
+				LoggingService.Debug("writing metadata to: " + filePath);
+				if (File.Exists(filePath)) {
+				    	if (overwriteExistingFile) {
+				    		File.Delete(filePath);	
+				    	} else {
+				    		return false;
+				    	}
+				}
+				using (StreamWriter sw = File.CreateText(filePath)) {
+					string xml = modelInfo.GetXml();
+					sw.Write(xml);
+					sw.Flush();
+					sw.Close();
+					return true;
+				}
+			} else {
+				throw new DbModelInfoDoesNotExistException(name);
+			}
+		}
+		
+		public static void SaveAll()
+		{
+			foreach (string name in cache.Keys) {
+				SaveToFile(name, true);
+			}
+		}
+		
+		public static void LoadNamesFromFiles()
+		{
+			// load DbModelInfo's from file system
+			string saveLocation = GetSaveLocation();
+			LoggingService.Debug("looking for metadata files at: " + saveLocation);
+			string[] files = Directory.GetFileSystemEntries(saveLocation);
+
+			cache.Clear();
+			for (int i = 0; i < files.Length; i++) {
+				LoggingService.Debug("found to load metadata from: " + files[i]);
+				int start = files[i].LastIndexOf('\\');
+				int end = files[i].LastIndexOf('.');
+				start++;
+				string name = files[i].Substring(start, end - start);
+				DbModelInfo nextModel = new DbModelInfo(name);
+				cache.Add(nextModel.Name, nextModel);
+			}	
+		}
+		
+		public static void LoadFromFiles()
+		{
+			// load DbModelInfo's from file system
+			string saveLocation = GetSaveLocation();
+			string[] files = Directory.GetFiles(saveLocation);
+			cache.Clear();
+			for (int i = 0; i < files.Length; i++) {
+				DbModelInfo nextModel = LoadFromFileAtPath(@files[i]);
+				cache.Add(nextModel.Name, nextModel);
+			}
+		}
+		
+		private static DbModelInfo LoadFromFileAtPath(string filePath)
+		{
+			LoggingService.Debug("loading DbModelInfo from filePath: " + filePath);
+			DbModelInfo nextModel = new DbModelInfo();
+			nextModel.ReadXml(filePath);
+			return nextModel;
+		}
+		
+		public static void LoadFromFile(string logicalConnectionName)
+		{
+			LoggingService.Debug("loading DbModelInfo for name: " + logicalConnectionName);
+			string saveLocation = GetSaveLocation();
+			string path = saveLocation + "\\" + logicalConnectionName + ".metadata";
+			DbModelInfo info = LoadFromFileAtPath(path);
+			cache.Remove(logicalConnectionName);
+			cache.Add(logicalConnectionName, info);
+		}
+		
+		private static string GetSaveLocation()
+		{
+			// append the path of the directory for saving Db files
+			
+			if (saveLocation == null) {
+				lock(lockObject) {
+					string configDir = PropertyService.ConfigDirectory;
+					saveLocation = configDir + @"\" + dbFilesDir;
+					saveLocation = saveLocation.Replace("/", @"\");
+				}
+			}
+			if (!Directory.Exists(saveLocation)) {
+				Directory.CreateDirectory(@saveLocation);
+			}
+			return saveLocation;			
+		}
+	}
+	
+	public class DbModelInfoDoesNotExistException: ApplicationException
+	{
+		string name;
+		
+		public DbModelInfoDoesNotExistException(string dbModelInfoName): base()
+		{
+			this.name = dbModelInfoName;	
+		}
+		
+		public string Name {
+			get {
+				return name;
+			}
+		}
+	}
+}
