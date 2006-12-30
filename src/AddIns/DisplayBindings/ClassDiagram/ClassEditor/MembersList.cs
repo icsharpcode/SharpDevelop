9aa3b0b5e0c27b24348e3914783c9cf27ab3e1b0
Itar Bar-Haim <itaibh@gmail.com>
2006-12-30 20:51:54 +0000

    Match to the newest version. Mainly includes features implementation.
    git-svn-id: svn://svn.sharpdevelop.net/sharpdevelop/trunk@2233 1ccf3a8d-04fe-1044-b7c0-cef0b8235c61

--- src/AddIns/DisplayBindings/ClassDiagram/ClassEditor/MembersList.cs
+++ src/AddIns/DisplayBindings/ClassDiagram/ClassEditor/MembersList.cs
@@ -1,9 +1,11 @@
-// <file>
-//     <copyright see="prj:///doc/copyright.txt"/>
-//     <license see="prj:///doc/license.txt"/>
-//     <owner name="Itai Bar-Haim" email=""/>
-//     <version>$Revision$</version>
-// </file>
+/*
+ * Created by SharpDevelop.
+ * User: itai
+ * Date: 20/10/2006
+ * Time: 20:41
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
 
 using System;
 using System.Collections.Generic;
