70d1a6694f82ecca42dcda642a14fd8bf40dd8cf
Itar Bar-Haim <itaibh@gmail.com>
2006-12-30 20:52:13 +0000

    Match to the newest version. Mainly includes refactoring and bug fixes.
    git-svn-id: svn://svn.sharpdevelop.net/sharpdevelop/trunk@2234 1ccf3a8d-04fe-1044-b7c0-cef0b8235c61

--- src/AddIns/DisplayBindings/ClassDiagram/DiagramRouter/Direction.cs
+++ src/AddIns/DisplayBindings/ClassDiagram/DiagramRouter/Direction.cs
@@ -1,10 +1,11 @@
-// <file>
-//     <copyright see="prj:///doc/copyright.txt"/>
-//     <license see="prj:///doc/license.txt"/>
-//     <owner name="Itai Bar-Haim" email=""/>
-//     <version>$Revision$</version>
-// </file>
-
+/*
+ * Created by SharpDevelop.
+ * User: itai
+ * Date: 03/11/2006
+ * Time: 19:42
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
 using System;
 using System.Collections.Generic;
 using System.Drawing;
