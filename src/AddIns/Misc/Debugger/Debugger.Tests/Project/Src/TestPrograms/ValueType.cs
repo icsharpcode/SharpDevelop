a4b61226e70e7b54c5a5fa5f6173e0db3c329d5e
David Srbecký <dsrbecky@gmail.com>
2008-01-14 11:39:37 +0000

    Added a test for value type (struct)
    git-svn-id: svn://svn.sharpdevelop.net/sharpdevelop/trunk@2833 1ccf3a8d-04fe-1044-b7c0-cef0b8235c61

---
+++ src/AddIns/Misc/Debugger/Debugger.Tests/Project/Src/TestPrograms/ValueType.cs
@@ -0,0 +1,42 @@
+// <file>
+//     <copyright see="prj:///doc/copyright.txt"/>
+//     <license see="prj:///doc/license.txt"/>
+//     <owner name="David Srbecký" email="dsrbecky@gmail.com"/>
+//     <version>$Revision$</version>
+// </file>
+
+using System;
+
+namespace Debugger.Tests
+{
+	public struct ValueType
+	{
+		public static void Main()
+		{
+			new ValueType().Fun();
+		}
+		
+		public void Fun()
+		{
+			System.Diagnostics.Debugger.Break();
+		}
+	}
+
+#if TESTS
+	public partial class DebuggerTests
+	{
+		[NUnit.Framework.Test]
+		public void ValueType()
+		{
+			StartTest("ValueType");
+			WaitForPause();
+			
+			ObjectDump("this", process.SelectedStackFrame.ThisValue);
+			ObjectDump("typeof(this)", process.SelectedStackFrame.ThisValue.Type);
+			process.Continue();
+			process.WaitForExit();
+			CheckXmlOutput();
+		}
+	}
+#endif
+}
