38a00c9455aa6c49476a6fa446512e32a935f62c
Dickon Field <dickon.field@stalkingcat.co.uk>
2008-02-09 15:31:36 +0000

    git-svn-id: svn://svn.sharpdevelop.net/sharpdevelop/trunk@2944 1ccf3a8d-04fe-1044-b7c0-cef0b8235c61

---
+++ src/AddIns/Misc/ServerTools/Properties/DbConnectionsNode.cs
@@ -0,0 +1,24 @@
+/*
+ * Created by SharpDevelop.
+ * User: dickon
+ * Date: 22/01/2008
+ * Time: 17:39
+ * 
+ * 
+ */
+
+using System;
+using System.Windows.Controls;
+
+namespace ICSharpCode.ServerTools.Properties
+{
+	/// <summary>
+	/// Description of DbConnectionsNode.
+	/// </summary>
+	public class DbConnectionsNode: TreeViewItem
+	{
+		public DbConnectionsNode()
+		{
+		}
+	}
+}
