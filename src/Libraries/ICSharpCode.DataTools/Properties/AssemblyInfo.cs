ce31a3b6dc0c057a285f8fac65158cb6261e770c
Dickon Field <dickon.field@stalkingcat.co.uk>
2008-02-21 22:14:38 +0000

    Removed COM visibility attributes since they are not required, and may cause issues if you build as an user other than Administrator
    git-svn-id: svn://svn.sharpdevelop.net/sharpdevelop/trunk@2979 1ccf3a8d-04fe-1044-b7c0-cef0b8235c61

--- src/Libraries/ICSharpCode.DataTools/Properties/AssemblyInfo.cs
+++ src/Libraries/ICSharpCode.DataTools/Properties/AssemblyInfo.cs
@@ -14,14 +14,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7849b68b-43bd-4b57-90bf-c2c3aeaa4bb6")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
