1bd7eca986bbd4439e6ef55a70672d8434836e3c
Justin Dearing <zippy1981@gmail.com>
2008-06-19 02:01:40 +0000

    Documented ProcessExitedException. Added a new constructor, as well as a ProcessName property. I cannot populate these yet.
    git-svn-id: svn://svn.sharpdevelop.net/sharpdevelop/trunk@3109 1ccf3a8d-04fe-1044-b7c0-cef0b8235c61

--- src/AddIns/Misc/Debugger/Debugger.Core/Project/Src/Debugger/DebuggerException.cs
+++ src/AddIns/Misc/Debugger/Debugger.Core/Project/Src/Debugger/DebuggerException.cs
@@ -9,6 +9,9 @@ using System;
 
 namespace Debugger
 {
+	/// <summary>
+	/// Type of exception thrown by the Debugger.
+	/// </summary>
 	public class DebuggerException: System.Exception
 	{
 		public DebuggerException() {}
@@ -16,8 +19,32 @@ namespace Debugger
 		public DebuggerException(string message, System.Exception inner): base(message, inner) {}
 	}
 	
+	/// <summary>
+	/// An exception that is thrown when the debugged process unexpectedly exits.
+	/// </summary>
 	public class ProcessExitedException: DebuggerException
 	{
+		string processName = null;
+		
+		/// <summary>
+		/// The name of the process that has exited.
+		/// </summary>
+		public string ProcessName {
+			get { return processName; }
+		}
+		
+		/// <summary>
+		/// Creates a ProcessExitedException for an unnamed process.
+		/// </summary>
 		public ProcessExitedException(): base("Process exited") {}
+		
+		/// <summary>
+		/// Creates a ProcessExitedException for a process.
+		/// </summary>
+		/// <param name="processName">The name of the process</param>
+		public ProcessExitedException(string processName): base(string.Format("Process '{0}' exited.", processName)) {
+			this.processName = processName;
+		}
+		
 	}
 }
