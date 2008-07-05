47221d89d5a255600e10789e4d8786306a94c1eb
David Srbecký <dsrbecky@gmail.com>
2008-07-05 22:04:08 +0000

    Implemented Run to cursor command
    git-svn-id: svn://svn.sharpdevelop.net/sharpdevelop/trunk@3177 1ccf3a8d-04fe-1044-b7c0-cef0b8235c61

---
+++ src/AddIns/Misc/Debugger/Debugger.AddIn/Project/Src/Service/RunToCursorCommand.cs
@@ -0,0 +1,33 @@
+﻿// <file>
+//     <copyright see="prj:///doc/copyright.txt"/>
+//     <license see="prj:///doc/license.txt"/>
+//     <owner name="David Srbecký" email="dsrbecky@gmail.com"/>
+//     <version>$Revision$</version>
+// </file>
+
+using System;
+using ICSharpCode.Core;
+using ICSharpCode.SharpDevelop.DefaultEditor.Gui.Editor;
+using ICSharpCode.SharpDevelop.Debugging;
+
+using Debugger;
+
+namespace ICSharpCode.SharpDevelop.Services
+{
+	public class RunToCursorCommand : AbstractMenuCommand
+	{
+		public override void Run()
+		{
+			SharpDevelopTextAreaControl textEditor = this.Owner as SharpDevelopTextAreaControl;
+			WindowsDebugger winDebugger = DebuggerService.CurrentDebugger as WindowsDebugger;
+			if (textEditor == null || winDebugger == null) return;
+			
+			Breakpoint breakpoint = winDebugger.DebuggerCore.AddBreakpoint(textEditor.FileName, null, textEditor.ActiveTextAreaControl.Caret.Line + 1, textEditor.ActiveTextAreaControl.Caret.Column, true);
+			breakpoint.Hit += delegate { breakpoint.Remove(); };
+			winDebugger.DebuggedProcess.Paused += delegate { breakpoint.Remove(); };
+			if (!winDebugger.IsProcessRunning) {
+				winDebugger.Continue();
+			}
+		}
+	}
+}
