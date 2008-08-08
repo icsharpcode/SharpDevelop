40e82bc57c9fdc3adc9da793007c47448474a84b
Justin Dearing <zippy1981@gmail.com>
2008-08-08 12:11:11 +0000

    Added a DeleteAllMarks option to the breakpoint pad.
    git-svn-id: svn://svn.sharpdevelop.net/sharpdevelop/trunk@3315 1ccf3a8d-04fe-1044-b7c0-cef0b8235c61

--- src/Main/Base/Project/Src/TextEditor/Bookmarks/Pad/BookmarkPadToolbarCommands.cs
+++ src/Main/Base/Project/Src/TextEditor/Bookmarks/Pad/BookmarkPadToolbarCommands.cs
@@ -6,6 +6,7 @@
 // </file>
 
 using System;
+using System.Collections.Generic;
 using ICSharpCode.Core;
 using ICSharpCode.SharpDevelop.DefaultEditor.Commands;
 using ICSharpCode.SharpDevelop.Gui;
@@ -15,6 +16,7 @@ using System.Windows.Forms;
 
 namespace ICSharpCode.SharpDevelop.Bookmarks
 {
+	#region Goto Commands
 	public class GotoNext : AbstractEditActionMenuCommand
 	{
 		public override IEditAction EditAction {
@@ -33,11 +35,50 @@ namespace ICSharpCode.SharpDevelop.Bookmarks
 		}
 	}
 	
+	#endregion Goto Commands	
+	
+	#region Delete BookMark(s) commands
+	
+	public abstract class AbstractDeleteMarkClass : AbstractMenuCommand
+	{	
+		protected void deleteBookMark (BookmarkNode node) {
+			if (node.Bookmark.Document != null) {
+				node.Bookmark.Document.BookmarkManager.RemoveMark(node.Bookmark);
+			} else {
+				ICSharpCode.SharpDevelop.Bookmarks.BookmarkManager.RemoveMark(node.Bookmark);
+			}
+		}
+	}
+	
+	/// <summary>
+	/// Deletes all <see cref="BookmarkNode" />s in the BookMarkPad.
+	/// </summary>
+	public class DeleteAllMarks : AbstractDeleteMarkClass
+	{
+		public override void Run()
+		{
+			IEnumerable<TreeNode> nodes = ((BookmarkPadBase)Owner).AllNodes;
+			foreach(TreeNode innerNode in nodes) {
+				BookmarkFolderNode folderNode =  innerNode as BookmarkFolderNode;
+				// Its problebly not the most effecient way of doing it, but it works.
+				if (folderNode != null) {
+					for (int i = folderNode.Nodes.Count - 1; i >= 0 ; i--)
+					{
+						if (folderNode.Nodes[i] is BookmarkNode) {
+							deleteBookMark(folderNode.Nodes[i] as BookmarkNode);
+						}
+					}
+				}
+			}
+			WorkbenchSingleton.MainForm.Refresh();			
+		}
+	}
+	
 	
 	/// <summary>
 	/// Deletes the currently selected <see cref="BookmarkNode" /> or <see cref="BookmarkFolderNode" />
 	/// </summary>
-	public class DeleteMark : AbstractMenuCommand
+	public class DeleteMark : AbstractDeleteMarkClass
 	{
 		public override void Run()
 		{
@@ -58,15 +99,9 @@ namespace ICSharpCode.SharpDevelop.Bookmarks
 				WorkbenchSingleton.MainForm.Refresh();
 			}
 		}
-		
-		void deleteBookMark (BookmarkNode node) {
-			if (node.Bookmark.Document != null) {
-				node.Bookmark.Document.BookmarkManager.RemoveMark(node.Bookmark);
-			} else {
-				ICSharpCode.SharpDevelop.Bookmarks.BookmarkManager.RemoveMark(node.Bookmark);
-			}
-		}
 	}
+	
+	#endregion Delete BookMark(s) commands
 	
 	public class EnableDisableAll : AbstractMenuCommand
 	{
