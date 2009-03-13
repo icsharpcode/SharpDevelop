f5145966843ac87f1da07da827e4b3ec78494b1d
Mike Krüger <mike@icsharpcode.net>
2009-03-13 09:25:04 +0000

    * Src/PrettyPrinter/CSharp/PrettyPrintOptions.cs: * Src/PrettyPrinter/CSharp/CSharpOutputVisitor.cs: Added some output   options.
    git-svn-id: svn://svn.sharpdevelop.net/sharpdevelop/branches/3.0@3848 1ccf3a8d-04fe-1044-b7c0-cef0b8235c61

--- src/Libraries/NRefactory/Project/Src/PrettyPrinter/CSharp/CSharpOutputVisitor.cs
+++ src/Libraries/NRefactory/Project/Src/PrettyPrinter/CSharp/CSharpOutputVisitor.cs
@@ -551,7 +551,11 @@ namespace ICSharpCode.NRefactory.PrettyPrinter
 			outputFormatter.Indent();
 			OutputModifier(propertyGetRegion.Modifier);
 			outputFormatter.PrintText("get");
-			OutputBlockAllowInline(propertyGetRegion.Block, prettyPrintOptions.PropertyGetBraceStyle);
+			if (prettyPrintOptions.AllowPropertyGetBlockInline) {
+				OutputBlockAllowInline(propertyGetRegion.Block, prettyPrintOptions.PropertyGetBraceStyle);
+			} else {
+				OutputBlock(propertyGetRegion.Block, prettyPrintOptions.PropertyGetBraceStyle);
+			}
 			return null;
 		}
 		
@@ -561,7 +565,11 @@ namespace ICSharpCode.NRefactory.PrettyPrinter
 			outputFormatter.Indent();
 			OutputModifier(propertySetRegion.Modifier);
 			outputFormatter.PrintText("set");
-			OutputBlockAllowInline(propertySetRegion.Block, prettyPrintOptions.PropertySetBraceStyle);
+			if (prettyPrintOptions.AllowPropertySetBlockInline) {
+				OutputBlockAllowInline(propertySetRegion.Block, prettyPrintOptions.PropertySetBraceStyle);
+			} else {
+				OutputBlock(propertySetRegion.Block, prettyPrintOptions.PropertySetBraceStyle);
+			}
 			return null;
 		}
 		
@@ -606,7 +614,11 @@ namespace ICSharpCode.NRefactory.PrettyPrinter
 			VisitAttributes(eventAddRegion.Attributes, data);
 			outputFormatter.Indent();
 			outputFormatter.PrintText("add");
-			OutputBlockAllowInline(eventAddRegion.Block, prettyPrintOptions.EventAddBraceStyle);
+			if (prettyPrintOptions.AllowEventAddBlockInline) {
+				OutputBlockAllowInline(eventAddRegion.Block, prettyPrintOptions.EventAddBraceStyle);
+			} else {
+				OutputBlock(eventAddRegion.Block, prettyPrintOptions.EventAddBraceStyle);
+			}
 			return null;
 		}
 		
@@ -615,7 +627,11 @@ namespace ICSharpCode.NRefactory.PrettyPrinter
 			VisitAttributes(eventRemoveRegion.Attributes, data);
 			outputFormatter.Indent();
 			outputFormatter.PrintText("remove");
-			OutputBlockAllowInline(eventRemoveRegion.Block, prettyPrintOptions.EventRemoveBraceStyle);
+			if (prettyPrintOptions.AllowEventRemoveBlockInline) {
+				OutputBlockAllowInline(eventRemoveRegion.Block, prettyPrintOptions.EventRemoveBraceStyle);
+			} else {
+				OutputBlock(eventRemoveRegion.Block, prettyPrintOptions.EventRemoveBraceStyle);
+			}
 			return null;
 		}
 		
