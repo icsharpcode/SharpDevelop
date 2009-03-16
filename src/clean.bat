951e8971a6abb0e562659afa8c76a36b60ef3873
Daniel Grunwald <daniel@danielgrunwald.de>
2009-03-16 18:00:09 +0000

    Build profiler when using SharpDevelop .bat files.
    git-svn-id: svn://svn.sharpdevelop.net/sharpdevelop/branches/3.0@3859 1ccf3a8d-04fe-1044-b7c0-cef0b8235c61

--- src/clean.bat
+++ src/clean.bat
@@ -1,2 +1,4 @@
 %windir%\microsoft.net\framework\v3.5\msbuild /t:clean SharpDevelop.sln "/p:BooBinPath=%CD%\AddIns\BackendBindings\Boo\RequiredLibraries" "/p:FSharpBuildTasksPath=%CD%\AddIns\BackendBindings\FSharp\RequiredLibraries"
 @IF %ERRORLEVEL% NEQ 0 PAUSE
+%windir%\microsoft.net\framework\v3.5\msbuild /t:clean AddIns\Misc\Profiler\AutomatedBuild.proj
+@IF %ERRORLEVEL% NEQ 0 PAUSE
--- src/debugbuild.bat
+++ src/debugbuild.bat
@@ -1,2 +1,8 @@
-%windir%\microsoft.net\framework\v3.5\msbuild SharpDevelop.sln "/p:BooBinPath=%CD%\AddIns\BackendBindings\Boo\RequiredLibraries" "/p:FSharpBuildTasksPath=%CD%\AddIns\BackendBindings\FSharp\RequiredLibraries"
-@IF %ERRORLEVEL% NEQ 0 PAUSE
+%windir%\microsoft.net\framework\v3.5\msbuild SharpDevelop.sln /p:Configuration=Debug "/p:BooBinPath=%CD%\AddIns\BackendBindings\Boo\RequiredLibraries" "/p:FSharpBuildTasksPath=%CD%\AddIns\BackendBindings\FSharp\RequiredLibraries"
+@IF %ERRORLEVEL% NEQ 0 GOTO err
+%windir%\microsoft.net\framework\v3.5\msbuild AddIns\Misc\Profiler\AutomatedBuild.proj /p:Configuration=Debug
+@IF %ERRORLEVEL% NEQ 0 GOTO err
+@exit /B 0
+:err
+@PAUSE
+@exit /B 1
