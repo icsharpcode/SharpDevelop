61e63ad4d2a227eb3892707682500d4b979e16a0
Daniel Grunwald <daniel@danielgrunwald.de>
2009-04-25 21:16:20 +0000

    CopyToClipboard: don't truncate string to 256 characters
    git-svn-id: svn://svn.sharpdevelop.net/sharpdevelop/branches/3.0@4023 1ccf3a8d-04fe-1044-b7c0-cef0b8235c61

--- src/AddIns/Misc/Debugger/Debugger.Core/Project/Src/Wrappers/Util.cs
+++ src/AddIns/Misc/Debugger/Debugger.Core/Project/Src/Wrappers/Util.cs
@@ -23,36 +23,44 @@ namespace Debugger.Wrappers
 		
 		public static string GetString(UnmanagedStringGetter getter, uint defaultLength, bool trim)
 		{
-			const uint MAX_LENGTH = 1024 * 1024;
+			// 8M characters ought to be enough for everyone...
+			// (we need some limit to avoid OutOfMemoryExceptions when trying to load extremely large
+			// strings - see SD2-1470).
+			const uint MAX_LENGTH = 8 * 1024 * 1024;
 			string managedString;
 			IntPtr unmanagedString;
 			uint exactLength;
-			
+
+			if (defaultLength > MAX_LENGTH)
+				defaultLength = MAX_LENGTH;
 			// First attempt
 			unmanagedString = Marshal.AllocHGlobal((int)defaultLength * 2 + 2); // + 2 for terminating zero
-			getter(defaultLength, out exactLength, defaultLength > 0 ? unmanagedString : IntPtr.Zero);
-			
-			exactLength = (exactLength > MAX_LENGTH) ? MAX_LENGTH : exactLength;
-			
-			if(exactLength > defaultLength) {
-				// Second attempt
+			try {
+				getter(defaultLength, out exactLength, defaultLength > 0 ? unmanagedString : IntPtr.Zero);
+				
+				exactLength = (exactLength > MAX_LENGTH) ? MAX_LENGTH : exactLength;
+				
+				if(exactLength > defaultLength) {
+					// Second attempt
+					Marshal.FreeHGlobal(unmanagedString);
+					// TODO: Consider removing "+ 2" for the zero
+					unmanagedString = Marshal.AllocHGlobal((int)exactLength * 2 + 2); // + 2 for terminating zero
+					uint unused;
+					getter(exactLength, out unused, unmanagedString);
+				}
+				
+				// TODO:  Check how the trimming and the last 0 charater works
+				
+				// Return managed string and free unmanaged memory
+				managedString = Marshal.PtrToStringUni(unmanagedString, (int)exactLength);
+				//Console.WriteLine("Marshaled string from COM: \"" + managedString + "\" lenght=" + managedString.Length + " arrayLenght=" + exactLenght);
+				// The API might or might not include terminating null at the end
+				if (trim) {
+					managedString = managedString.TrimEnd('\0');
+				}
+			} finally {
 				Marshal.FreeHGlobal(unmanagedString);
-				// TODO: Consider removing "+ 2" for the zero
-				unmanagedString = Marshal.AllocHGlobal((int)exactLength * 2 + 2); // + 2 for terminating zero
-				uint unused;
-				getter(exactLength, out unused, unmanagedString);
 			}
-			
-			// TODO:  Check how the trimming and the last 0 charater works
-			
-			// Return managed string and free unmanaged memory
-			managedString = Marshal.PtrToStringUni(unmanagedString, (int)exactLength);
-			//Console.WriteLine("Marshaled string from COM: \"" + managedString + "\" lenght=" + managedString.Length + " arrayLenght=" + exactLenght);
-			// The API might or might not include terminating null at the end
-			if (trim) {
-				managedString = managedString.TrimEnd('\0');
-			}
-			Marshal.FreeHGlobal(unmanagedString);
 			return managedString;
 		}
 	}
