b4622d6a7ffd71edd3b13342f60edfa6ceef7d3a
Ivan Shumilin <ivanshumilin@gmail.com>
2009-05-05 14:32:51 +0000

    Override Platform variable to avoid MSB4126 error on some machines. (The specified solution configuration "Debug|MCD" is invalid).
    git-svn-id: svn://svn.sharpdevelop.net/sharpdevelop/trunk@4050 1ccf3a8d-04fe-1044-b7c0-cef0b8235c61

--- src/clean.bat
+++ src/clean.bat
@@ -1,4 +1,4 @@
-%windir%\microsoft.net\framework\v3.5\msbuild /t:clean SharpDevelop.sln "/p:BooBinPath=%CD%\AddIns\BackendBindings\Boo\RequiredLibraries" "/p:FSharpBuildTasksPath=%CD%\AddIns\BackendBindings\FSharp\RequiredLibraries"
+%windir%\microsoft.net\framework\v3.5\msbuild /t:clean SharpDevelop.sln "/p:BooBinPath=%CD%\AddIns\BackendBindings\Boo\RequiredLibraries" "/p:FSharpBuildTasksPath=%CD%\AddIns\BackendBindings\FSharp\RequiredLibraries" "/p:Platform=Any CPU"
 @IF %ERRORLEVEL% NEQ 0 PAUSE
 %windir%\microsoft.net\framework\v3.5\msbuild /t:clean AddIns\Misc\Profiler\AutomatedBuild.proj
 @IF %ERRORLEVEL% NEQ 0 PAUSE
--- src/debugbuild.bat
+++ src/debugbuild.bat
@@ -1,4 +1,4 @@
-%windir%\microsoft.net\framework\v3.5\msbuild SharpDevelop.sln /p:Configuration=Debug "/p:BooBinPath=%CD%\AddIns\BackendBindings\Boo\RequiredLibraries" "/p:FSharpBuildTasksPath=%CD%\AddIns\BackendBindings\FSharp\RequiredLibraries"
+%windir%\microsoft.net\framework\v3.5\msbuild SharpDevelop.sln /p:Configuration=Debug "/p:BooBinPath=%CD%\AddIns\BackendBindings\Boo\RequiredLibraries" "/p:FSharpBuildTasksPath=%CD%\AddIns\BackendBindings\FSharp\RequiredLibraries" "/p:Platform=Any CPU"
 @IF %ERRORLEVEL% NEQ 0 GOTO err
 %windir%\microsoft.net\framework\v3.5\msbuild AddIns\Misc\Profiler\AutomatedBuild.proj /p:Configuration=Debug
 @IF %ERRORLEVEL% NEQ 0 GOTO err
