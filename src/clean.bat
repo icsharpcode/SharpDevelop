f5b8a4a2dfc630395dac046544d69baf7c9b2b47
Daniel Grunwald <daniel@danielgrunwald.de>
2009-05-21 23:45:16 +0000

    Set TargetFrameworkVersion to 4.0
    git-svn-id: svn://svn.sharpdevelop.net/sharpdevelop/branches/dotnet4@4098 1ccf3a8d-04fe-1044-b7c0-cef0b8235c61

--- src/clean.bat
+++ src/clean.bat
@@ -1,4 +1,4 @@
-%windir%\microsoft.net\framework\v3.5\msbuild /t:clean SharpDevelop.sln "/p:BooBinPath=%CD%\AddIns\BackendBindings\Boo\RequiredLibraries" "/p:FSharpBuildTasksPath=%CD%\AddIns\BackendBindings\FSharp\RequiredLibraries" "/p:Platform=Any CPU"
+%windir%\microsoft.net\framework\v4.0.20506\msbuild /m /t:clean SharpDevelop.sln "/p:BooBinPath=%CD%\AddIns\BackendBindings\Boo\RequiredLibraries" "/p:FSharpBuildTasksPath=%CD%\AddIns\BackendBindings\FSharp\RequiredLibraries" "/p:Platform=Any CPU"
 @IF %ERRORLEVEL% NEQ 0 PAUSE
-%windir%\microsoft.net\framework\v3.5\msbuild /t:clean AddIns\Misc\Profiler\AutomatedBuild.proj
+%windir%\microsoft.net\framework\v4.0.20506\msbuild /m /t:clean AddIns\Misc\Profiler\AutomatedBuild.proj
 @IF %ERRORLEVEL% NEQ 0 PAUSE
