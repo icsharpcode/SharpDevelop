4db960a353375c284f7d00337861392b157f62d3
Daniel Grunwald <daniel@danielgrunwald.de>
2009-05-22 14:34:12 +0000

    Create new projects using MSBuild 4.0
    git-svn-id: svn://svn.sharpdevelop.net/sharpdevelop/branches/dotnet4@4102 1ccf3a8d-04fe-1044-b7c0-cef0b8235c61

--- src/Main/Base/Project/Src/Project/MSBuildBasedProject.cs
+++ src/Main/Base/Project/Src/Project/MSBuildBasedProject.cs
@@ -163,7 +163,7 @@ namespace ICSharpCode.SharpDevelop.Project
 			FileName = information.OutputProjectFileName;
 			
 			project.FullFileName = information.OutputProjectFileName;
-			project.DefaultToolsVersion = "3.5";
+			project.DefaultToolsVersion = "4.0";
 			
 			base.IdGuid = "{" + Guid.NewGuid().ToString().ToUpperInvariant() + "}";
 			MSBuild.BuildPropertyGroup group = project.AddNewPropertyGroup(false);
