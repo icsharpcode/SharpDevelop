5eee3babfca6ca92a1864c5cb6c768a52d1e6682
Daniel Grunwald <daniel@danielgrunwald.de>
2009-06-02 20:58:26 +0000

    Implemented syntax highlighting in search results pad.
    git-svn-id: svn://svn.sharpdevelop.net/sharpdevelop/trunk@4207 1ccf3a8d-04fe-1044-b7c0-cef0b8235c61

--- src/AddIns/Misc/SearchAndReplace/Project/Gui/SearchResultNode.cs
+++ src/AddIns/Misc/SearchAndReplace/Project/Gui/SearchResultNode.cs
@@ -5,14 +5,16 @@
 //     <version>$Revision$</version>
 // </file>
 
-using ICSharpCode.SharpDevelop;
 using System;
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Media;
+using ICSharpCode.AvalonEdit.Document;
+using ICSharpCode.AvalonEdit.Highlighting;
 using ICSharpCode.Core;
+using ICSharpCode.SharpDevelop;
 using ICSharpCode.SharpDevelop.Editor;
 using ICSharpCode.SharpDevelop.Editor.Search;
 
@@ -23,19 +25,50 @@ namespace SearchAndReplace
 	/// </summary>
 	sealed class SearchResultNode : SearchNode
 	{
+		static readonly FontFamily resultLineFamily = new FontFamily("Consolas, Courier New");
+		
 		SearchResultMatch result;
 		int lineNumber, column;
-		string resultText;
+		HighlightedInlineBuilder inlineBuilder;
 		
 		public SearchResultNode(SearchResultMatch result)
 		{
 			this.result = result;
 			
 			IDocument document = result.CreateDocument();
-			IDocumentLine matchedLine = document.GetLineForOffset(result.Offset);
-			lineNumber = matchedLine.LineNumber;
-			column = result.Offset - matchedLine.Offset + 1;
-			resultText = matchedLine.Text;
+			var startPosition = result.GetStartPosition(document);
+			this.lineNumber = startPosition.Line;
+			this.column = startPosition.Column;
+			
+			if (lineNumber >= 1 && lineNumber <= document.TotalNumberOfLines) {
+				IDocumentLine matchedLine = document.GetLine(lineNumber);
+				inlineBuilder = new HighlightedInlineBuilder(matchedLine.Text);
+				inlineBuilder.SetFontFamily(0, inlineBuilder.Text.Length, resultLineFamily);
+				
+				DocumentHighlighter highlighter = document.GetService(typeof(DocumentHighlighter)) as DocumentHighlighter;
+				TextDocument textDocument = document.GetService(typeof(TextDocument)) as TextDocument;
+				if (highlighter != null && textDocument != null) {
+					HighlightedLine highlightedLine = highlighter.HighlightLine(textDocument.GetLineByNumber(lineNumber));
+					int startOffset = highlightedLine.DocumentLine.Offset;
+					// copy only the foreground color
+					foreach (HighlightedSection section in highlightedLine.Sections) {
+						if (section.Color.Foreground != null) {
+							inlineBuilder.SetForeground(section.Offset - startOffset, section.Length, section.Color.Foreground.GetBrush(null));
+						}
+					}
+				}
+				
+				// now highlight the match in bold
+				if (column >= 1) {
+					var endPosition = result.GetEndPosition(document);
+					if (endPosition.Line == startPosition.Line && endPosition.Column > startPosition.Column) {
+						// subtract one from the column to get the offset inside the line's text
+						int startOffset = column - 1;
+						int endOffset = Math.Min(inlineBuilder.Text.Length, endPosition.Column - 1);
+						inlineBuilder.SetFontWeight(startOffset, endOffset - startOffset, FontWeights.Bold);
+					}
+				}
+			}
 		}
 		
 		bool showFileName = true;
@@ -52,21 +85,19 @@ namespace SearchAndReplace
 			}
 		}
 		
-		static readonly FontFamily resultLineFamily = new FontFamily("Consolas, Courier New");
-		
 		protected override object CreateText()
 		{
-			string displayText = result.DisplayText;
-			if (displayText != null)
-				return displayText;
+			LoggingService.Debug("Creating text for search result (" + lineNumber + ", " + column + ") ");
 			
 			TextBlock textBlock = new TextBlock();
 			textBlock.Inlines.Add("(" + lineNumber + ", " + column + ")\t");
 			
-			Span resultLineSpan = new Span();
-			resultLineSpan.FontFamily = resultLineFamily;
-			resultLineSpan.Inlines.Add(resultText);
-			textBlock.Inlines.Add(resultLineSpan);
+			string displayText = result.DisplayText;
+			if (displayText != null) {
+				textBlock.Inlines.Add(displayText);
+			} else if (inlineBuilder != null) {
+				textBlock.Inlines.AddRange(inlineBuilder.CreateInlines());
+			}
 			
 			if (showFileName) {
 				textBlock.Inlines.Add(
