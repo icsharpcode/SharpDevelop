3e95b10b6907928a72b4d81c26927810dd9dd833
Siegfried Pammer <siegfriedpammer@gmail.com>
2009-09-02 14:18:06 +0000

    implemented Find References for the profiler tree view
    git-svn-id: svn://svn.sharpdevelop.net/sharpdevelop/branches/3.0@4864 1ccf3a8d-04fe-1044-b7c0-cef0b8235c61

---
+++ src/AddIns/Misc/Profiler/Frontend/AddIn/Src/Commands/FindReferences.cs
@@ -0,0 +1,40 @@
+﻿// <file>
+//     <copyright see="prj:///doc/copyright.txt"/>
+//     <license see="prj:///doc/license.txt"/>
+//     <owner name="Siegfried Pammer" email="siegfriedpammer@gmail.com"/>
+//     <version>$Revision$</version>
+// </file>
+using System;
+using System.Linq;
+using ICSharpCode.Core;
+using ICSharpCode.SharpDevelop.Dom;
+using ICSharpCode.SharpDevelop.Gui;
+using ICSharpCode.SharpDevelop.Refactoring;
+
+namespace ICSharpCode.Profiler.AddIn.Commands
+{
+	/// <summary>
+	/// Description of FindReferences.
+	/// </summary>
+	public class FindReferences : DomMenuCommand
+	{
+		public override void Run()
+		{
+			var selectedItem = GetSelectedItems().FirstOrDefault();
+
+			if (selectedItem != null) {
+				IClass c = GetClassFromName(selectedItem.FullyQualifiedClassName);
+				if (c != null) {
+					IMember member = GetMemberFromName(c, selectedItem.MethodName, selectedItem.Parameters);
+					string memberName = member.DeclaringType.Name + "." + member.Name;
+					using (AsynchronousWaitDialog monitor = AsynchronousWaitDialog.ShowWaitDialog("${res:SharpDevelop.Refactoring.FindReferences}"))
+					{
+						FindReferencesAndRenameHelper.ShowAsSearchResults(StringParser.Parse("${res:SharpDevelop.Refactoring.ReferencesTo}",
+						                                                                     new string[,] {{ "Name", memberName }}),
+						                                                  RefactoringService.FindReferences(member, monitor));
+					}
+				}
+			}
+		}
+	}
+}
