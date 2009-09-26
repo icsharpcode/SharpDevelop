59eb977b8ae18aff928d877f409ba9d463cf9c69
Matt Ward <ward.matt@gmail.com>
2009-09-26 10:12:59 +0000

    Text editor indent settings now used when generating event handlers in Python forms designer.
    git-svn-id: svn://svn.sharpdevelop.net/sharpdevelop/branches/3.0@5000 1ccf3a8d-04fe-1044-b7c0-cef0b8235c61

--- src/AddIns/BackendBindings/Python/PythonBinding/Project/Src/PythonDesignerGenerator.cs
+++ src/AddIns/BackendBindings/Python/PythonBinding/Project/Src/PythonDesignerGenerator.cs
@@ -132,7 +132,7 @@ namespace ICSharpCode.PythonBinding
 				// Insert the event handler at the end of the class with an extra 
 				// new line before it.
 				IDocument doc = viewContent.DesignerCodeFileDocument;
-				string eventHandler = CreateEventHandler(eventMethodName, body, "\t");
+				string eventHandler = CreateEventHandler(eventMethodName, body, NRefactoryToPythonConverter.GetIndentString(textEditorProperties));
 				int line = doc.LineSegmentCollection.Count;
 				LineSegment lastLineSegment = doc.GetLineSegment(line - 1);
 				int offset = lastLineSegment.Offset + lastLineSegment.Length;
@@ -229,6 +229,7 @@ namespace ICSharpCode.PythonBinding
 		/// <summary>
 		/// Returns the generated event handler.
 		/// </summary>
+		/// <param name="indentation">The indent string to use for the event handler.</param>
 		protected string CreateEventHandler(string eventMethodName, string body, string indentation)
 		{			
 			if (String.IsNullOrEmpty(body)) {
@@ -241,9 +242,9 @@ namespace ICSharpCode.PythonBinding
 			eventHandler.Append("def ");
 			eventHandler.Append(eventMethodName);
 			eventHandler.Append("(self, sender, e):");
-			eventHandler.AppendLine();
+			eventHandler.AppendLine();			
 			eventHandler.Append(indentation);
-			eventHandler.Append("\t");
+			eventHandler.Append(NRefactoryToPythonConverter.GetIndentString(textEditorProperties));
 			eventHandler.Append(body);
 			
 			return eventHandler.ToString();
