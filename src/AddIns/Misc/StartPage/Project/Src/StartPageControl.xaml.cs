80b9d458bc722fc6133cf162bd454dfdae659d9c
Daniel Grunwald <daniel@danielgrunwald.de>
2010-01-07 18:26:30 +0000

    It's 2010. Don't hard-code copyright years in StartPageControl, read them from GlobalAssemblyInfo instead.
    git-svn-id: svn://svn.sharpdevelop.net/sharpdevelop/trunk@5381 1ccf3a8d-04fe-1044-b7c0-cef0b8235c61

--- src/AddIns/Misc/StartPage/Project/Src/StartPageControl.xaml.cs
+++ src/AddIns/Misc/StartPage/Project/Src/StartPageControl.xaml.cs
@@ -7,6 +7,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Windows.Controls;
 
 using ICSharpCode.Core;
@@ -25,6 +26,9 @@ namespace ICSharpCode.StartPage
 			// WPF does not use DataTemplates if the item already is a UIElement; so we 'box' it.
 			List<BoxEntry> entries = items.ConvertAll(control => new BoxEntry { Control = control } );
 			startPageItems.ItemsSource = entries;
+			
+			var aca = (AssemblyCopyrightAttribute)typeof(StartPageControl).Assembly.GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false)[0];
+			copyrightYearRange.Text = aca.Copyright.Substring(0, 9);
 		}
 		
 		sealed class BoxEntry
