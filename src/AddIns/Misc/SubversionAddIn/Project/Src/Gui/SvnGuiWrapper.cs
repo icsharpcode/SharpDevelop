icsharpcode/SharpDevelop

commit f5170d4fdeb8abf83855ff698ac5268c6484c0dd

Remove unused code. Replace ExtendedRegistry with .NET Registry functions. Replace SD.Set<T> with .NET SortedSet<T>.
git-svn-id: svn://svn.sharpdevelop.net/sharpdevelop/trunk@5246 1ccf3a8d-04fe-1044-b7c0-cef0b8235c61

src/AddIns/Misc/SubversionAddIn/Project/Src/Gui/SvnGuiWrapper.cs
@@ -5,7 +5,6 @@
 //     <version>$Revision$</version>
 // </file>
 
-using ICSharpCode.Profiler.Controller;
 using System;
 using System.Diagnostics;
 using System.Text;
@@ -22,16 +21,20 @@ namespace ICSharpCode.Svn
 	/// </summary>
 	public static class SvnGuiWrapper
 	{
-		static string GetPathFromRegistry(ExtendedRegistry registry, string valueName)
+		static string GetPathFromRegistry(RegistryHive hive, string valueName)
 		{
-			return registry.GetValue("SOFTWARE\\TortoiseSVN", valueName) as string;
+			RegistryView view = Environment.Is64BitOperatingSystem ? RegistryView.Registry64 : RegistryView.Default;
+			using (RegistryKey baseKey = RegistryKey.OpenBaseKey(hive, view)) {
+				using (RegistryKey key = baseKey.OpenSubKey("SOFTWARE\\TortoiseSVN")) {
+					return key.GetValue(valueName) as string;
+				}
+			}
 		}
 		
 		static string GetPathFromRegistry(string valueName)
 		{
-			bool is64Bit = ExtendedRegistry.Is64BitWindows;
-			return GetPathFromRegistry(is64Bit ? ExtendedRegistry.CurrentUser64 : ExtendedRegistry.CurrentUser32, valueName)
-				?? GetPathFromRegistry(is64Bit ? ExtendedRegistry.LocalMachine64 : ExtendedRegistry.LocalMachine32, valueName);
+			return GetPathFromRegistry(RegistryHive.CurrentUser, valueName)
+				?? GetPathFromRegistry(RegistryHive.LocalMachine, valueName);
 		}
 		
 		static void Proc(string command, string fileName, MethodInvoker callback)

AddIns/ICSharpCode.SharpDevelop.addin
@@ -1036,9 +1036,6 @@
 			<OptionPanel id    = "SelectStyle"
 			             label = "${res:Dialog.Options.IDEOptions.SelectVisualStyle.PanelName}"
 			             class = "ICSharpCode.SharpDevelop.Gui.OptionPanels.SelectStylePanel"/>
-			<OptionPanel id    = "Fullscreen"
-			             label = "${res:Dialog.Options.IDEOptions.FullScreen.Title}"
-			             class = "ICSharpCode.SharpDevelop.Gui.OptionPanels.FullscreenPanel"/>
 			<OptionPanel id    = "LoadSave"
 			             label = "${res:Dialog.Options.IDEOptions.LoadSaveOptions.PanelName}"
 			             class = "ICSharpCode.SharpDevelop.Gui.OptionPanels.LoadSavePanel"/>

src/AddIns/Misc/Profiler/Controller/DetectBinaryType.cs
@@ -77,7 +77,7 @@ namespace ICSharpCode.Profiler.Controller
 		
 		public static bool RunsAs64Bit(string exeName)
 		{
-			if (!ExtendedRegistry.Is64BitWindows) {
+			if (!Environment.Is64BitOperatingSystem) {
 				// this Windows installation is a 32-bit windows, so the app can never run as 64-bit.
 				return false;
 			}

src/AddIns/Misc/Profiler/Controller/ExtendedRegistry.cs
@@ -1,262 +1,0 @@
-// <file>
-//     <copyright see="prj:///doc/copyright.txt"/>
-//     <license see="prj:///doc/license.txt"/>
-//     <owner name="Siegfried Pammer" email="sie_pam@gmx.at"/>
-//     <version>$Revision$</version>
-// </file>
-
-using System;
-using System.ComponentModel;
-using System.Runtime.InteropServices;
-
-using HKEY = System.UIntPtr;
-
-namespace ICSharpCode.Profiler.Controller
-{
-	/// <summary>
-	/// Provides access to 32-bit Windows Registry on 32-bit systems and 32-bit and 64-bit views of the Windows Registry on 64-bit systems.
-	/// </summary>
-	#if PUBLIC_EXTENDEDREGISTRY
-	public
-		#endif
-		class ExtendedRegistry
-	{
-		static class NativeMethods
-		{
-//			internal static readonly UIntPtr HKEY_CLASSES_ROOT = new UIntPtr(0x80000000);
-			internal static readonly UIntPtr HKEY_CURRENT_USER = new UIntPtr(0x80000001);
-			internal static readonly UIntPtr HKEY_LOCAL_MACHINE = new UIntPtr(0x80000002);
-//			internal static readonly UIntPtr HKEY_USERS = new UIntPtr(0x80000003);
-//			internal static readonly UIntPtr HKEY_PERFORMANCE_DATA = new UIntPtr(0x80000004);
-//			internal static readonly UIntPtr HKEY_CURRENT_CONFIG = new UIntPtr(0x80000005);
-//			internal static readonly UIntPtr HKEY_DYN_DATA = new UIntPtr(0x80000006);
-			
-			[DllImport("advapi32.dll", CharSet = CharSet.Unicode)]
-			internal static extern int RegOpenKeyEx(HKEY hKey, string subKey, uint options, RegSAM samDesired, out HKEY hKeyResult);
-			
-			[DllImport("advapi32.dll")]
-			internal static extern int RegCloseKey(HKEY hKey);
-			
-			[DllImport("advapi32.dll", CharSet = CharSet.Unicode)]
-			internal static extern int RegQueryValueEx(HKEY hkey, string valueName, IntPtr reserved, out uint type, IntPtr buffer, ref uint bufferLength);
-			
-			[DllImport("advapi32.dll", CharSet = CharSet.Unicode)]
-			internal static extern int RegCreateKeyEx(HKEY hkey, string subKey, uint reserved, string lpClass, uint options, RegSAM samDesired, IntPtr securityAttributes, out HKEY result, out uint disposition);
-			
-			[DllImport("advapi32.dll", CharSet = CharSet.Unicode)]
-			internal static extern int RegDeleteKeyEx(HKEY hKey, string subKey, RegSAM samDesired, uint reserved);
-			
-			[DllImport("advapi32.dll", CharSet = CharSet.Unicode)]
-			internal static extern int RegDeleteKey(HKEY hKey, string subKey);
-			
-			[DllImport("advapi32.dll", CharSet = CharSet.Unicode)]
-			internal static extern int RegSetValueEx(HKEY hkey, string valueName, uint reserved, uint type, IntPtr data, uint dataLength);
-			
-			[DllImport("kernel32")]
-			internal static extern IntPtr GetCurrentProcess();
-			
-			[DllImport("kernel32", SetLastError = true)]
-			[return: MarshalAs(UnmanagedType.Bool)]
-			internal static extern bool IsWow64Process(IntPtr hProcess, [MarshalAs(UnmanagedType.Bool)] out bool wow64Process);
-		}
-		
-		[Flags]
-		enum RegOption
-		{
-			NonVolatile = 0x0,
-			Volatile = 0x1,
-			CreateLink = 0x2,
-			BackupRestore = 0x4,
-			OpenLink = 0x8
-		}
-		
-		[Flags]
-		enum RegSAM
-		{
-			QueryValue = 0x0001,
-			SetValue = 0x0002,
-			CreateSubKey = 0x0004,
-			EnumerateSubKeys = 0x0008,
-			Notify = 0x0010,
-			CreateLink = 0x0020,
-			WOW64_32Key = 0x0200,
-			WOW64_64Key = 0x0100,
-			Read = 0x00020019,
-			Write = 0x00020006,
-			Execute = 0x00020019,
-			AllAccess = 0x000f003f
-		}
-		
-		const uint REG_SZ = 0x1;
-		const int ERROR_MORE_DATA = 234;
-		const int ERROR_FILE_NOT_FOUND = 2;
-		
-		enum RegResult
-		{
-			CreatedNewKey = 0x00000001,
-			OpenedExistingKey = 0x00000002
-		}
-		
-		UIntPtr rootKey;
-		RegSAM keyType;
-		
-		ExtendedRegistry(UIntPtr rootKey, RegSAM keyType) {
-			this.rootKey = rootKey;
-			this.keyType = keyType;
-		}
-		
-		/// <summary>
-		/// Sets a value in a key. if the key does not exist it is created.
-		/// </summary>
-		/// <param name="subkey">The key, where to insert the value.</param>
-		/// <param name="valueName">The name of the value, use null for "(Default)".</param>
-		/// <param name="value">The data of the value.</param>
-		public void SetValue(string subkey, string valueName, string value)
-		{
-			if (value == null)
-				throw new ArgumentNullException("value");
-			if (subkey == null)
-				throw new ArgumentNullException("subkey");
-			
-			UIntPtr keyPtr;
-			uint dummy;
-			IntPtr ptr = IntPtr.Zero;
-			int errorCode = NativeMethods.RegCreateKeyEx(rootKey, subkey, 0x0, null, 0x0, RegSAM.CreateSubKey | RegSAM.SetValue | keyType, IntPtr.Zero, out keyPtr, out dummy);
-			if (errorCode != 0)
-				throw ExceptionForError(errorCode);
-			try {
-				ptr = Marshal.StringToHGlobalUni(value);
-				errorCode = NativeMethods.RegSetValueEx(keyPtr, valueName, 0, REG_SZ, ptr, (uint)((value.Length + 1) * 2));
-				if (errorCode != 0)
-					throw ExceptionForError(errorCode);
-			} finally {
-				if (ptr != null)
-					Marshal.FreeHGlobal(ptr);
-				errorCode = NativeMethods.RegCloseKey(keyPtr);
-			}
-			// check error code from RegCloseKey (but only if there was no other exception)
-			if (errorCode != 0)
-				throw ExceptionForError(errorCode);
-		}
-		
-		static Exception ExceptionForError(int code)
-		{
-			switch (code) {
-				case 5:
-					return new UnauthorizedAccessException();
-				default:
-					return new Win32Exception(code);
-			}
-		}
-		
-		/// <summary>
-		/// Gets a value from the registry.
-		/// </summary>
-		/// <param name="subkey">The key containing the value.</param>
-		/// <param name="valueName">The name of the value, use null for "(Default)".</param>
-		/// <returns>null, if the key does not exist or the value does not exist.
-		/// Otherwise, the value as string (currently, only REG_SZ is supported).</returns>
-		public object GetValue(string subkey, string valueName)
-		{
-			if (subkey == null)
-				throw new ArgumentNullException("subkey");
-			object result;
-			UIntPtr keyPtr;
-			IntPtr buffer = IntPtr.Zero;
-			int errorCode = NativeMethods.RegOpenKeyEx(rootKey, subkey, 0x0, RegSAM.Read | keyType, out keyPtr);
-			if (errorCode == ERROR_FILE_NOT_FOUND)
-				return null;
-			if (errorCode != 0)
-				throw ExceptionForError(errorCode);
-			try {
-				uint type;
-				uint bufferLength = 0;
-				// detect length of string
-				errorCode = NativeMethods.RegQueryValueEx(keyPtr, valueName, IntPtr.Zero, out type, IntPtr.Zero, ref bufferLength);
-				if (errorCode == ERROR_FILE_NOT_FOUND)
-					return null;
-				if (errorCode != 0 && errorCode != ERROR_MORE_DATA)
-					throw ExceptionForError(errorCode);
-				if (type != REG_SZ)
-					throw new NotSupportedException("Unsupported data type: " + type);
-				
-				buffer = Marshal.AllocHGlobal((int)bufferLength);
-				errorCode = NativeMethods.RegQueryValueEx(keyPtr, valueName, IntPtr.Zero, out type, buffer, ref bufferLength);
-				if (errorCode != 0)
-					throw ExceptionForError(errorCode);
-				
-				result = Marshal.PtrToStringUni(buffer);
-			} finally {
-				if (buffer != IntPtr.Zero)
-					Marshal.FreeHGlobal(buffer);
-				errorCode = NativeMethods.RegCloseKey(keyPtr);
-			}
-			// check error code from RegCloseKey (but only if there was no other exception)
-			if (errorCode != 0)
-				throw ExceptionForError(errorCode);
-			return result;
-		}
-		
-		/// <summary>
-		/// Deletes a key in the Windows Registry. The key must have no sub keys. All values of the key are deleted.
-		/// </summary>
-		/// <param name="subkey">The subkey to delete.</param>
-		public void DeleteSubkey(string subkey)
-		{
-			if (subkey == null)
-				throw new ArgumentNullException("subkey");
-			int retVal = (Environment.OSVersion.Version >= new Version(5,2))
-				? NativeMethods.RegDeleteKeyEx(rootKey, subkey, keyType, 0)
-				: NativeMethods.RegDeleteKey(rootKey, subkey);
-			if (retVal != 0 && retVal != ERROR_FILE_NOT_FOUND)
-				throw ExceptionForError(retVal);
-		}
-		
-		/// <summary>
-		/// Gets the 32-bit view of HKEY_CURRENT_USER.
-		/// </summary>
-		public static ExtendedRegistry CurrentUser32 {
-			get { return new ExtendedRegistry(NativeMethods.HKEY_CURRENT_USER, RegSAM.WOW64_32Key); }
-		}
-		
-		/// <summary>
-		/// Gets the 32-bit view of HKEY_LOCAL_MACHINE.
-		/// </summary>
-		public static ExtendedRegistry LocalMachine32 {
-			get { return new ExtendedRegistry(NativeMethods.HKEY_LOCAL_MACHINE, RegSAM.WOW64_32Key); }
-		}
-		
-		/// <summary>
-		/// Gets the 64-bit view of HKEY_CURRENT_USER.
-		/// </summary>
-		public static ExtendedRegistry CurrentUser64 {
-			get { return new ExtendedRegistry(NativeMethods.HKEY_CURRENT_USER, RegSAM.WOW64_64Key); }
-		}
-		
-		/// <summary>
-		/// Gets the 64-bit view of HKEY_LOCAL_MACHINE.
-		/// </summary>
-		public static ExtendedRegistry LocalMachine64 {
-			get { return new ExtendedRegistry(NativeMethods.HKEY_LOCAL_MACHINE, RegSAM.WOW64_64Key); }
-		}
-		
-		/// <summary>
-		/// Gets whether the OS is 64-bit Windows.
-		/// </summary>
-		public static bool Is64BitWindows {
-			get {
-				// this is a 64-bit process -> Windows is 64-bit
-				if (IntPtr.Size == 8)
-					return true;
-				
-				// this is a 32-bit process -> we need to check whether we run in Wow64 emulation
-				bool is64Bit;
-				if (NativeMethods.IsWow64Process(NativeMethods.GetCurrentProcess(), out is64Bit)) {
-					return is64Bit;
-				} else {
-					throw new Win32Exception(Marshal.GetLastWin32Error());
-				}
-			}
-		}
-	}
-}

src/AddIns/Misc/Profiler/Controller/Profiler.Controller.csproj
@@ -127,7 +127,6 @@
     </Compile>
     <Compile Include="Data\UnmanagedProfilingDataSet.cs">
     </Compile>
-    <Compile Include="ExtendedRegistry.cs" />
     <Compile Include="ExtensionMethods.cs" />
     <Compile Include="Interprocess\AtomicBoolean.cs" />
     <Compile Include="ProfilerException.cs" />

src/AddIns/Misc/Profiler/Controller/Registrar.cs
@@ -7,6 +7,7 @@
 
 using System;
 using System.IO;
+using Microsoft.Win32;
 
 namespace ICSharpCode.Profiler.Controller
 {
@@ -38,16 +39,16 @@ namespace ICSharpCode.Profiler.Controller
 			if (!File.Exists(path))
 				throw new FileNotFoundException("DLL not found!");
 			
-			return Register(is64Bit, guid, libraryId, classId, path);
+			return Register(is64Bit ? RegistryView.Registry64 : RegistryView.Registry32, guid, libraryId, classId, path);
 		}
 
-		static bool Register(bool is64Bit, string guid, string libraryId, string classId, string path)
+		static bool Register(RegistryView view, string guid, string libraryId, string classId, string path)
 		{
 			try {
-				CreateKeys(is64Bit ? ExtendedRegistry.LocalMachine64 : ExtendedRegistry.LocalMachine32, guid, libraryId, classId, path);
+				CreateKeys(RegistryHive.LocalMachine, view, guid, libraryId, classId, path);
 			} catch (UnauthorizedAccessException) {
 				try {
-					CreateKeys(is64Bit ? ExtendedRegistry.CurrentUser64 : ExtendedRegistry.CurrentUser32, guid, libraryId, classId, path);
+					CreateKeys(RegistryHive.CurrentUser, view, guid, libraryId, classId, path);
 				} catch (UnauthorizedAccessException) {
 					return false;
 				}
@@ -55,19 +56,26 @@ namespace ICSharpCode.Profiler.Controller
 			
 			return true;
 		}
-
-		static void CreateKeys(ExtendedRegistry reg, string guid, string libraryId, string classId, string path)
+		
+		static void CreateKeys(RegistryHive hive, RegistryView view, string guid, string libraryId, string classId, string path)
 		{
-			reg.SetValue("Software\\Classes\\CLSID\\" + guid, null, classId + " Class");
-			reg.SetValue("Software\\Classes\\CLSID\\" + guid + "\\InProcServer32", null, path);
-			reg.SetValue("Software\\Classes\\CLSID\\" + guid + "\\ProgId", null, libraryId + "." + classId);
+			using (RegistryKey key = RegistryKey.OpenBaseKey(hive, view)) {
+				using (RegistryKey subKey = key.CreateSubKey("Software\\Classes\\CLSID\\" + guid))
+					subKey.SetValue(null, classId + " Class");
+				using (RegistryKey subKey = key.CreateSubKey("Software\\Classes\\CLSID\\" + guid + "\\InProcServer32"))
+					subKey.SetValue(null, path);
+				using (RegistryKey subKey = key.CreateSubKey("Software\\Classes\\CLSID\\" + guid + "\\ProgId"))
+					subKey.SetValue(null, libraryId + "." + classId);
+			}
 		}
 		
-		static void DeleteKey(ExtendedRegistry reg, string guid)
+		static void DeleteKey(RegistryHive hive, RegistryView view, string guid)
 		{
-			reg.DeleteSubkey("Software\\Classes\\CLSID\\" + guid + "\\ProgId");
-			reg.DeleteSubkey("Software\\Classes\\CLSID\\" + guid + "\\InProcServer32");
-			reg.DeleteSubkey("Software\\Classes\\CLSID\\" + guid);
+			using (RegistryKey key = RegistryKey.OpenBaseKey(hive, view)) {
+				key.DeleteSubKey("Software\\Classes\\CLSID\\" + guid + "\\ProgId", false);
+				key.DeleteSubKey("Software\\Classes\\CLSID\\" + guid + "\\InProcServer32", false);
+				key.DeleteSubKey("Software\\Classes\\CLSID\\" + guid, false);
+			}
 		}
 		
 		/// <summary>
@@ -81,12 +89,13 @@ namespace ICSharpCode.Profiler.Controller
 			if (guid == null)
 				throw new ArgumentNullException("guid");
 			
+			RegistryView view = is64Bit ? RegistryView.Registry64 : RegistryView.Registry32;
 			try {
-				DeleteKey(is64Bit ? ExtendedRegistry.LocalMachine64 : ExtendedRegistry.LocalMachine32, guid);
-				DeleteKey(is64Bit ? ExtendedRegistry.CurrentUser64 : ExtendedRegistry.CurrentUser32, guid);
+				DeleteKey(RegistryHive.LocalMachine, view, guid);
+				DeleteKey(RegistryHive.CurrentUser, view, guid);
 			} catch (UnauthorizedAccessException) {
 				try {
-					DeleteKey(is64Bit ? ExtendedRegistry.CurrentUser64 : ExtendedRegistry.CurrentUser32, guid);
+					DeleteKey(RegistryHive.CurrentUser, view, guid);
 				} catch (UnauthorizedAccessException) {
 					return false;
 				}

src/AddIns/Misc/SubversionAddIn/Project/SubversionAddIn.csproj
@@ -1,4 +1,5 @@
-﻿<Project DefaultTargets="Build" xmlns="http://schemas.microsoft.com/developer/msbuild/2003" ToolsVersion="4.0">
+﻿<?xml version="1.0" encoding="utf-8"?>
+<Project DefaultTargets="Build" xmlns="http://schemas.microsoft.com/developer/msbuild/2003" ToolsVersion="4.0">
   <PropertyGroup>
     <OutputType>Library</OutputType>
     <RootNamespace>ICSharpCode.Svn</RootNamespace>
@@ -56,9 +57,6 @@
     <None Include="ICSharpCode.Svn.addin">
       <CopyToOutputDirectory>Always</CopyToOutputDirectory>
     </None>
-    <Compile Include="..\..\Profiler\Controller\ExtendedRegistry.cs">
-      <Link>Src\Gui\ExtendedRegistry.cs</Link>
-    </Compile>
     <Compile Include="Src\Gui\HistoryViewDisplayBinding\InfoPanel.cs" />
     <Compile Include="Src\Gui\HistoryViewDisplayBinding\InfoPanel.Designer.cs">
       <DependentUpon>InfoPanel.cs</DependentUpon>

src/Main/Base/Project/ICSharpCode.SharpDevelop.csproj
@@ -201,7 +201,6 @@
     <Compile Include="Src\Gui\Pads\ProjectBrowser\TreeNodes\DirectoryNodeFactory.cs" />
     <Compile Include="Src\Gui\Pads\TaskList\TaskListPadCommands.cs" />
     <Compile Include="Src\Gui\Pads\ToolsPad.cs" />
-    <None Include="Src\Gui\Workbench\Layouts\SimpleWorkbenchLayout.cs" />
     <Compile Include="Src\Gui\Workbench\Layouts\AvalonDockLayout.cs" />
     <Compile Include="Src\Gui\Workbench\Layouts\AvalonPadContent.cs" />
     <Compile Include="Src\Gui\Workbench\Layouts\AvalonWorkbenchWindow.cs" />
@@ -389,13 +388,6 @@
       <SubType>Component</SubType>
     </Compile>
     <Compile Include="Src\Gui\Pads\PropertyPad\PropertyPad.cs" />
-    <None Include="Src\Gui\Workbench\DefaultWorkbench.cs">
-      <SubType>Form</SubType>
-    </None>
-    <None Include="Src\Gui\Workbench\Layouts\SdiWorkspaceLayout.cs" />
-    <None Include="Src\Gui\Workbench\Layouts\SdiWorkspaceWindow.cs">
-      <SubType>Form</SubType>
-    </None>
     <Compile Include="Src\Gui\XmlForms\SharpDevelopPropertyValueCreator.cs" />
     <Compile Include="Src\Gui\XmlForms\SharpDevelopStringValueFilter.cs" />
     <Compile Include="Src\Services\IconService.cs" />
@@ -662,10 +654,6 @@
       <SubType>UserControl</SubType>
     </Compile>
     <Compile Include="Src\Services\RefactoringService\RefactoringMenuBuilder.cs" />
-    <EmbeddedResource Include="Resources\FullscreenPanel.xfrm" />
-    <Compile Include="Src\Gui\Dialogs\OptionPanels\IDEOptions\FullscreenPanel.cs">
-      <SubType>UserControl</SubType>
-    </Compile>
     <Compile Include="Src\Services\RefactoringService\LocalVariableRefactoring.cs" />
     <Compile Include="Src\Services\RefactoringService\FindReferencesAndRenameHelper.cs" />
     <Compile Include="Src\Internal\Doozers\DirectoryDoozer.cs" />
@@ -751,10 +739,8 @@
     <Compile Include="Src\Project\IParser.cs" />
     <Compile Include="Src\Project\CompilableProject.cs" />
     <Compile Include="Src\Project\MSBuildInternals.cs" />
-    <Compile Include="Src\Util\Pair.cs" />
     <Compile Include="Src\Util\ScrollUtils.cs" />
     <Compile Include="Src\Util\SDWindowsFormsHost.cs" />
-    <Compile Include="Src\Util\Set.cs" />
     <Compile Include="Src\Util\ReadOnlyCollectionWrapper.cs" />
     <Compile Include="Src\Project\Items\ItemType.cs" />
     <Compile Include="Src\Project\BuildTarget.cs" />

commit 88c568b8749be4fe4ae0a4594a4184f24208e974

Fixed NullReferenceException in ICSharpCode.Svn.SvnGuiWrapper.GetPathFromRegistry(RegistryHive hive, String valueName) when TortoiseSVN is not installed (bug introduced in r5246).
git-svn-id: svn://svn.sharpdevelop.net/sharpdevelop/trunk@5423 1ccf3a8d-04fe-1044-b7c0-cef0b8235c61

src/AddIns/Misc/SubversionAddIn/Project/Src/Gui/SvnGuiWrapper.cs
@@ -26,7 +26,10 @@ namespace ICSharpCode.Svn
 			RegistryView view = Environment.Is64BitOperatingSystem ? RegistryView.Registry64 : RegistryView.Default;
 			using (RegistryKey baseKey = RegistryKey.OpenBaseKey(hive, view)) {
 				using (RegistryKey key = baseKey.OpenSubKey("SOFTWARE\\TortoiseSVN")) {
-					return key.GetValue(valueName) as string;
+					if (key != null)
+						return key.GetValue(valueName) as string;
+					else
+						return null;
 				}
 			}
 		}