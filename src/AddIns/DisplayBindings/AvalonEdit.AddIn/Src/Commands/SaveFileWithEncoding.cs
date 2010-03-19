f49db06b88dd7383025a36b39f8be97933cbfe17
Daniel Grunwald <daniel@danielgrunwald.de>
2010-03-19 17:44:40 +0000

    Allow user to specify an encoding when saving a text file.
    git-svn-id: svn://svn.sharpdevelop.net/sharpdevelop/trunk@5629 1ccf3a8d-04fe-1044-b7c0-cef0b8235c61

---
+++ src/AddIns/DisplayBindings/AvalonEdit.AddIn/Src/Commands/SaveFileWithEncoding.cs
@@ -0,0 +1,34 @@
+﻿// <file>
+//     <copyright see="prj:///doc/copyright.txt"/>
+//     <license see="prj:///doc/license.txt"/>
+//     <author name="Daniel Grunwald"/>
+//     <version>$Revision$</version>
+// </file>
+
+using System;
+using ICSharpCode.Core;
+using ICSharpCode.SharpDevelop.Gui;
+
+namespace ICSharpCode.AvalonEdit.AddIn.Commands
+{
+	/// <summary>
+	/// Choose encoding, then save file.
+	/// </summary>
+	public class SaveFileWithEncoding : AbstractMenuCommand
+	{
+		public override void Run()
+		{
+			IViewContent vc = WorkbenchSingleton.Workbench.ActiveViewContent;
+			ICodeEditorProvider cep = vc as ICodeEditorProvider;
+			if (cep != null) {
+				ChooseEncodingDialog dlg = new ChooseEncodingDialog();
+				dlg.Owner = WorkbenchSingleton.MainWindow;
+				dlg.Encoding = cep.CodeEditor.PrimaryTextEditor.Encoding;
+				if (dlg.ShowDialog() == true) {
+					cep.CodeEditor.PrimaryTextEditor.Encoding = dlg.Encoding;
+					SharpDevelop.Commands.SaveFile.Save(vc.PrimaryFile);
+				}
+			}
+		}
+	}
+}
