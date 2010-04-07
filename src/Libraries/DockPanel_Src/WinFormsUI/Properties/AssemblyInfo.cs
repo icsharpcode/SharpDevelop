b7dd5692aa92f1a8345d22526896fe5fce9c626f
Daniel Grunwald <daniel@danielgrunwald.de>
2010-04-07 10:58:31 +0000

    Assign fixed version number to DockPanelSuite; the auto-increment versions seems to have caused setup trouble for some people.
    git-svn-id: svn://svn.sharpdevelop.net/sharpdevelop/branches/3.0@5675 1ccf3a8d-04fe-1044-b7c0-cef0b8235c61

--- src/Libraries/DockPanel_Src/WinFormsUI/Properties/AssemblyInfo.cs
+++ src/Libraries/DockPanel_Src/WinFormsUI/Properties/AssemblyInfo.cs
@@ -18,7 +18,8 @@ using System.Diagnostics.CodeAnalysis;
 
 [assembly: Guid("9d690ef9-ce19-4c69-874c-e24d8eb36aff")]
 
-[assembly: AssemblyVersion("2.2.*")]
+// just using a "high enough" fixed version number to avoid setup trouble
+[assembly: AssemblyVersion("2.2.20104.0")]
 [assembly: AssemblyFileVersion("2.2.0.0")]
 
 [module: SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", Scope = "namespace", Target = "WeifenLuo.WinFormsUI.Docking", MessageId = "Weifen")]
