5843430b263ee5fba79c55395766ede0b23c0d30
Daniel Grunwald <daniel@danielgrunwald.de>
2010-05-01 12:11:05 +0000

    Fixed C# parser bug when CheckedExpression occurs in an ExpressionStatement.
    git-svn-id: svn://svn.sharpdevelop.net/sharpdevelop/branches/3.0@5743 1ccf3a8d-04fe-1044-b7c0-cef0b8235c61

--- src/Libraries/NRefactory/Test/Parser/Statements/CheckedStatementTests.cs
+++ src/Libraries/NRefactory/Test/Parser/Statements/CheckedStatementTests.cs
@@ -23,10 +23,20 @@ namespace ICSharpCode.NRefactory.Tests.Ast
 			CheckedStatement checkedStatement = ParseUtilCSharp.ParseStatement<CheckedStatement>("checked { }");
 			Assert.IsFalse(checkedStatement.Block.IsNull);
 		}
+		
+		[Test]
+		public void CSharpCheckedStatementAndExpressionTest()
+		{
+			CheckedStatement checkedStatement = ParseUtilCSharp.ParseStatement<CheckedStatement>("checked { checked(++i); }");
+			Assert.IsFalse(checkedStatement.Block.IsNull);
+			ExpressionStatement es = (ExpressionStatement)checkedStatement.Block.Children[0];
+			CheckedExpression ce = (CheckedExpression)es.Expression;
+			Assert.IsInstanceOf<UnaryOperatorExpression>(ce.Expression);
+		}
 		#endregion
 		
 		#region VB.NET
-			// No VB.NET representation
+		// No VB.NET representation
 		#endregion
 	}
 }
