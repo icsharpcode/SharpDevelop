4036234c688372d890cdd57d57472c5f400ff259
Peter Forstmeier <peter.forstmeier@t-online.de>
2010-05-14 18:00:56 +0000

    Change Dir Structure in TestProject
    git-svn-id: svn://svn.sharpdevelop.net/sharpdevelop/branches/reports@5825 1ccf3a8d-04fe-1044-b7c0-cef0b8235c61

---
+++ src/AddIns/Misc/Reports/ICSharpCode.Reports.Core/Test/ICSharpCode.Reports.Core.Test/Exporter/DataPageBuilderFixture.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Peter Forstmeier
+ * Date: 14.05.2010
+ * Time: 20:04
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+using System;
+using NUnit.Framework;
+
+namespace ICSharpCode.Reports.Core.Test.Exporter
+{
+	[TestFixture]
+	public class DataPageBuilderFixture
+	{
+		[Test]
+		public void TestMethod()
+		{
+			// TODO: Add your test.
+		}
+		
+		[TestFixtureSetUp]
+		public void Init()
+		{
+			// TODO: Add Init code.
+		}
+		
+		[TestFixtureTearDown]
+		public void Dispose()
+		{
+			// TODO: Add tear down code.
+		}
+	}
+}
