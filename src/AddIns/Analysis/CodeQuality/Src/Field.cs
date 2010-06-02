19b4d75de9fe9c5cfc5d290ead985cc645dac26b
Tomáš Linhart <SneakerXZ@gmail.com>
2010-06-02 12:37:05 +0000

    Initial commit of Code Quality Analysis AddIn.
    git-svn-id: svn://svn.sharpdevelop.net/sharpdevelop/trunk@5893 1ccf3a8d-04fe-1044-b7c0-cef0b8235c61

---
+++ src/AddIns/Analysis/CodeQuality/Src/Field.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using QuickGraph;
+
+namespace ICSharpCode.CodeQualityAnalysis
+{
+    public class Field : IDependency
+    {
+        /// <summary>
+        /// Name of field
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Type of field
+        /// </summary>
+        public Type Type { get; set; }
+
+        public BidirectionalGraph<object, IEdge<object>> BuildDependencyGraph()
+        {
+            return null;
+        }
+    }
+}
