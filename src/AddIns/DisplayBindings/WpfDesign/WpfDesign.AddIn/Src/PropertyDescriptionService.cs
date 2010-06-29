d6ba95a4b4203516d45d2f75bcb9f49846cc1d78
Kumar Devvrat <kumardevvrat@gmail.com>
2010-06-29 04:44:20 +0000

    Add description for attached properties
    git-svn-id: svn://svn.sharpdevelop.net/sharpdevelop/branches/wpfdesigner@6007 1ccf3a8d-04fe-1044-b7c0-cef0b8235c61

--- src/AddIns/DisplayBindings/WpfDesign/WpfDesign.AddIn/Src/PropertyDescriptionService.cs
+++ src/AddIns/DisplayBindings/WpfDesign/WpfDesign.AddIn/Src/PropertyDescriptionService.cs
@@ -12,29 +12,39 @@ using ICSharpCode.SharpDevelop.Editor.CodeCompletion;
 
 namespace ICSharpCode.WpfDesign.AddIn
 {
-	public class PropertyDescriptionService : IPropertyDescriptionService
-	{
-		OpenedFile file;
-		
-		public PropertyDescriptionService(OpenedFile file)
-		{
-			if (file == null)
-				throw new ArgumentNullException("file");
-			this.file = file;
-		}
-		
-		public object GetDescription(DesignItemProperty property)
-		{
-			IProjectContent pc = MyTypeFinder.GetProjectContent(file);
-			if (pc != null) {
-				IClass c = pc.GetClassByReflectionName(property.DeclaringType.FullName, true);
-				if (c != null) {
-					IMember m = DefaultProjectContent.GetMemberByReflectionName(c, property.Name);
-					if (m != null)
-						return CodeCompletionItem.ConvertDocumentation(m.Documentation);
-				}
-			}
-			return null;
-		}
-	}
+    public class PropertyDescriptionService : IPropertyDescriptionService
+    {
+        OpenedFile file;
+
+        public PropertyDescriptionService(OpenedFile file)
+        {
+            if (file == null)
+                throw new ArgumentNullException("file");
+            this.file = file;
+        }
+
+        public object GetDescription(DesignItemProperty property)
+        {
+            IProjectContent pc = MyTypeFinder.GetProjectContent(file);
+            if (pc != null) {
+            	 // For attached Properties
+                if (property.DependencyFullName != null && property.Name.Contains(".")) {
+                    IClass c = pc.GetClassByReflectionName(property.DependencyProperty.OwnerType.FullName, true);
+                    if (c != null) {
+                        IMember m = DefaultProjectContent.GetMemberByReflectionName(c, property.DependencyProperty.Name + "Property");
+                        if (m != null)
+                            return CodeCompletionItem.ConvertDocumentation(m.Documentation);
+                    }
+                } else {
+                    IClass c = pc.GetClassByReflectionName(property.DeclaringType.FullName, true);
+                    if (c != null) {
+                        IMember m = DefaultProjectContent.GetMemberByReflectionName(c, property.Name);
+                        if (m != null)
+                            return CodeCompletionItem.ConvertDocumentation(m.Documentation);
+                    }
+                }
+            }
+            return null;
+        }
+    }
 }
