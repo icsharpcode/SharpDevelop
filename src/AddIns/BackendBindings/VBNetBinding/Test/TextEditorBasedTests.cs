6f56216b3e2f6e91770821c22ce8c710661bd534
Siegfried Pammer <siegfriedpammer@gmail.com>
2010-07-14 11:05:47 +0000

    added infrastructure for VB code completion tests
    git-svn-id: svn://svn.sharpdevelop.net/sharpdevelop/branches/vbnet@6121 1ccf3a8d-04fe-1044-b7c0-cef0b8235c61

---
+++ src/AddIns/BackendBindings/VBNetBinding/Test/TextEditorBasedTests.cs
@@ -0,0 +1,79 @@
+﻿// <file>
+//     <copyright see="prj:///doc/copyright.txt"/>
+//     <license see="prj:///doc/license.txt"/>
+//     <owner name="Siegfried Pammer" email="siegfriedpammer@gmail.com"/>
+//     <version>$Revision$</version>
+// </file>
+
+using ICSharpCode.SharpDevelop.Editor.CodeCompletion;
+using System;
+using ICSharpCode.SharpDevelop.Dom;
+using NUnit.Framework;
+
+namespace ICSharpCode.VBNetBinding.Tests
+{
+	public class TextEditorBasedTests
+	{
+		protected MockTextEditor textEditor;
+		
+		[SetUp]
+		public void SetupTest()
+		{
+			this.textEditor = new MockTextEditor();
+		}
+		
+		protected void TestCtrlSpace(string fileHeader, string fileFooter, bool expected, Action<ICompletionItemList> constraint)
+		{
+			this.textEditor.Document.Text = fileHeader + fileFooter;
+			this.textEditor.Caret.Offset = fileHeader.Length;
+			this.textEditor.CreateParseInformation();
+			
+			bool invoked = new VBNetCompletionBinding().CtrlSpace(textEditor);
+			
+			Assert.AreEqual(expected, invoked);
+			
+			ICompletionItemList list = this.textEditor.LastCompletionItemList;
+			
+			constraint(list);
+		}
+		
+		protected void TestKeyPress(string fileHeader, string fileFooter, char keyPressed, CodeCompletionKeyPressResult keyPressResult, Action<ICompletionItemList> constraint)
+		{
+			this.textEditor.Document.Text = fileHeader + fileFooter;
+			this.textEditor.Caret.Offset = fileHeader.Length;
+			this.textEditor.CreateParseInformation();
+			
+			CodeCompletionKeyPressResult result = new VBNetCompletionBinding().HandleKeyPress(textEditor, keyPressed);
+			
+			Assert.AreEqual(keyPressResult, result);
+			
+			ICompletionItemList list = this.textEditor.LastCompletionItemList;
+			
+			constraint(list);
+		}
+		
+		protected void TestTextInsert(string fileHeader, string fileFooter, char completionChar, ICompletionItemList list, ICompletionItem item, string expectedOutput, int expectedOffset)
+		{
+			this.textEditor.Document.Text = fileHeader + fileFooter;
+			this.textEditor.Caret.Offset = fileHeader.Length;
+			this.textEditor.CreateParseInformation();
+			
+			CompletionContext context = new CompletionContext() {
+				Editor = this.textEditor,
+				CompletionChar = completionChar,
+				StartOffset = textEditor.Caret.Offset,
+				EndOffset = textEditor.Caret.Offset
+			};
+			
+			list.Complete(context, item);
+			
+			if (!context.CompletionCharHandled && context.CompletionChar != '\n')
+				this.textEditor.Document.Insert(this.textEditor.Caret.Offset, completionChar + "");
+			
+			string insertedText = this.textEditor.Document.GetText(fileHeader.Length, this.textEditor.Document.TextLength - fileHeader.Length - fileFooter.Length);
+			
+			Assert.AreEqual(expectedOutput, insertedText);
+			Assert.AreEqual(fileHeader.Length + expectedOffset, textEditor.Caret.Offset);
+		}
+	}
+}
