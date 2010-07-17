b893e70e4bbc75607876f0c31e20652c6b8d6d03
Matt Ward <ward.matt@gmail.com>
2010-07-17 12:10:41 +0000

    Restructured code coverage unit tests.
    git-svn-id: svn://svn.sharpdevelop.net/sharpdevelop/trunk@6132 1ccf3a8d-04fe-1044-b7c0-cef0b8235c61

---
+++ src/AddIns/Analysis/CodeCoverage/Test/Coverage/PartCoverSettingsTestFixture.cs
@@ -0,0 +1,87 @@
+// <file>
+//     <copyright see="prj:///doc/copyright.txt"/>
+//     <license see="prj:///doc/license.txt"/>
+//     <owner name="Matthew Ward" email="mrward@users.sourceforge.net"/>
+//     <version>$Revision$</version>
+// </file>
+
+using ICSharpCode.SharpDevelop.Internal.Templates;
+using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+using ICSharpCode.CodeCoverage;
+using ICSharpCode.Core;
+using ICSharpCode.SharpDevelop.Project;
+using NUnit.Framework;
+
+namespace ICSharpCode.CodeCoverage.Tests.Coverage
+{
+	/// <summary>
+	/// Tests the saving and loading of the PartCover settings file. This
+	/// file is used to stores the includes and excludes regular expressions
+	/// that PartCover uses.
+	/// </summary>
+	[TestFixture]
+	public class PartCoverSettingsTestFixture
+	{
+		PartCoverSettings settings;
+		PartCoverSettings savedSettings;
+		StringBuilder savedSettingsXml;
+		XmlDocument doc;
+		
+		[SetUp]
+		public void Init()
+		{
+			settings = new PartCoverSettings();
+			settings.Include.Add("[a]*");
+			settings.Include.Add("[b]*");
+			settings.Exclude.Add("[c]*");
+			settings.Exclude.Add("[d]*");
+			savedSettingsXml = new StringBuilder();
+			settings.Save(new StringWriter(savedSettingsXml));
+			savedSettings = new PartCoverSettings(new StringReader(savedSettingsXml.ToString()));
+
+			doc = new XmlDocument();
+			doc.LoadXml(savedSettingsXml.ToString());
+		}
+		
+		[Test]
+		public void IsIncludeListSaved()
+		{
+			Assert.AreEqual(settings.Include, savedSettings.Include);
+		}
+		
+		[Test]
+		public void IsExcludedListSaved()
+		{
+			Assert.AreEqual(settings.Exclude, savedSettings.Exclude);
+		}
+		
+		[Test]
+		public void PartCoverSettingsFileName()
+		{
+			MSBuildBasedProject project = new MSBuildBasedProject(
+				new ProjectCreateInformation {
+					Solution = new Solution(),
+					OutputProjectFileName = @"C:\temp\test.csproj",
+					ProjectName = "test"
+				});
+			
+			Assert.AreEqual(@"C:\temp\test.PartCover.Settings", PartCoverSettings.GetFileName(project));
+		}
+		
+		[Test]
+		public void FourRuleElements()
+		{			
+			Assert.AreEqual(4, doc.SelectNodes("/PartCoverSettings/Rule").Count);
+		}
+		
+		[Test]
+		public void FirstRuleElement()
+		{
+			Assert.IsNotNull(doc.SelectSingleNode("/PartCoverSettings/Rule[text()='+[a]*']"));
+		}
+	}
+}
+
---
+++ src/AddIns/Analysis/CodeCoverage/Test/Highlighting/CodeCoverageMarkersInvalidStartColumnTestFixture.cs
@@ -0,0 +1,66 @@
+// <file>
+//     <copyright see="prj:///doc/copyright.txt"/>
+//     <license see="prj:///doc/license.txt"/>
+//     <owner name="Matthew Ward" email="mrward@users.sourceforge.net"/>
+//     <version>$Revision$</version>
+// </file>
+
+using ICSharpCode.SharpDevelop.Tests.Utils;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using ICSharpCode.CodeCoverage;
+using ICSharpCode.Core;
+using ICSharpCode.SharpDevelop.Editor;
+using NUnit.Framework;
+
+namespace ICSharpCode.CodeCoverage.Tests.Highlighting
+{
+	[TestFixture]
+	public class CodeCoverageMarkersInvalidStartColumnTestFixture
+	{
+		List<ITextMarker> markers;
+		
+		[SetUp]
+		public void Init()
+		{
+			try {
+				string configFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "NCoverAddIn.Tests");
+				PropertyService.InitializeService(configFolder, Path.Combine(configFolder, "data"), "NCoverAddIn.Tests");
+			} catch (Exception) {}
+			
+			IDocument document = MockTextMarkerService.CreateDocumentWithMockService();
+			ITextMarkerService markerStrategy = document.GetService(typeof(ITextMarkerService)) as ITextMarkerService;
+			document.Text = "abcdefg\r\nabcdefg";
+			
+			string xml = "<PartCoverReport>\r\n" +
+				"\t<File id=\"1\" url=\"c:\\Projects\\Test\\Test.cs\" />\r\n" +
+				"\t<Assembly id=\"1\" name=\"XmlEditor.Tests\" module=\"C:\\Projects\\Test\\XmlEditor.Tests\\bin\\XmlEditor.Tests.DLL\" domain=\"test-domain-XmlEditor.Tests.dll\" domainIdx=\"1\" />\r\n" +
+				"\t<Type name=\"XmlEditor.Tests.Schema.SingleElementSchemaTestFixture\" asmref=\"1\">\r\n" +
+				"\t\t<Method name=\"GetSchema\">\r\n" +
+				"\t\t<pt visit=\"1\" fid=\"1\" sl=\"1\" sc=\"0\" el=\"1\" ec=\"1\" />\r\n" +
+				"\t\t<pt visit=\"1\" fid=\"1\" sl=\"1\" sc=\"-1\" el=\"1\" ec=\"1\" />\r\n" +
+				"\t\t<pt visit=\"1\" fid=\"1\" sl=\"1\" sc=\"8\" el=\"1\" ec=\"20\" />\r\n" +
+				"\t\t</Method>\r\n" +
+				"\t</Type>\r\n" +
+				"</PartCoverReport>";
+			
+			CodeCoverageResults results = new CodeCoverageResults(new StringReader(xml));
+			CodeCoverageMethod method = results.Modules[0].Methods[0];
+			CodeCoverageHighlighter highlighter = new CodeCoverageHighlighter();
+			highlighter.AddMarkers(document, method.SequencePoints);
+			
+			markers = new List<ITextMarker>();
+			foreach (ITextMarker marker in markerStrategy.TextMarkers) {
+				markers.Add(marker);
+			}
+		}
+		
+		[Test]
+		public void NoMarkersAdded()
+		{
+			Assert.AreEqual(0, markers.Count, 
+				"Should not be any markers added since all sequence point start columns are invalid.");
+		}
+	}
+}
