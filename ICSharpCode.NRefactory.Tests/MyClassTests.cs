2b4cea9ae8d38c7a1733171df4cd3cbbe72dc493
Daniel Grunwald <daniel@danielgrunwald.de>
2010-07-26 22:57:58 +0200

    Create empty project

---
+++ ICSharpCode.NRefactory.Tests/MyClassTests.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) 2010 AlphaSierraPapa for the SharpDevelop Team (for details please see \doc\copyright.txt)
+// This code is distributed under MIT X11 license (for details please see \doc\license.txt)
+
+using System;
+using System.Collections.Generic;
+
+namespace ICSharpCode.NRefactory.Tests
+{
+	[TestFixture]
+	public class MyClassTests
+	{
+		[Test]
+		public void OK()
+		{
+			
+		}
+	}
+}
---
+++ ICSharpCode.NRefactory/MyClass.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) 2010 AlphaSierraPapa for the SharpDevelop Team (for details please see \doc\copyright.txt)
+// This code is distributed under MIT X11 license (for details please see \doc\license.txt)
+
+using System;
+using System.Collections.Generic;
+
+namespace ICSharpCode.NRefactory
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	public class MyClass
+	{
+		
+	}
+}
