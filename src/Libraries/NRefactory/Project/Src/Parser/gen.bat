d2f4f622de1cc5a44fea67325431a63e0d847c5b
Siegfried Pammer <siegfriedpammer@gmail.com>
2010-10-03 18:07:28 +0200

    removed trace-generation from SharpCoco call for VB .NET

--- src/Libraries/NRefactory/Project/Src/Parser/gen.bat
+++ src/Libraries/NRefactory/Project/Src/Parser/gen.bat
@@ -9,7 +9,7 @@ SharpCoco -namespace ICSharpCode.NRefactory.Parser.CSharp cs.ATG
 move Parser.cs ..\CSharp
 
 copy ..\VBNet\VBNET.ATG
-SharpCoco -trace GIPXA -namespace ICSharpCode.NRefactory.Parser.VB VBNET.ATG
+SharpCoco -namespace ICSharpCode.NRefactory.Parser.VB VBNET.ATG
 move Parser.cs ..\VBNet
 
 del cs.ATG
