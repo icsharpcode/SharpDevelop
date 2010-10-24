2f1cb1ddaa2a0a8739ce467e2c74b085bdf17b12
mrward <ward.matt@gmail.com>
2010-10-24 18:42:24 +0100

    Update NUnit to version 2.5.8

--- src/Tools/NUnit/buildnunitconsole.bat
+++ src/Tools/NUnit/buildnunitconsole.bat
@@ -1,4 +1,4 @@
-%windir%\microsoft.net\framework\v3.5\msbuild /property:Configuration=Release nunit-console\nunit-console.sln 
+%windir%\microsoft.net\framework\v3.5\msbuild /t:Rebuild /property:Configuration=Release nunit-console\nunit-console.sln 
 copy nunit-console.exe nunit-console-x86.exe
 "%programfiles%\Microsoft.net\sdk\v2.0\bin\corflags" /32bit+ nunit-console-x86.exe
 @IF %ERRORLEVEL% NEQ 0 GOTO err
