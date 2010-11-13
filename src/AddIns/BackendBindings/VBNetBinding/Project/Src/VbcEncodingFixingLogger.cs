8260cc1c8154de34bdb9b150694db66d54071005
Siegfried Pammer <siegfriedpammer@gmail.com>
2010-11-13 20:47:57 +0100

    fixed SD-1746 - VB compiler errors are shown in incorrect column if the line contains tabs (http://bugtracker.sharpdevelop.net/issue/ViewIssue.aspx?id=1746&PROJID=4)

--- src/AddIns/BackendBindings/VBNetBinding/Project/Src/VbcEncodingFixingLogger.cs
+++ src/AddIns/BackendBindings/VBNetBinding/Project/Src/VbcEncodingFixingLogger.cs
@@ -40,12 +40,50 @@ namespace ICSharpCode.VBNetBinding
 			{
 				error.ErrorText = FixEncoding(error.ErrorText);
 				error.FileName = FixEncoding(error.FileName);
+				error.Column = FixColumn(error.FileName, error.Line, error.Column);
 				nextFilter.HandleError(error);
 			}
 			
 			public void HandleBuildEvent(Microsoft.Build.Framework.BuildEventArgs e)
 			{
 				nextFilter.HandleBuildEvent(e);
+				if (e is TaskFinishedEventArgs && lastFile != null) {
+					lastFile.Close();
+					lastFile = null;
+				}
+			}
+			
+			string lastFileName, lastLineText;
+			StreamReader lastFile;
+			int lastLine;
+			
+			int FixColumn(string fileName, int line, int column)
+			{
+				if (!File.Exists(fileName) || line < 1 || column < 1)
+					return column;
+				
+				if (fileName != lastFileName || line < lastLine || lastFile == null) {
+					if (lastFile != null)
+						lastFile.Close();
+					lastFile = new StreamReader(fileName);
+					lastFileName = fileName;
+					lastLineText = "";
+					lastLine = 0;
+				}
+				
+				while (lastLine < line && lastLineText != null) {
+					lastLineText = lastFile.ReadLine();
+					lastLine++;
+				}
+				
+				if (!string.IsNullOrEmpty(lastLineText)) {
+					for (int i = 0; i < column && i < lastLineText.Length; i++) {
+						if (lastLineText[i] == '\t')
+							column -= 3;
+					}
+				}
+				
+				return column;
 			}
 		}
 	}
