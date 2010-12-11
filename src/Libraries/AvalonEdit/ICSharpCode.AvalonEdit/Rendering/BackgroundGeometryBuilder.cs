4ca5aa4757f5f0e0639d630880042db5e0324fe0
Eusebiu Marcu <marcueusebiu@gmail.com>
2010-12-12 01:56:08 +0200

    Center the geometry of the builder

--- src/Libraries/AvalonEdit/ICSharpCode.AvalonEdit/Rendering/BackgroundGeometryBuilder.cs
+++ src/Libraries/AvalonEdit/ICSharpCode.AvalonEdit/Rendering/BackgroundGeometryBuilder.cs
@@ -56,16 +56,16 @@ namespace ICSharpCode.AvalonEdit.Rendering
 			foreach (Rect r in GetRectsForSegment(textView, segment)) {
 				if (AlignToWholePixels) {
 					AddRectangle(PixelSnapHelpers.Round(r.Left, pixelSize.Width),
-					             PixelSnapHelpers.Round(r.Top, pixelSize.Height),
+					             PixelSnapHelpers.Round(r.Top + 1, pixelSize.Height),
 					             PixelSnapHelpers.Round(r.Right, pixelSize.Width),
-					             PixelSnapHelpers.Round(r.Bottom, pixelSize.Height));
+					             PixelSnapHelpers.Round(r.Bottom + 1, pixelSize.Height));
 				} else if (AlignToMiddleOfPixels) {
 					AddRectangle(PixelSnapHelpers.PixelAlign(r.Left, pixelSize.Width),
-					             PixelSnapHelpers.PixelAlign(r.Top, pixelSize.Height),
+					             PixelSnapHelpers.PixelAlign(r.Top + 1, pixelSize.Height),
 					             PixelSnapHelpers.PixelAlign(r.Right, pixelSize.Width),
-					             PixelSnapHelpers.PixelAlign(r.Bottom, pixelSize.Height));
+					             PixelSnapHelpers.PixelAlign(r.Bottom + 1, pixelSize.Height));
 				} else {
-					AddRectangle(r.Left, r.Top, r.Right, r.Bottom);
+					AddRectangle(r.Left, r.Top + 1, r.Right, r.Bottom + 1);
 				}
 			}
 		}
