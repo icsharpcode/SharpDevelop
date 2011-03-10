icsharpcode/SharpDevelop

commit b7b12d310e2021338c3e12e9926ecf380a44e471

Reduce memory usage when dealing with long lines and word-wrapping.

src/Libraries/AvalonEdit/ICSharpCode.AvalonEdit/Rendering/LinkElementGenerator.cs
@@ -2,16 +2,8 @@
 // This code is distributed under the GNU LGPL (for details please see \doc\license.txt)
 
 using System;
-using System.Diagnostics;
 using System.Text.RegularExpressions;
-using System.Windows;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.TextFormatting;
-
-using ICSharpCode.AvalonEdit.Document;
-using System.Windows.Navigation;
+using ICSharpCode.AvalonEdit.Utils;
 
 namespace ICSharpCode.AvalonEdit.Rendering
 {
@@ -68,8 +60,8 @@ namespace ICSharpCode.AvalonEdit.Rendering
 		Match GetMatch(int startOffset)
 		{
 			int endOffset = CurrentContext.VisualLine.LastDocumentLine.EndOffset;
-			string relevantText = CurrentContext.Document.GetText(startOffset, endOffset - startOffset);
-			return linkRegex.Match(relevantText);
+			StringSegment relevantText = CurrentContext.GetText(startOffset, endOffset - startOffset);
+			return linkRegex.Match(relevantText.Text, relevantText.Offset, relevantText.Count);
 		}
 		
 		/// <inheritdoc/>

src/Libraries/AvalonEdit/ICSharpCode.AvalonEdit/Rendering/SingleCharacterElementGenerator.cs
@@ -60,10 +60,11 @@ namespace ICSharpCode.AvalonEdit.Rendering
 		public override int GetFirstInterestedOffset(int startOffset)
 		{
 			DocumentLine endLine = CurrentContext.VisualLine.LastDocumentLine;
-			string relevantText = CurrentContext.Document.GetText(startOffset, endLine.EndOffset - startOffset);
+			StringSegment relevantText = CurrentContext.GetText(startOffset, endLine.EndOffset - startOffset);
 			
-			for (int i = 0; i < relevantText.Length; i++) {
-				char c = relevantText[i];
+			int endPos = relevantText.Offset + relevantText.Count;
+			for (int i = relevantText.Offset; i < endPos; i++) {
+				char c = relevantText.Text[i];
 				switch (c) {
 					case ' ':
 						if (ShowSpaces)

src/Libraries/AvalonEdit/ICSharpCode.AvalonEdit/Utils/StringSegment.cs
@@ -1,0 +1,107 @@
+﻿// Copyright (c) AlphaSierraPapa for the SharpDevelop Team (for details please see \doc\copyright.txt)
+// This code is distributed under MIT X11 license (for details please see \doc\license.txt)
+
+using System;
+
+namespace ICSharpCode.AvalonEdit.Utils
+{
+	/// <summary>
+	/// Represents a string with a segment.
+	/// Similar to System.ArraySegment&lt;T&gt;, but for strings instead of arrays.
+	/// </summary>
+	public struct StringSegment : IEquatable<StringSegment>
+	{
+		readonly string text;
+		readonly int offset;
+		readonly int count;
+		
+		/// <summary>
+		/// Creates a new StringSegment.
+		/// </summary>
+		public StringSegment(string text, int offset, int count)
+		{
+			if (text == null)
+				throw new ArgumentNullException("text");
+			if (offset < 0 || offset > text.Length)
+				throw new ArgumentOutOfRangeException("offset");
+			if (offset + count > text.Length)
+				throw new ArgumentOutOfRangeException("count");
+			this.text = text;
+			this.offset = offset;
+			this.count = count;
+		}
+		
+		/// <summary>
+		/// Creates a new StringSegment.
+		/// </summary>
+		public StringSegment(string text)
+		{
+			if (text == null)
+				throw new ArgumentNullException("text");
+			this.text = text;
+			this.offset = 0;
+			this.count = text.Length;
+		}
+		
+		/// <summary>
+		/// Gets the string used for this segment.
+		/// </summary>
+		public string Text {
+			get { return text; }
+		}
+		
+		/// <summary>
+		/// Gets the start offset of the segment with the text.
+		/// </summary>
+		public int Offset {
+			get { return offset; }
+		}
+		
+		/// <summary>
+		/// Gets the length of the segment.
+		/// </summary>
+		public int Count {
+			get { return count; }
+		}
+		
+		#region Equals and GetHashCode implementation
+		/// <inheritdoc/>
+		public override bool Equals(object obj)
+		{
+			if (obj is StringSegment)
+				return Equals((StringSegment)obj); // use Equals method below
+			else
+				return false;
+		}
+		
+		/// <inheritdoc/>
+		public bool Equals(StringSegment other)
+		{
+			// add comparisions for all members here
+			return object.ReferenceEquals(this.text, other.text) && offset == other.offset && count == other.count;
+		}
+		
+		/// <inheritdoc/>
+		public override int GetHashCode()
+		{
+			return text.GetHashCode() ^ offset ^ count;
+		}
+		
+		/// <summary>
+		/// Equality operator.
+		/// </summary>
+		public static bool operator ==(StringSegment left, StringSegment right)
+		{
+			return left.Equals(right);
+		}
+		
+		/// <summary>
+		/// Inequality operator.
+		/// </summary>
+		public static bool operator !=(StringSegment left, StringSegment right)
+		{
+			return !left.Equals(right);
+		}
+		#endregion
+	}
+}

src/Libraries/AvalonEdit/ICSharpCode.AvalonEdit/ICSharpCode.AvalonEdit.csproj
@@ -342,6 +342,7 @@
     <Compile Include="Utils\Rope.cs" />
     <Compile Include="Utils\RopeNode.cs" />
     <Compile Include="Utils\RopeTextReader.cs" />
+    <Compile Include="Utils\StringSegment.cs" />
     <Compile Include="Utils\TextFormatterFactory.cs" />
     <Compile Include="Utils\WeakEventManagerBase.cs" />
     <Compile Include="Utils\PixelSnapHelpers.cs" />

src/Libraries/AvalonEdit/ICSharpCode.AvalonEdit/Rendering/ITextRunConstructionContext.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Windows.Media.TextFormatting;
 using ICSharpCode.AvalonEdit.Document;
+using ICSharpCode.AvalonEdit.Utils;
 
 namespace ICSharpCode.AvalonEdit.Rendering
 {
@@ -31,5 +32,16 @@ namespace ICSharpCode.AvalonEdit.Rendering
 		/// Gets the global text run properties.
 		/// </summary>
 		TextRunProperties GlobalTextRunProperties { get; }
+		
+		/// <summary>
+		/// Gets a piece of text from the document.
+		/// </summary>
+		/// <remarks>
+		/// This method is allowed to return a larger string than requested.
+		/// It does this by returning a <see cref="StringSegment"/> that describes the requested segment within the returned string.
+		/// This method should be the preferred text access method in the text transformation pipeline, as it can avoid repeatedly allocating string instances
+		/// for text within the same line.
+		/// </remarks>
+		StringSegment GetText(int offset, int length);
 	}
 }

src/Libraries/AvalonEdit/ICSharpCode.AvalonEdit/Rendering/VisualLineText.cs
@@ -53,8 +53,8 @@ namespace ICSharpCode.AvalonEdit.Rendering
 				throw new ArgumentNullException("context");
 			
 			int relativeOffset = startVisualColumn - VisualColumn;
-			string text = context.Document.GetText(context.VisualLine.FirstDocumentLine.Offset + RelativeTextOffset + relativeOffset, DocumentLength - relativeOffset);
-			return new TextCharacters(text, 0, text.Length, this.TextRunProperties);
+			StringSegment text = context.GetText(context.VisualLine.FirstDocumentLine.Offset + RelativeTextOffset + relativeOffset, DocumentLength - relativeOffset);
+			return new TextCharacters(text.Text, text.Offset, text.Count, this.TextRunProperties);
 		}
 		
 		/// <inheritdoc/>
@@ -71,8 +71,8 @@ namespace ICSharpCode.AvalonEdit.Rendering
 				throw new ArgumentNullException("context");
 			
 			int relativeOffset = visualColumnLimit - VisualColumn;
-			string text = context.Document.GetText(context.VisualLine.FirstDocumentLine.Offset + RelativeTextOffset, relativeOffset);
-			CharacterBufferRange range = new CharacterBufferRange(text, 0, text.Length);
+			StringSegment text = context.GetText(context.VisualLine.FirstDocumentLine.Offset + RelativeTextOffset, relativeOffset);
+			CharacterBufferRange range = new CharacterBufferRange(text.Text, text.Offset, text.Count);
 			return new TextSpan<CultureSpecificCharacterBufferRange>(range.Length, new CultureSpecificCharacterBufferRange(this.TextRunProperties.CultureInfo, range));
 		}
 		

src/Libraries/AvalonEdit/ICSharpCode.AvalonEdit/Rendering/VisualLineTextSource.cs
@@ -6,6 +6,7 @@ using System.Diagnostics;
 using System.Windows.Media.TextFormatting;
 
 using ICSharpCode.AvalonEdit.Document;
+using ICSharpCode.AvalonEdit.Utils;
 
 namespace ICSharpCode.AvalonEdit.Rendering
 {
@@ -82,6 +83,20 @@ namespace ICSharpCode.AvalonEdit.Rendering
 		public override int GetTextEffectCharacterIndexFromTextSourceCharacterIndex(int textSourceCharacterIndex)
 		{
 			throw new NotSupportedException();
+		}
+		
+		string cachedString;
+		int cachedStringOffset;
+		
+		public StringSegment GetText(int offset, int length)
+		{
+			if (cachedString != null) {
+				if (offset >= cachedStringOffset && offset + length <= cachedStringOffset + cachedString.Length) {
+					return new StringSegment(cachedString, offset - cachedStringOffset, length);
+				}
+			}
+			cachedStringOffset = offset;
+			return new StringSegment(cachedString = this.Document.GetText(offset, length));
 		}
 	}
 }

commit b4f6f3a435bba08603528058e51890db13f55e47

Fixed bug introduced in b7b12d310e2021338c3e12e9926ecf380a44e471 (Reduce memory usage when dealing with long lines and word-wrapping)

src/Libraries/AvalonEdit/ICSharpCode.AvalonEdit/Rendering/LinkElementGenerator.cs
@@ -57,25 +57,29 @@ namespace ICSharpCode.AvalonEdit.Rendering
 			this.RequireControlModifierForClick = options.RequireControlModifierForHyperlinkClick;
 		}
 		
-		Match GetMatch(int startOffset)
+		Match GetMatch(int startOffset, out int matchOffset)
 		{
 			int endOffset = CurrentContext.VisualLine.LastDocumentLine.EndOffset;
 			StringSegment relevantText = CurrentContext.GetText(startOffset, endOffset - startOffset);
-			return linkRegex.Match(relevantText.Text, relevantText.Offset, relevantText.Count);
+			Match m = linkRegex.Match(relevantText.Text, relevantText.Offset, relevantText.Count);
+			matchOffset = m.Success ? m.Index - relevantText.Offset + startOffset : -1;
+			return m;
 		}
 		
 		/// <inheritdoc/>
 		public override int GetFirstInterestedOffset(int startOffset)
 		{
-			Match m = GetMatch(startOffset);
-			return m.Success ? startOffset + m.Index : -1;
+			int matchOffset;
+			GetMatch(startOffset, out matchOffset);
+			return matchOffset;
 		}
 		
 		/// <inheritdoc/>
 		public override VisualLineElement ConstructElement(int offset)
 		{
-			Match m = GetMatch(offset);
-			if (m.Success && m.Index == 0) {
+			int matchOffset;
+			Match m = GetMatch(offset, out matchOffset);
+			if (m.Success && matchOffset == offset) {
 				Uri uri = GetUriFromMatch(m);
 				if (uri == null)
 					return null;

src/Libraries/AvalonEdit/ICSharpCode.AvalonEdit/Rendering/SingleCharacterElementGenerator.cs
@@ -62,9 +62,8 @@ namespace ICSharpCode.AvalonEdit.Rendering
 			DocumentLine endLine = CurrentContext.VisualLine.LastDocumentLine;
 			StringSegment relevantText = CurrentContext.GetText(startOffset, endLine.EndOffset - startOffset);
 			
-			int endPos = relevantText.Offset + relevantText.Count;
-			for (int i = relevantText.Offset; i < endPos; i++) {
-				char c = relevantText.Text[i];
+			for (int i = 0; i < relevantText.Count; i++) {
+				char c = relevantText.Text[relevantText.Offset + i];
 				switch (c) {
 					case ' ':
 						if (ShowSpaces)

src/Libraries/AvalonEdit/ICSharpCode.AvalonEdit/Rendering/VisualLine.cs
@@ -137,8 +137,14 @@ namespace ICSharpCode.AvalonEdit.Rendering
 								askInterestOffset = 0;
 								offset += element.DocumentLength;
 								if (offset > currentLineEnd) {
-									LastDocumentLine = document.GetLineByOffset(offset);
-									currentLineEnd = LastDocumentLine.Offset + LastDocumentLine.Length;
+									DocumentLine newEndLine = document.GetLineByOffset(offset);
+									if (newEndLine == this.LastDocumentLine) {
+										throw new InvalidOperationException(
+											"The VisualLineElementGenerator " + g.GetType().Name +
+											" produced an element which ends within the line delimiter");
+									}
+									currentLineEnd = newEndLine.Offset + newEndLine.Length;
+									this.LastDocumentLine = newEndLine;
 								}
 								break;
 							}

src/Libraries/AvalonEdit/ICSharpCode.AvalonEdit/Utils/StringSegment.cs
@@ -1,5 +1,5 @@
 ﻿// Copyright (c) AlphaSierraPapa for the SharpDevelop Team (for details please see \doc\copyright.txt)
-// This code is distributed under MIT X11 license (for details please see \doc\license.txt)
+// This code is distributed under the GNU LGPL (for details please see \doc\license.txt)
 
 using System;
 