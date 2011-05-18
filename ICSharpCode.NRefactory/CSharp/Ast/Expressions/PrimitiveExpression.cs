fc86a6770491e756c28223cfd1caec62bd9c4881
Mike Krüger <mkrueger@novell.com>
2011-05-18 10:45:32 +0200

    Fixed relocation bug.

--- ICSharpCode.NRefactory/CSharp/Ast/Expressions/PrimitiveExpression.cs
+++ ICSharpCode.NRefactory/CSharp/Ast/Expressions/PrimitiveExpression.cs
@@ -29,7 +29,7 @@ namespace ICSharpCode.NRefactory.CSharp
 	/// <summary>
 	/// Represents a literal value.
 	/// </summary>
-	public class PrimitiveExpression : Expression
+	public class PrimitiveExpression : Expression, IRelocatable
 	{
 		public static readonly object AnyValue = new object();
 		
@@ -63,6 +63,13 @@ namespace ICSharpCode.NRefactory.CSharp
 			this.startLocation = startLocation;
 			this.length = length;
 		}
+		
+		#region IRelocationable implementation
+		void IRelocatable.SetStartLocation (AstLocation startLocation)
+		{
+			this.startLocation = startLocation;
+		}
+		#endregion
 		
 		public override S AcceptVisitor<T, S> (IAstVisitor<T, S> visitor, T data)
 		{
