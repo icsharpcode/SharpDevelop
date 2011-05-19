d6d6d5a9080b2a15f0111e0b062b6a56440918ab
Matt Ward <ward.matt@gmail.com>
2011-05-19 20:03:22 +0100

    Do not indicate a line is waiting when scripting console is disposed.

---
+++ src/AddIns/BackendBindings/Scripting/Test/Console/ThreadSafeScriptingConsoleEventsTests.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) AlphaSierraPapa for the SharpDevelop Team (for details please see \doc\copyright.txt)
+// This code is distributed under the GNU LGPL (for details please see \doc\license.txt)
+
+using System;
+using NUnit.Framework;
+
+namespace ICSharpCode.Scripting.Tests.Console
+{
+	[TestFixture]
+	public class ThreadSafeScriptingConsoleEventsTests
+	{
+		ThreadSafeScriptingConsoleEvents consoleEvents;
+		
+		void CreateConsoleEvents()
+		{
+			consoleEvents = new ThreadSafeScriptingConsoleEvents();
+		}
+		
+		[Test]
+		public void WaitForLine_DisposedEventSetAndLineReceivedEventSet_ReturnsFalse()
+		{
+			CreateConsoleEvents();
+			consoleEvents.SetLineReceivedEvent();
+			consoleEvents.SetDisposedEvent();
+			
+			bool wait = consoleEvents.WaitForLine();
+			
+			Assert.IsFalse(wait);
+		}
+	}
+}
