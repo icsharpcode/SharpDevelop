66d07b6e62e179fb22d27dd674e3c246cb15ae0b
Matt Ward <ward.matt@gmail.com>
2011-06-02 19:34:04 +0100

    Fix errors when installing jQuery NuGet package.

---
+++ src/AddIns/Misc/PackageManagement/Project/Src/VisualStudio/Package.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) AlphaSierraPapa for the SharpDevelop Team (for details please see \doc\copyright.txt)
+// This code is distributed under the GNU LGPL (for details please see \doc\license.txt)
+
+using System;
+using Microsoft.VisualStudio.ExtensionManager;
+
+namespace Microsoft.VisualStudio.Shell
+{
+	public abstract class Package
+	{
+		public static object GetGlobalService(Type serviceType)
+		{
+			return new SVsExtensionManager();
+		}
+	}
+}
