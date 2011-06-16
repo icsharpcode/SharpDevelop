b7fc89cb9713a17c0387d12c2958434f968ae2b7
Mike Krüger <mkrueger@novell.com>
2011-06-16 15:14:10 +0200

    added header.

--- ICSharpCode.NRefactory/TypeSystem/IAnnotatable.cs
+++ ICSharpCode.NRefactory/TypeSystem/IAnnotatable.cs
@@ -1,3 +1,6 @@
+// Copyright (c) 2010 AlphaSierraPapa for the SharpDevelop Team (for details please see \doc\copyright.txt)
+// This code is distributed under MIT X11 license (for details please see \doc\license.txt)
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
