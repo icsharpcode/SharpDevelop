c692824b5ddfeacd12db3f68132282a329e2a8f7
Matt Ward <ward.matt@gmail.com>
2011-08-29 17:45:29 +0100

    Resolve assemblies referenced by T4 templates that are not referenced by the project.

---
+++ src/AddIns/Misc/TextTemplating/Project/Src/TextTemplatingReflectionProjectContent.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) AlphaSierraPapa for the SharpDevelop Team (for details please see \doc\copyright.txt)
+// This code is distributed under the GNU LGPL (for details please see \doc\license.txt)
+
+using System;
+using ICSharpCode.SharpDevelop.Dom;
+
+namespace ICSharpCode.TextTemplating
+{
+	public class TextTemplatingReflectionProjectContent : IReflectionProjectContent
+	{
+		public TextTemplatingReflectionProjectContent(IProjectContent projectContent)
+			: this(projectContent as ReflectionProjectContent)
+		{
+		}
+		
+		TextTemplatingReflectionProjectContent(ReflectionProjectContent projectContent)
+		{
+			if (projectContent != null) {
+				AssemblyLocation = projectContent.AssemblyLocation;
+			}
+		}
+		
+		public string AssemblyLocation { get; private set; }
+	}
+}
---
+++ src/AddIns/Misc/TextTemplating/Test/Helpers/FakeAssemblyParserService.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) AlphaSierraPapa for the SharpDevelop Team (for details please see \doc\copyright.txt)
+// This code is distributed under the GNU LGPL (for details please see \doc\license.txt)
+
+using System;
+using ICSharpCode.SharpDevelop.Project;
+using ICSharpCode.TextTemplating;
+
+namespace TextTemplating.Tests.Helpers
+{
+	public class FakeAssemblyParserService : IAssemblyParserService
+	{
+		public FakeReflectionProjectContent FakeReflectionProjectContent = new FakeReflectionProjectContent();
+		public ReferenceProjectItem ItemPassedToGetReflectionProjectContentForReference;
+		
+		public IReflectionProjectContent GetReflectionProjectContentForReference(ReferenceProjectItem item)
+		{
+			ItemPassedToGetReflectionProjectContentForReference = item;
+			return FakeReflectionProjectContent;
+		}
+	}
+}
