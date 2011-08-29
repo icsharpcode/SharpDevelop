d01a87961a9fa61acc6aaf7309f87c152fb4044e
Siegfried Pammer <siegfriedpammer@gmail.com>
2011-08-29 19:54:47 +0200

    implement SD-1855 - Add option for disabling ChangeMarkerMargin

--- src/AddIns/DisplayBindings/AvalonEdit.AddIn/Src/CodeEditor.cs
+++ src/AddIns/DisplayBindings/AvalonEdit.AddIn/Src/CodeEditor.cs
@@ -117,8 +117,9 @@ namespace ICSharpCode.AvalonEdit.AddIn
 					} else {
 						this.errorPainter.UpdateErrors();
 					}
-					changeWatcher.Initialize(this.DocumentAdapter);
-					
+					if (changeWatcher != null) {
+						changeWatcher.Initialize(this.DocumentAdapter);
+					}
 					FetchParseInformation();
 				}
 			}
@@ -145,8 +146,9 @@ namespace ICSharpCode.AvalonEdit.AddIn
 			
 			textMarkerService = new TextMarkerService(this);
 			iconBarManager = new IconBarManager();
-			changeWatcher = new DefaultChangeWatcher();
-			
+			if (CodeEditorOptions.Instance.EnableChangeMarkerMargin) {
+				changeWatcher = new DefaultChangeWatcher();
+			}
 			primaryTextEditor = CreateTextEditor();
 			primaryTextEditorAdapter = (CodeEditorAdapter)primaryTextEditor.TextArea.GetService(typeof(ITextEditor));
 			Debug.Assert(primaryTextEditorAdapter != null);
@@ -205,7 +207,9 @@ namespace ICSharpCode.AvalonEdit.AddIn
 			textView.Services.AddService(typeof(IBookmarkMargin), iconBarManager);
 			codeEditorView.TextArea.LeftMargins.Insert(0, new IconBarMargin(iconBarManager));
 			
-			codeEditorView.TextArea.LeftMargins.Add(new ChangeMarkerMargin(changeWatcher));
+			if (CodeEditorOptions.Instance.EnableChangeMarkerMargin) {
+				codeEditorView.TextArea.LeftMargins.Add(new ChangeMarkerMargin(changeWatcher));
+			}
 			
 			textView.Services.AddService(typeof(ISyntaxHighlighter), new AvalonEditSyntaxHighlighterAdapter(textView));
 			
