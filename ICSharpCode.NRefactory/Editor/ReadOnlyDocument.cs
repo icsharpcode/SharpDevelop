96195ac0ef9482af230fca95bacb61d02dd06805
Mike Krüger <mkrueger@xamarin.com>
2011-09-03 15:29:28 +0200

    fixed +-1 issue.

--- ICSharpCode.NRefactory/Editor/ReadOnlyDocument.cs
+++ ICSharpCode.NRefactory/Editor/ReadOnlyDocument.cs
@@ -151,10 +151,10 @@ namespace ICSharpCode.NRefactory.Editor
 			if (line < 1 || line > lines.Length)
 				throw new ArgumentOutOfRangeException("line", line, "Value must be between 1 and " + lines.Length);
 			int lineStart = GetStartOffset(line);
-			if (column <= 0)
+			if (column <= 1)
 				return lineStart;
 			int lineEnd = GetEndOffset(line);
-			if (column >= lineEnd - lineStart)
+			if (column - 1 >= lineEnd - lineStart)
 				return lineEnd;
 			return lineStart + column - 1;
 		}
