9f70a1fb76978dbceef91d258f86f32d16a5836b
Daniel Grunwald <daniel@danielgrunwald.de>
2011-09-18 12:47:42 +0200

    Fixed NullReferenceException in CodeManipulation.ExtendSelection.

--- src/AddIns/DisplayBindings/AvalonEdit.AddIn/Src/CodeManipulation.cs
+++ src/AddIns/DisplayBindings/AvalonEdit.AddIn/Src/CodeManipulation.cs
@@ -277,6 +277,8 @@ namespace ICSharpCode.AvalonEdit.AddIn
 		
 		static void SelectText(Selection selection, ITextEditor editor)
 		{
+			if (selection == null)
+				return;
 			int startOffset, endOffset;
 			try {
 				startOffset = editor.Document.PositionToOffset(selection.Start);
