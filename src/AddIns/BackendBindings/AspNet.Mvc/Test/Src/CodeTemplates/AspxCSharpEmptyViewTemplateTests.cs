901ca06928c6fa69332ced99e051c1268a4776c0
Matt Ward <ward.matt@gmail.com>
2011-11-10 19:26:41 +0000

    Add tests for code in C# Aspx and Razor empty view T4 templates.

---
+++ src/AddIns/BackendBindings/AspNet.Mvc/Test/Src/CodeTemplates/AspxCSharpEmptyViewTemplateTests.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) AlphaSierraPapa for the SharpDevelop Team (for details please see \doc\copyright.txt)
+// This code is distributed under the GNU LGPL (for details please see \doc\license.txt)
+
+using System;
+using AspNet.Mvc.Tests.Helpers;
+using ICSharpCode.AspNet.Mvc.AspxCSharp;
+using NUnit.Framework;
+
+namespace AspNet.Mvc.Tests.CodeTemplates
+{
+	[TestFixture]
+	public class AspxCSharpEmptyViewTemplateTests
+	{
+		Empty emptyViewTemplate;
+		TestableMvcTextTemplateHost mvcHost;
+		
+		void CreateEmptyViewTemplatePreprocessor()
+		{
+			mvcHost = new TestableMvcTextTemplateHost();
+			emptyViewTemplate = new Empty();
+			emptyViewTemplate.Host = mvcHost;
+		}
+		
+		[Test]
+		public void GetViewPageType_HostViewDataTypeNameIsMyAppMyModel_ReturnsMyAppMyModelSurroundedByAngleBrackets()
+		{
+			CreateEmptyViewTemplatePreprocessor();
+			mvcHost.ViewDataTypeName = "MyApp.MyModel";
+			
+			string viewPageType = emptyViewTemplate.GetViewPageType();
+			
+			Assert.AreEqual("<MyApp.MyModel>", viewPageType);
+		}
+		
+		[Test]
+		public void GetViewPageType_HostViewDataTypeNameIsNull_ReturnsEmptyString()
+		{
+			CreateEmptyViewTemplatePreprocessor();
+			mvcHost.ViewDataTypeName = null;
+			
+			string viewPageType = emptyViewTemplate.GetViewPageType();
+			
+			Assert.AreEqual(String.Empty, viewPageType);
+		}
+		
+		[Test]
+		public void GetViewPageType_HostViewDataTypeNameIsEmptyString_ReturnsEmptyString()
+		{
+			CreateEmptyViewTemplatePreprocessor();
+			mvcHost.ViewDataTypeName = String.Empty;
+			
+			string viewPageType = emptyViewTemplate.GetViewPageType();
+			
+			Assert.AreEqual(String.Empty, viewPageType);
+		}
+	}
+}
---
+++ src/AddIns/BackendBindings/AspNet.Mvc/Test/Src/CodeTemplates/RazorCSharpEmptyViewTemplateTests.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) AlphaSierraPapa for the SharpDevelop Team (for details please see \doc\copyright.txt)
+// This code is distributed under the GNU LGPL (for details please see \doc\license.txt)
+
+using System;
+using AspNet.Mvc.Tests.Helpers;
+using ICSharpCode.AspNet.Mvc.CSHtml;
+using NUnit.Framework;
+
+namespace AspNet.Mvc.Tests.CodeTemplates
+{
+	[TestFixture]
+	public class RazorCSharpEmptyViewTemplateTests
+	{
+		Empty emptyViewTemplate;
+		TestableMvcTextTemplateHost mvcHost;
+		
+		void CreateEmptyViewTemplatePreprocessor()
+		{
+			mvcHost = new TestableMvcTextTemplateHost();
+			emptyViewTemplate = new Empty();
+			emptyViewTemplate.Host = mvcHost;
+		}
+		
+		[Test]
+		public void GetModelDirective_HostViewDataTypeNameIsMyAppMyModel_ReturnsRazorModelFollowedByMyAppMyModel()
+		{
+			CreateEmptyViewTemplatePreprocessor();
+			mvcHost.ViewDataTypeName = "MyApp.MyModel";
+			
+			string modelDirective = emptyViewTemplate.GetModelDirective();
+			
+			Assert.AreEqual("@model MyApp.MyModel", modelDirective);
+		}
+		
+		[Test]
+		public void GetModelDirective_HostViewDataTypeNameIsNull_ReturnsEmptyString()
+		{
+			CreateEmptyViewTemplatePreprocessor();
+			mvcHost.ViewDataTypeName = null;
+			
+			string modelDirective = emptyViewTemplate.GetModelDirective();
+			
+			Assert.AreEqual(String.Empty, modelDirective);
+		}
+		
+		[Test]
+		public void GetModelDirective_HostViewDataTypeNameIsEmptyString_ReturnsEmptyString()
+		{
+			CreateEmptyViewTemplatePreprocessor();
+			mvcHost.ViewDataTypeName = String.Empty;
+			
+			string modelDirective = emptyViewTemplate.GetModelDirective();
+			
+			Assert.AreEqual(String.Empty, modelDirective);
+		}
+
+	}
+}
