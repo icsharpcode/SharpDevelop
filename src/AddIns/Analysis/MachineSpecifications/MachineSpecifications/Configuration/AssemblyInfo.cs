e7d03f328ea52745a7d57ec0ada900333a3fb132
Matt Ward <ward.matt@gmail.com>
2011-11-11 18:13:54 +0000

    MSpec addin assembly version number now uses common SharpDevelop version number.

--- src/AddIns/Analysis/MachineSpecifications/MachineSpecifications/Configuration/AssemblyInfo.cs
+++ src/AddIns/Analysis/MachineSpecifications/MachineSpecifications/Configuration/AssemblyInfo.cs
@@ -1,31 +1,16 @@
-﻿#region Using directives
+﻿// Copyright (c) AlphaSierraPapa for the SharpDevelop Team (for details please see \doc\copyright.txt)
+// This code is distributed under the GNU LGPL (for details please see \doc\license.txt)
 
-using System;
 using System.Reflection;
-using System.Runtime.InteropServices;
 
-#endregion
+// Information about this assembly is defined by the following
+// attributes.
+//
+// change them to the information which is associated with the assembly
+// you compile.
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("MachineSpecifications")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("MachineSpecifications AddIn for SharpDevelop")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MachineSpecifications")]
-[assembly: AssemblyCopyright("Copyright 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// This sets the default COM visibility of types in the assembly to invisible.
-// If you need to expose a type to COM, use [ComVisible(true)] on that type.
-[assembly: ComVisible(false)]
-
-// The assembly version has following format :
-//
-// Major.Minor.Build.Revision
-//
-// You can specify all the values or you can use the default the Revision and 
-// Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.*")]
