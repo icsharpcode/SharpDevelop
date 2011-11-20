1d7738ff6dc9b9f297c8cb59f56df0c37facbd17
Matt Ward <ward.matt@gmail.com>
2011-11-20 14:50:51 +0000

    Add basic List view T4 template for ASP.NET MVC.

---
+++ src/AddIns/BackendBindings/AspNet.Mvc/Test/Src/CodeTemplates/AspxCSharpListViewTemplateTests.cs
@@ -0,0 +1,172 @@
+﻿// Copyright (c) AlphaSierraPapa for the SharpDevelop Team (for details please see \doc\copyright.txt)
+// This code is distributed under the GNU LGPL (for details please see \doc\license.txt)
+
+using System;
+using AspNet.Mvc.Tests.CodeTemplates.Models;
+using AspNet.Mvc.Tests.Helpers;
+using AspxCSharp = ICSharpCode.AspNet.Mvc.AspxCSharp;
+using NUnit.Framework;
+
+namespace AspNet.Mvc.Tests.CodeTemplates
+{
+	[TestFixture]
+	public class AspxCSharpListViewTemplateTests
+	{
+		AspxCSharp.List templatePreprocessor;
+		TestableMvcTextTemplateHost mvcHost;
+		
+		void CreateViewTemplatePreprocessor()
+		{
+			mvcHost = new TestableMvcTextTemplateHost();
+			templatePreprocessor = new AspxCSharp.List();
+			templatePreprocessor.Host = mvcHost;
+		}
+		
+		[Test]
+		public void GetViewPageType_HostViewDataTypeNameIsMyAppMyModel_ReturnsMyAppMyModelSurroundedByAngleBrackets()
+		{
+			CreateViewTemplatePreprocessor();
+			mvcHost.ViewDataTypeName = "MyApp.MyModel";
+			
+			string viewPageType = templatePreprocessor.GetViewPageType();
+			
+			Assert.AreEqual("<MyApp.MyModel>", viewPageType);
+		}
+		
+		[Test]
+		public void GetViewPageType_HostViewDataTypeNameIsNull_ReturnsEmptyString()
+		{
+			CreateViewTemplatePreprocessor();
+			mvcHost.ViewDataTypeName = null;
+			
+			string viewPageType = templatePreprocessor.GetViewPageType();
+			
+			Assert.AreEqual(String.Empty, viewPageType);
+		}
+		
+		[Test]
+		public void GetViewPageType_HostViewDataTypeNameIsEmptyString_ReturnsEmptyString()
+		{
+			CreateViewTemplatePreprocessor();
+			mvcHost.ViewDataTypeName = String.Empty;
+			
+			string viewPageType = templatePreprocessor.GetViewPageType();
+			
+			Assert.AreEqual(String.Empty, viewPageType);
+		}
+		
+		[Test]
+		public void TransformText_ModelHasNoPropertiesAndNoMasterPage_ReturnsFullHtmlPageWithFormAndFieldSetForModel()
+		{
+			CreateViewTemplatePreprocessor();
+			Type modelType = typeof(ModelWithNoProperties);
+			mvcHost.ViewDataType = modelType;
+			mvcHost.ViewDataTypeName = modelType.FullName;
+			mvcHost.ViewName = "MyView";
+			
+			string output = templatePreprocessor.TransformText();
+		
+			string expectedOutput = 
+@"<%@ Page Language=""C#"" Inherits=""System.Web.Mvc.ViewPage<IEnumerable<AspNet.Mvc.Tests.CodeTemplates.Models.ModelWithNoProperties>>"" %>
+
+<!DOCTYPE html>
+<html>
+	<head runat=""server"">
+		<title>MyView</title>
+	</head>
+	<body>
+		<p>
+			<%: Html.ActionLink(""Create"", ""Create"") %>
+		</p>
+		<table>
+			<% foreach (var item in Model) { %>
+			<tr>
+				<td>
+					<%: Html.ActionLink(""Edit"", ""Edit"") %> |
+					<%: Html.ActionLink(""Details"", ""Details"") %> |
+					<%: Html.ActionLink(""Delete"", ""Delete"") %>
+				</td>
+			</tr>
+			<% } %>
+		</table>
+	</body>
+</html>
+";
+			Assert.AreEqual(expectedOutput, output);
+		}
+		
+		[Test]
+		public void TransformText_ModelHasNoPropertiesAndIsPartialView_ReturnsControlWithFormAndFieldSetForModel()
+		{
+			CreateViewTemplatePreprocessor();
+			mvcHost.IsPartialView = true;
+			Type modelType = typeof(ModelWithNoProperties);
+			mvcHost.ViewDataType = modelType;
+			mvcHost.ViewDataTypeName = modelType.FullName;
+			mvcHost.ViewName = "MyView";
+			
+			string output = templatePreprocessor.TransformText();
+		
+			string expectedOutput = 
+@"<%@ Control Language=""C#"" Inherits=""System.Web.Mvc.ViewUserControl<IEnumerable<AspNet.Mvc.Tests.CodeTemplates.Models.ModelWithNoProperties>>"" %>
+
+<p>
+	<%: Html.ActionLink(""Create"", ""Create"") %>
+</p>
+<table>
+	<% foreach (var item in Model) { %>
+	<tr>
+		<td>
+			<%: Html.ActionLink(""Edit"", ""Edit"") %> |
+			<%: Html.ActionLink(""Details"", ""Details"") %> |
+			<%: Html.ActionLink(""Delete"", ""Delete"") %>
+		</td>
+	</tr>
+	<% } %>
+</table>
+";
+			Assert.AreEqual(expectedOutput, output);
+		}
+		
+		[Test]
+		public void TransformText_ModelHasNoPropertiesAndIsContentPage_ReturnsContentPageWithFormAndFieldSetForModel()
+		{
+			CreateViewTemplatePreprocessor();
+			mvcHost.IsContentPage = true;
+			Type modelType = typeof(ModelWithNoProperties);
+			mvcHost.ViewDataType = modelType;
+			mvcHost.ViewDataTypeName = modelType.FullName;
+			mvcHost.ViewName = "MyView";
+			mvcHost.MasterPageFile = "~/Views/Shared/Site.master";
+			mvcHost.PrimaryContentPlaceHolderID = "Main";
+			
+			string output = templatePreprocessor.TransformText();
+		
+			string expectedOutput = 
+@"<%@ Page Language=""C#"" MasterPageFile=""~/Views/Shared/Site.master"" Inherits=""System.Web.Mvc.ViewPage<IEnumerable<AspNet.Mvc.Tests.CodeTemplates.Models.ModelWithNoProperties>>"" %>
+
+<asp:Content ID=""Content1"" ContentPlaceHolderID=""Title"" runat=""server"">
+MyView
+</asp:Content>
+
+<asp:Content ID=""Content2"" ContentPlaceHolderID=""Main"" runat=""server"">
+	<p>
+		<%: Html.ActionLink(""Create"", ""Create"") %>
+	</p>
+	<table>
+		<% foreach (var item in Model) { %>
+		<tr>
+			<td>
+				<%: Html.ActionLink(""Edit"", ""Edit"") %> |
+				<%: Html.ActionLink(""Details"", ""Details"") %> |
+				<%: Html.ActionLink(""Delete"", ""Delete"") %>
+			</td>
+		</tr>
+		<% } %>
+	</table>
+</asp:Content>
+";
+			Assert.AreEqual(expectedOutput, output);
+		}
+	}
+}
---
+++ src/AddIns/BackendBindings/AspNet.Mvc/Test/Src/CodeTemplates/RazorCSharpListViewTemplateTests.cs
@@ -0,0 +1,173 @@
+﻿// Copyright (c) AlphaSierraPapa for the SharpDevelop Team (for details please see \doc\copyright.txt)
+// This code is distributed under the GNU LGPL (for details please see \doc\license.txt)
+
+using System;
+using AspNet.Mvc.Tests.CodeTemplates.Models;
+using AspNet.Mvc.Tests.Helpers;
+using CSHtml = ICSharpCode.AspNet.Mvc.CSHtml;
+using NUnit.Framework;
+
+namespace AspNet.Mvc.Tests.CodeTemplates
+{
+	[TestFixture]
+	public class RazorCSharpListViewTemplateTests
+	{
+		CSHtml.List templatePreprocessor;
+		TestableMvcTextTemplateHost mvcHost;
+		
+		void CreateViewTemplatePreprocessor()
+		{
+			mvcHost = new TestableMvcTextTemplateHost();
+			templatePreprocessor = new CSHtml.List();
+			templatePreprocessor.Host = mvcHost;
+		}
+		
+		[Test]
+		public void GetModelDirective_HostViewDataTypeNameIsMyAppMyModel_ReturnsRazorModelFollowedByIEnumerableMyAppMyModel()
+		{
+			CreateViewTemplatePreprocessor();
+			mvcHost.ViewDataTypeName = "MyApp.MyModel";
+			
+			string modelDirective = templatePreprocessor.GetModelDirective();
+			
+			Assert.AreEqual("@model IEnumerable<MyApp.MyModel>", modelDirective);
+		}
+		
+		[Test]
+		public void GetModelDirective_HostViewDataTypeNameIsNull_ReturnsEmptyString()
+		{
+			CreateViewTemplatePreprocessor();
+			mvcHost.ViewDataTypeName = null;
+			
+			string modelDirective = templatePreprocessor.GetModelDirective();
+			
+			Assert.AreEqual(String.Empty, modelDirective);
+		}
+		
+		[Test]
+		public void GetModelDirective_HostViewDataTypeNameIsEmptyString_ReturnsEmptyString()
+		{
+			CreateViewTemplatePreprocessor();
+			mvcHost.ViewDataTypeName = String.Empty;
+			
+			string modelDirective = templatePreprocessor.GetModelDirective();
+			
+			Assert.AreEqual(String.Empty, modelDirective);
+		}
+		
+		[Test]
+		public void TransformText_ModelHasNoPropertiesAndNoMasterPage_ReturnsFullHtmlPageWithFormAndFieldSetForModel()
+		{
+			CreateViewTemplatePreprocessor();
+			Type modelType = typeof(ModelWithNoProperties);
+			mvcHost.ViewDataType = modelType;
+			mvcHost.ViewDataTypeName = modelType.FullName;
+			mvcHost.ViewName = "MyView";
+			
+			string output = templatePreprocessor.TransformText();
+		
+			string expectedOutput = 
+@"@model IEnumerable<AspNet.Mvc.Tests.CodeTemplates.Models.ModelWithNoProperties>
+
+<!DOCTYPE html>
+<html>
+	<head runat=""server"">
+		<title>MyView</title>
+	</head>
+	<body>
+		<p>
+			@Html.ActionLink(""Create"", ""Create"")
+		</p>
+		<table>
+			@foreach (var item in Model) {
+				<tr>
+					<td>
+						@Html.ActionLink(""Edit"", ""Edit"") |
+						@Html.ActionLink(""Details"", ""Details"") |
+						@Html.ActionLink(""Delete"", ""Delete"")
+					</td>
+				</tr>
+			}
+		</table>
+	</body>
+</html>
+";
+			Assert.AreEqual(expectedOutput, output);
+		}
+		
+		[Test]
+		public void TransformText_ModelHasNoPropertiesAndIsPartialView_ReturnsControlWithFormAndFieldSetForModel()
+		{
+			CreateViewTemplatePreprocessor();
+			mvcHost.IsPartialView = true;
+			Type modelType = typeof(ModelWithNoProperties);
+			mvcHost.ViewDataType = modelType;
+			mvcHost.ViewDataTypeName = modelType.FullName;
+			mvcHost.ViewName = "MyView";
+			
+			string output = templatePreprocessor.TransformText();
+		
+			string expectedOutput = 
+@"@model IEnumerable<AspNet.Mvc.Tests.CodeTemplates.Models.ModelWithNoProperties>
+
+<p>
+	@Html.ActionLink(""Create"", ""Create"")
+</p>
+<table>
+	@foreach (var item in Model) {
+		<tr>
+			<td>
+				@Html.ActionLink(""Edit"", ""Edit"") |
+				@Html.ActionLink(""Details"", ""Details"") |
+				@Html.ActionLink(""Delete"", ""Delete"")
+			</td>
+		</tr>
+	}
+</table>
+";
+			Assert.AreEqual(expectedOutput, output);
+		}
+		
+		[Test]
+		public void TransformText_ModelHasNoPropertiesAndIsContentPage_ReturnsContentPageWithFormAndFieldSetForModel()
+		{
+			CreateViewTemplatePreprocessor();
+			mvcHost.IsContentPage = true;
+			Type modelType = typeof(ModelWithNoProperties);
+			mvcHost.ViewDataType = modelType;
+			mvcHost.ViewDataTypeName = modelType.FullName;
+			mvcHost.ViewName = "MyView";
+			mvcHost.MasterPageFile = "~/Views/Shared/Site.master";
+			mvcHost.PrimaryContentPlaceHolderID = "Main";
+			
+			string output = templatePreprocessor.TransformText();
+		
+			string expectedOutput = 
+@"@model IEnumerable<AspNet.Mvc.Tests.CodeTemplates.Models.ModelWithNoProperties>
+
+@{
+	ViewBag.Title = ""MyView"";
+	Layout = ""~/Views/Shared/Site.master"";
+}
+
+<h2>MyView</h2>
+
+<p>
+	@Html.ActionLink(""Create"", ""Create"")
+</p>
+<table>
+	@foreach (var item in Model) {
+		<tr>
+			<td>
+				@Html.ActionLink(""Edit"", ""Edit"") |
+				@Html.ActionLink(""Details"", ""Details"") |
+				@Html.ActionLink(""Delete"", ""Delete"")
+			</td>
+		</tr>
+	}
+</table>
+";
+			Assert.AreEqual(expectedOutput, output);
+		}
+	}
+}
