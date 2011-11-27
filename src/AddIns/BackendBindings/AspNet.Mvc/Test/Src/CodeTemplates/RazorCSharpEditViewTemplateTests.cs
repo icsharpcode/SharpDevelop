eefdc40e50e37720ee60b3e76db6d46cc6568ca9
Matt Ward <ward.matt@gmail.com>
2011-11-27 12:04:24 +0000

    Generate code for model properties in ASP.NET MVC Edit view T4 templates.

--- src/AddIns/BackendBindings/AspNet.Mvc/Test/Src/CodeTemplates/RazorCSharpEditViewTemplateTests.cs
+++ src/AddIns/BackendBindings/AspNet.Mvc/Test/Src/CodeTemplates/RazorCSharpEditViewTemplateTests.cs
@@ -2,6 +2,9 @@
 // This code is distributed under the GNU LGPL (for details please see \doc\license.txt)
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
+
 using AspNet.Mvc.Tests.CodeTemplates.Models;
 using AspNet.Mvc.Tests.Helpers;
 using ICSharpCode.AspNet.Mvc.CSHtml;
@@ -20,6 +23,16 @@ namespace AspNet.Mvc.Tests.CodeTemplates
 			mvcHost = new TestableMvcTextTemplateHost();
 			templatePreprocessor = new Edit();
 			templatePreprocessor.Host = mvcHost;
+		}
+		
+		IEnumerable<Edit.ModelProperty> GetModelProperties()
+		{
+			return templatePreprocessor.GetModelProperties();
+		}
+		
+		Edit.ModelProperty GetFirstModelProperty()
+		{
+			return GetModelProperties().First();
 		}
 		
 		[Test]
@@ -154,6 +167,57 @@ namespace AspNet.Mvc.Tests.CodeTemplates
 	@Html.ValidationSummary(true)
 	<fieldset>
 		<legend>ModelWithNoProperties</legend>
+		
+		<p>
+			<input type=""submit"" value=""Save""/>
+		</p>
+	</fieldset>
+}
+<div>
+	@Html.ActionLink(""Back"", ""Index"")
+</div>
+";
+			Assert.AreEqual(expectedOutput, output);
+		}
+		
+		[Test]
+		public void GetModelProperties_ModelHasOnePropertyCalledName_ReturnsModelPropertyCalledName()
+		{
+			CreateViewTemplatePreprocessor();
+			mvcHost.ViewDataType = typeof(ModelWithOneProperty);
+			
+			Edit.ModelProperty modelProperty = GetFirstModelProperty();
+			
+			Assert.AreEqual("Name", modelProperty.Name);
+		}
+		
+		[Test]
+		public void TransformText_ModelHasOnePropertyAndIsPartialView_ReturnsControlWithHtmlHelpersForModelProperty()
+		{
+			CreateViewTemplatePreprocessor();
+			mvcHost.IsPartialView = true;
+			Type modelType = typeof(ModelWithOneProperty);
+			mvcHost.ViewDataType = modelType;
+			mvcHost.ViewDataTypeName = modelType.FullName;
+			mvcHost.ViewName = "MyView";
+			
+			string output = templatePreprocessor.TransformText();
+		
+			string expectedOutput = 
+@"@model AspNet.Mvc.Tests.CodeTemplates.Models.ModelWithOneProperty
+
+@using (Html.BeginForm()) {
+	@Html.ValidationSummary(true)
+	<fieldset>
+		<legend>ModelWithOneProperty</legend>
+		
+		<div class=""editor-label"">
+			@Html.LabelFor(model => model.Name)
+		</div>
+		<div class=""editor-field"">
+			@Html.EditorFor(model => model.Name)
+			@Html.ValidationMessageFor(model => model.Name)
+		</div>
 		
 		<p>
 			<input type=""submit"" value=""Save""/>
