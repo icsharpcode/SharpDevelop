42ce4ca6e10c6277540fda6136cc239353990c42
Daniel Grunwald <daniel@danielgrunwald.de>
2011-12-01 14:20:12 +0100

    Fixed type inference in foreach when the collection type does not implement IEnumerable.

--- ICSharpCode.NRefactory.CSharp/Resolver/ResolveVisitor.cs
+++ ICSharpCode.NRefactory.CSharp/Resolver/ResolveVisitor.cs
@@ -2424,7 +2424,7 @@ namespace ICSharpCode.NRefactory.CSharp.Resolver
 			if (IsVar(foreachStatement.VariableType)) {
 				if (navigator.Scan(foreachStatement.VariableType) == ResolveVisitorNavigationMode.Resolve) {
 					IType collectionType = Resolve(foreachStatement.InExpression).Type;
-					IType elementType = GetElementType(collectionType, resolver.Compilation, false);
+					IType elementType = GetElementTypeFromCollection(collectionType);
 					StoreCurrentState(foreachStatement.VariableType);
 					StoreResult(foreachStatement.VariableType, new TypeResolveResult(elementType));
 					v = MakeVariable(elementType, foreachStatement.VariableNameToken);
@@ -2581,7 +2581,7 @@ namespace ICSharpCode.NRefactory.CSharp.Resolver
 		{
 			if (resolverEnabled && resolver.CurrentMember != null) {
 				IType returnType = resolver.CurrentMember.ReturnType;
-				IType elementType = GetElementType(returnType, resolver.Compilation, true);
+				IType elementType = GetElementTypeFromIEnumerable(returnType, resolver.Compilation, true);
 				ResolveAndProcessConversion(yieldStatement.Expression, elementType);
 			} else {
 				Scan(yieldStatement.Expression);
@@ -2864,7 +2864,7 @@ namespace ICSharpCode.NRefactory.CSharp.Resolver
 										var result = visitor.Resolve(initializer).Type;
 										
 										if (isForEach) {
-											result = GetElementType(result, storedContext.Compilation, false);
+											result = visitor.GetElementTypeFromCollection(result);
 										}
 										this.resolvedType = result;
 									});
@@ -2936,10 +2936,31 @@ namespace ICSharpCode.NRefactory.CSharp.Resolver
 			}
 		}
 		 */
-		static IType GetElementType(IType result, ICompilation compilation, bool allowIEnumerator)
+		
+		IType GetElementTypeFromCollection(IType collectionType)
+		{
+			switch (collectionType.Kind) {
+				case TypeKind.Array:
+					return ((ArrayType)collectionType).ElementType;
+				case TypeKind.Dynamic:
+					return SpecialType.Dynamic;
+			}
+			var memberLookup = resolver.CreateMemberLookup();
+			var getEnumeratorMethodGroup = memberLookup.Lookup(new ResolveResult(collectionType), "GetEnumerator", EmptyList<IType>.Instance, true) as MethodGroupResolveResult;
+			if (getEnumeratorMethodGroup != null) {
+				var or = getEnumeratorMethodGroup.PerformOverloadResolution(resolver.Compilation, new ResolveResult[0]);
+				if (or.FoundApplicableCandidate && !or.IsAmbiguous && !or.BestCandidate.IsStatic && or.BestCandidate.IsPublic) {
+					IType enumeratorType = or.BestCandidate.ReturnType;
+					return memberLookup.Lookup(new ResolveResult(enumeratorType), "Current", EmptyList<IType>.Instance, false).Type;
+				}
+			}
+			return GetElementTypeFromIEnumerable(collectionType, resolver.Compilation, false);
+		}
+		
+		static IType GetElementTypeFromIEnumerable(IType collectionType, ICompilation compilation, bool allowIEnumerator)
 		{
 			bool foundNonGenericIEnumerable = false;
-			foreach (IType baseType in result.GetAllBaseTypes()) {
+			foreach (IType baseType in collectionType.GetAllBaseTypes()) {
 				ITypeDefinition baseTypeDef = baseType.GetDefinition();
 				if (baseTypeDef != null) {
 					KnownTypeCode typeCode = baseTypeDef.KnownTypeCode;
@@ -3124,7 +3145,7 @@ namespace ICSharpCode.NRefactory.CSharp.Resolver
 		{
 			// This assumes queries are only used on IEnumerable.
 			// We might want to look at the signature of a LINQ method (e.g. Select) instead.
-			return GetElementType(type, resolver.Compilation, false);
+			return GetElementTypeFromIEnumerable(type, resolver.Compilation, false);
 		}
 		
 		sealed class QueryExpressionLambda : LambdaResolveResult
