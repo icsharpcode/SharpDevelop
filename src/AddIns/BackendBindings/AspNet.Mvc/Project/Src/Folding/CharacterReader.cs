89c2621bf66c330dc3f5f025accef543f6fa59ee
Matt Ward <ward.matt@gmail.com>
2011-12-07 18:29:55 +0000

    Add simple ASP.NET web forms fold parser.

---
+++ src/AddIns/BackendBindings/AspNet.Mvc/Project/Src/Folding/CharacterReader.cs
@@ -0,0 +1,85 @@
+﻿// Copyright (c) AlphaSierraPapa for the SharpDevelop Team (for details please see \doc\copyright.txt)
+// This code is distributed under the GNU LGPL (for details please see \doc\license.txt)
+
+using System;
+using System.IO;
+
+namespace ICSharpCode.AspNet.Mvc.Folding
+{
+	public class CharacterReader
+	{
+		public const int EndOfCharacters = -1;
+		
+		TextReader reader;
+		
+		public CharacterReader(string html)
+			: this(new StringReader(html))
+		{
+		}
+		
+		public CharacterReader(TextReader reader)
+		{
+			this.reader = reader;
+		}
+		
+		public bool Read()
+		{
+			CurrentCharacter = reader.Read();
+			Offset++;
+			return HasMoreCharactersToRead();
+		}
+		
+		bool HasMoreCharactersToRead()
+		{
+			return CurrentCharacter != EndOfCharacters;
+		}
+		
+		public int Offset { get; private set; }
+		
+		public int PreviousOffset {
+			get { return Offset - 1; }
+		}
+		
+		public int CurrentCharacter { get; private set; }
+		
+		public bool IsLetterOrDigit()
+		{
+			return Char.IsLetterOrDigit((char)CurrentCharacter);
+		}
+		
+		public bool IsLessThanSign()
+		{
+			return CurrentCharacter == '<';
+		}
+		
+		public bool IsGreaterThanSign()
+		{
+			return CurrentCharacter == '>';
+		}
+		
+		public bool IsForwardSlash()
+		{
+			return CurrentCharacter == '/';
+		}
+		
+		public bool IsNextCharacterForwardSlash()
+		{
+			return reader.Peek() == '/';
+		}
+		
+		public bool IsSpace()
+		{
+			return CurrentCharacter == ' ';
+		}
+		
+		public bool IsDoubleQuote()
+		{
+			return CurrentCharacter == '\"';
+		}
+		
+		public bool IsSingleQuote()
+		{
+			return CurrentCharacter == '\'';
+		}
+	}
+}
