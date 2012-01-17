17c5871d5f15ce69890b66cb8fae1621fc9ef6a1
Siegfried Pammer <siegfriedpammer@gmail.com>
2012-01-17 22:55:49 +0100

    initial commit of new CodeQuality.csproj

---
+++ src/AddIns/Analysis/CodeQuality/Gui/AnalyzeCodeQualityViewContent.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) AlphaSierraPapa for the SharpDevelop Team (for details please see \doc\copyright.txt)
+// This code is distributed under the GNU LGPL (for details please see \doc\license.txt)
+
+using System;
+using ICSharpCode.SharpDevelop.Gui;
+
+namespace ICSharpCode.CodeQuality.Gui
+{
+	/// <summary>
+	/// Description of AnalyzeCodeQualityViewContent.
+	/// </summary>
+	public class AnalyzeCodeQualityViewContent : AbstractViewContent
+	{
+		MainView view;
+		
+		public AnalyzeCodeQualityViewContent()
+		{
+			this.view = new MainView();
+			SetLocalizedTitle("Code Quality Analyzer");
+		}
+		
+		public override object Control {
+			get { return view; }
+		}
+	}
+}
