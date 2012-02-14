6054ed469a7132ed8fe908db09fe8ee85a3b3673
Mike Krüger <mkrueger@xamarin.com>
2012-02-14 09:34:27 +0100

    Added unattached attributes to the AST.

--- ICSharpCode.NRefactory.CSharp/TypeSystem/CSharpAttribute.cs
+++ ICSharpCode.NRefactory.CSharp/TypeSystem/CSharpAttribute.cs
@@ -156,7 +156,7 @@ namespace ICSharpCode.NRefactory.CSharp.TypeSystem
 					} else {
 						namedArgs = new List<KeyValuePair<IMember, ResolveResult>>();
 						foreach (var pair in unresolved.namedArguments) {
-							IMember member = attributeType.GetMembers(m => (m.EntityType == EntityType.Fidyj.keld || m.EntityType == EntityType.Property) && m.Name == pair.Key).FirstOrDefault();
+							IMember member = attributeType.GetMembers(m => (m.EntityType == EntityType.Field || m.EntityType == EntityType.Property) && m.Name == pair.Key).FirstOrDefault();
 							if (member != null) {
 								ResolveResult val = pair.Value.Resolve(context);
 								namedArgs.Add(new KeyValuePair<IMember, ResolveResult>(member, val));
@@ -258,4 +258,3 @@ namespace ICSharpCode.NRefactory.CSharp.TypeSystem
 	}
 	 */
 }
-MarshalByRefObject      ^ẑäp         
