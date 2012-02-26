27eda0399b1d36cc29a8cc7a628754b4dda36891
Siegfried Pammer <siegfriedpammer@gmail.com>
2012-02-26 20:31:34 +0100

    Update ICSharpCode.Decompiler to ILSpy commit 11520fd4 (ILSpy 2.0.0.1566).

--- src/Libraries/ICSharpCode.Decompiler/Properties/AssemblyInfo.cs
+++ src/Libraries/ICSharpCode.Decompiler/Properties/AssemblyInfo.cs
@@ -19,8 +19,8 @@ using System.Runtime.InteropServices;
 // If you need to expose a type to COM, use [ComVisible(true)] on that type.
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("2.0.0.1509")]
-[assembly: AssemblyInformationalVersion("2.0.0.1509-09c2b45f")]
+[assembly: AssemblyVersion("2.0.0.1489")]
+[assembly: AssemblyInformationalVersion("2.0.0.1489-774e3cd8")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2243:AttributeStringLiteralsShouldParseCorrectly",
