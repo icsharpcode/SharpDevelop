4575266b1db821fa0ccca296160cbbd1ed8765f7
mike <@192.168.0.102>
2012-03-01 07:06:43 +0100

    Filter out members that are not suited for code completion.

--- ICSharpCode.NRefactory.CSharp/Completion/CSharpCompletionEngine.cs
+++ ICSharpCode.NRefactory.CSharp/Completion/CSharpCompletionEngine.cs
@@ -66,7 +66,7 @@ namespace ICSharpCode.NRefactory.CSharp.Completion
 			this.document = document;
 			this.factory = factory;
 			// Set defaults for additional input properties
-			this.FormattingPolicy = new CSharpFormattingOptions();
+			this.FormattingPolicy = new CSharpFormattingOptions ();
 			this.EolMarker = Environment.NewLine;
 			this.IndentString = "\t";
 		}
@@ -1911,6 +1911,8 @@ namespace ICSharpCode.NRefactory.CSharp.Completion
 				
 				var filteredList = new List<IMember> ();
 				foreach (var member in type.GetMembers ()) {
+					if (member.EntityType == EntityType.Indexer || member.EntityType == EntityType.Operator || member.EntityType == EntityType.Constructor || member.EntityType == EntityType.Destructor)
+						continue;
 //					Console.WriteLine ("member:" + member + member.IsShadowing);
 					if (!lookup.IsAccessible (member, isProtectedAllowed)) {
 //						Console.WriteLine ("skip access: " + member.FullName);
