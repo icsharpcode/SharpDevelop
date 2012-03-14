2c97bfacca1e357a3667a98d658ca30c80d83664
Mike Krüger <mkrueger@xamarin.com>
2012-03-14 16:21:35 +0100

    Added more location checks / added -verbose command line option for the ast verifier.

--- ICSharpCode.NRefactory.CSharp.AstVerifier/Main.cs
+++ ICSharpCode.NRefactory.CSharp.AstVerifier/Main.cs
@@ -20,10 +20,8 @@ namespace ICSharpCode.NRefactory.CSharp.AstVerifier
 					j++;
 					continue;
 				}
-				if (c1 != c2) {
-					Console.WriteLine ( i + ":"+ c1 + "!= " +j +":" + c2);
+				if (c1 != c2)
 					return false;
-				}
 				i++;
 				j++;
 			}
@@ -40,11 +38,14 @@ namespace ICSharpCode.NRefactory.CSharp.AstVerifier
 		public static void Main (string[] args)
 		{
 			if (args.Length == 0) {
-				Console.WriteLine ("Usage: AstVerifier [Directory]");
+				Console.WriteLine ("Usage: AstVerifier [-v|-verbose] [Directory]");
 				return;
 			}
+			string directory = args[args.Length - 1];
+			bool verboseOutput =  args.Length > 1 && (args[0] == "-v" || args[0] == "-verbose");
+
 			try {
-				if (!Directory.Exists (args[0])) {
+				if (!Directory.Exists (directory)) {
 					Console.WriteLine ("Directory not found.");
 					return;
 				}
@@ -53,8 +54,8 @@ namespace ICSharpCode.NRefactory.CSharp.AstVerifier
 				return;
 			}
 			int failed = 0, passed = 0;
-			Console.WriteLine ("search in " + args [0]);
-			foreach (var file in Directory.GetFileSystemEntries (args[0], "*", SearchOption.AllDirectories)) {
+			Console.WriteLine ("search in " + directory);
+			foreach (var file in Directory.GetFileSystemEntries (directory, "*", SearchOption.AllDirectories)) {
 				if (!file.EndsWith (".cs"))
 					continue;
 				string text = File.ReadAllText (file);
@@ -64,11 +65,11 @@ namespace ICSharpCode.NRefactory.CSharp.AstVerifier
 				string generated = unit.GetText ();
 				int i, j;
 				if (!IsMatch (text, generated, out i, out j)) {
-					if (i > 0 && j > 0) {
+					if (i > 0 && j > 0 && verboseOutput) {
 						Console.WriteLine ("fail :" + file + "----original:");
-						Console.WriteLine (text.Substring (0, i + 1));
+						Console.WriteLine (text.Substring (0, Math.Min (text.Length, i + 1)));
 						Console.WriteLine ("----generated:");
-						Console.WriteLine (generated.Substring (0, j + 1));
+						Console.WriteLine (generated.Substring (0, Math.Min (generated.Length, j + 1)));
 					}
 					failed++;
 				} else {
