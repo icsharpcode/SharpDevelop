9ec0d68db261ff56821bb6625b76f500738b3e80
Mike Krüger <mkrueger@xamarin.com>
2012-03-26 18:36:26 +0200

    Added create indexer action.

--- ICSharpCode.NRefactory.CSharp/Refactoring/CodeActions/CreateMethodDeclarationAction.cs
+++ ICSharpCode.NRefactory.CSharp/Refactoring/CodeActions/CreateMethodDeclarationAction.cs
@@ -146,7 +146,7 @@ namespace ICSharpCode.NRefactory.CSharp.Refactoring
 						new ThrowStatement(new ObjectCreateExpression(context.CreateShortType("System", "NotImplementedException")))
 					}
 				};
-				foreach (var parameter in GenerateParameters (context, invocation)) {
+				foreach (var parameter in GenerateParameters (context, invocation.Arguments)) {
 					decl.Parameters.Add(parameter);
 				}
 				if (isStatic) {
@@ -170,10 +170,10 @@ namespace ICSharpCode.NRefactory.CSharp.Refactoring
 			});
 		}
 
-		public IEnumerable<ParameterDeclaration> GenerateParameters(RefactoringContext context, InvocationExpression invocation)
+		public static IEnumerable<ParameterDeclaration> GenerateParameters(RefactoringContext context, IEnumerable<Expression> arguments)
 		{
 			Dictionary<string, int> nameCounter = new Dictionary<string, int>();
-			foreach (var argument in invocation.Arguments) {
+			foreach (var argument in arguments) {
 				ParameterModifier direction = ParameterModifier.None;
 				AstNode node;
 				if (argument is DirectionExpression) {
