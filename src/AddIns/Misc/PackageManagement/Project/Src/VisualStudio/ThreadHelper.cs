d300a536d58927aa15708b0ff88af741ec0ee6ef
Matt Ward <ward.matt@gmail.com>
2012-05-28 20:31:46 +0100

    Use Workbench safe thread call in Visual Studio ThreadHelper.

--- src/AddIns/Misc/PackageManagement/Project/Src/VisualStudio/ThreadHelper.cs
+++ src/AddIns/Misc/PackageManagement/Project/Src/VisualStudio/ThreadHelper.cs
@@ -2,6 +2,7 @@
 // This code is distributed under the GNU LGPL (for details please see \doc\license.txt)
 
 using System;
+using ICSharpCode.SharpDevelop.Gui;
 
 namespace Microsoft.VisualStudio.Shell
 {
@@ -19,7 +20,7 @@ namespace Microsoft.VisualStudio.Shell
 		
 		public T Invoke<T>(Func<T> method)
 		{
-			return method();
+			return WorkbenchSingleton.SafeThreadFunction<T>(method);
 		}
 	}
 }
