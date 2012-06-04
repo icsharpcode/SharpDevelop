e1ce528d586ac1cd256d4cad99f314e36075e6e9
PeterForstmeier <peter.forstmeier@t-online.de>
2012-06-04 20:50:41 +0200

    Signing

---
+++ src/Main/Base/Project/Src/Gui/Dialogs/OptionPanels/ProjectOptions/CreateKeyXaml.xaml.cs
@@ -0,0 +1,170 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Peter Forstmeier
+ * Date: 01.06.2012
+ * Time: 20:20
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+using ICSharpCode.Core;
+
+namespace ICSharpCode.SharpDevelop.Gui.OptionPanels
+{
+	/// <summary>
+	/// Interaction logic for CreateKeyXaml.xaml
+	/// </summary>
+	public partial class CreateKeyXaml : Window,INotifyPropertyChanged
+	{
+		
+		private bool checkBoxChecked;
+		private string keyFile;
+		private string baseDirectory;
+		
+		public CreateKeyXaml()
+		{
+			InitializeComponent();
+			DataContext = this;
+		}
+		
+		public CreateKeyXaml (string baseDirectory):this()
+		{
+			this.baseDirectory = baseDirectory;
+		}
+		
+		
+		public bool CheckBoxChecked {
+			get { return checkBoxChecked; }
+			set { checkBoxChecked = value;
+				OnPropertyChange("CheckBoxChecked");
+			}
+		}
+		
+		
+		public string KeyFile {
+			get { return keyFile; }
+			set { keyFile = value;
+				OnPropertyChange("KeyFile");			}
+		}
+		
+		/// <summary>
+        /// Gets the path of the "strong named" executable. This is used to create keys for strongly signing
+        /// .NET assemblies.
+        /// </summary>
+		public static string StrongNameTool {
+			get {
+        		return FileUtility.GetSdkPath("sn.exe");
+			}
+		}
+        
+        
+		public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+		
+		private void OnPropertyChange (string propertyName)
+		{
+			if (PropertyChanged != null) {
+				PropertyChanged(this, new System.ComponentModel.PropertyChangedEventArgs(propertyName));
+			}
+		}
+		
+		void okButtonClick(object sender, RoutedEventArgs e)
+		{
+			KeyFile = KeyFile.Trim();
+			if (KeyFile.Length == 0) {
+				MessageService.ShowMessage("${res:Dialog.ProjectOptions.Signing.EnterKeyName}");
+				return;
+			}
+			
+			if (CheckBoxChecked) {
+				if (!CheckPassword(this.passwordTextBox.Text.Trim(),this.confirmPasswordTextBox.Text.Trim()))
+				{
+					return;
+				}
+				MessageService.ShowMessage("Creating a key file with a password is currently not supported.");
+				return;
+			}
+			if (!KeyFile.EndsWith(".snk") && !KeyFile.EndsWith(".pfx"))
+				KeyFile += ".snk";
+			if (CreateKey(Path.Combine(baseDirectory, KeyFile))) {
+				this.DialogResult = true;
+				Close();
+			}
+		}
+		
+		 /// <summary>
+        /// Creates a key with the sn.exe utility.
+        /// </summary>
+        /// <param name="keyPath">The path of the key to create.</param>
+        /// <returns>True if the key was created correctly.</returns>
+		private static bool CreateKey(string keyPath)
+		{
+			if (File.Exists(keyPath)) {
+				string question = "${res:ICSharpCode.SharpDevelop.Internal.Templates.ProjectDescriptor.OverwriteQuestion}";
+				question = StringParser.Parse(question, new StringTagPair("fileNames", keyPath));
+				if (!MessageService.AskQuestion(question, "${res:ICSharpCode.SharpDevelop.Internal.Templates.ProjectDescriptor.OverwriteQuestion.InfoName}")) {
+					return false;
+				}
+			}
+			Process p = Process.Start(StrongNameTool, "-k \"" + keyPath + "\"");
+			p.WaitForExit();
+			if (p.ExitCode != 0) {
+				MessageService.ShowMessage("${res:Dialog.ProjectOptions.Signing.ErrorCreatingKey}");
+				return false;
+			}
+			return true;
+		}
+		
+		
+		public static bool CheckPassword(string password, string confirm)
+		{
+			
+//			if (password.Text.Length < 6) {
+//				MessageService.ShowMessage("${res:Dialog.ProjectOptions.Signing.PasswordTooShort}");
+//				password.Focus();
+//				return false;
+//			}
+//			if (password.Text != confirm.Text) {
+//				MessageService.ShowMessage("${res:Dialog.ProjectOptions.Signing.PasswordsDontMatch}");
+//				return false;
+//			}
+			return true;
+		}
+		
+		
+//		private static bool CheckPassword(Control password, Control confirm)
+//		{
+//			password.Text = password.Text.Trim();
+//			confirm.Text = confirm.Text.Trim();
+//			if (password.Text.Length < 6) {
+//				MessageService.ShowMessage("${res:Dialog.ProjectOptions.Signing.PasswordTooShort}");
+//				password.Focus();
+//				return false;
+//			}
+//			if (password.Text != confirm.Text) {
+//				MessageService.ShowMessage("${res:Dialog.ProjectOptions.Signing.PasswordsDontMatch}");
+//				return false;
+//			}
+//			return true;
+//		}
+		
+		
+		void cancelButtonClick(object sender, RoutedEventArgs e)
+		{
+			this.DialogResult = false;
+			Close();
+		}
+		
+		
+	}
+}
--- src/Main/Base/Project/Src/Gui/Dialogs/OptionPanels/ProjectOptions/SigningXaml.xaml.cs
+++ src/Main/Base/Project/Src/Gui/Dialogs/OptionPanels/ProjectOptions/SigningXaml.xaml.cs
@@ -8,14 +8,17 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+
 using System.Windows.Input;
 using System.Windows.Media;
 
+using ICSharpCode.Core;
 using ICSharpCode.SharpDevelop.Project;
 using ICSharpCode.SharpDevelop.Widgets;
 
@@ -26,13 +29,29 @@ namespace ICSharpCode.SharpDevelop.Gui.OptionPanels
 	/// </summary>
 	public partial class SigningXaml : ProjectOptionPanel
 	{
-//		private const string KeyFileExtensions = "*.snk;*.pfx;*.key";
-	//	private MSBuildBasedProject project;
-	
+		private const string KeyFileExtensions = "*.snk;*.pfx;*.key";
+		private List<string> keyFile = new List<string>();
+		private MSBuildBasedProject project;
+		
+		
 		public SigningXaml()
 		{
 			InitializeComponent();
-
+		}
+		
+		
+		private void Initialize()
+		{
+			FindKeys(base.BaseDirectory);
+			SelectedKey = AssemblyOriginatorKeyFile.Value;
+			if (SelectedKey != null) {
+				if (!KeyFile.Contains(SelectedKey)) {
+					keyFile.Add(SelectedKey);
+				}
+			}
+			
+			keyFile.Add(StringParser.Parse("<${res:Global.CreateButtonText}...>"));
+			keyFile.Add(StringParser.Parse("<${res:Global.BrowseText}...>"));
 		}
 		
 		public ProjectProperty<String> SignAssembly {
@@ -44,5 +63,115 @@ namespace ICSharpCode.SharpDevelop.Gui.OptionPanels
 			get { return GetProperty("AssemblyOriginatorKeyFile","", TextBoxEditMode.EditEvaluatedProperty); }
 		}
 		
+		
+		public ProjectProperty<string> DelaySign {
+			get { return GetProperty("DelaySign","", TextBoxEditMode.EditEvaluatedProperty); }
+		}
+		
+		
+		public ProjectProperty<string> AssemblyOriginatorKeyMode {
+			get { return GetProperty("AssemblyOriginatorKeyMode","", TextBoxEditMode.EditEvaluatedProperty); }
+		}
+		
+		#region overrides
+		
+		protected override void Load(MSBuildBasedProject project, string configuration, string platform)
+		{
+			base.Load(project, configuration, platform);
+			this.project = project;
+			Initialize();
+		}
+		
+		protected override bool Save(MSBuildBasedProject project, string configuration, string platform)
+		{
+			if (signAssemblyCheckBox.IsChecked) {
+				this.AssemblyOriginatorKeyFile.Value = "File";
+			}
+			return base.Save(project, configuration, platform);
+		}
+		#endregion
+		
+		
+		#region keyFile
+		
+		public List<string> KeyFile {
+			get { return keyFile; }
+			set {
+				keyFile = value;
+				base.RaisePropertyChanged(() => KeyFile);
+			}
+		}
+		
+		public string SelectedKey {get;set;}
+		
+		void FindKeys(string directory)
+		{
+			directory = FileUtility.NormalizePath(directory);
+			while (true) {
+				try {
+//					var files = from file in new DirectoryInfo(@"C:\").GetFiles()
+//            where file..Name.StartsWith("_")
+//            select file;
+
+					foreach (string fileName in Directory.GetFiles(directory, "*.snk")) {
+						keyFile.Add(MSBuildInternals.Escape(FileUtility.GetRelativePath(base.BaseDirectory, fileName)));
+					}
+					foreach (string fileName in Directory.GetFiles(directory, "*.pfx")) {
+						keyFile.Add(MSBuildInternals.Escape(FileUtility.GetRelativePath(base.BaseDirectory, fileName)));
+					}
+					foreach (string fileName in Directory.GetFiles(directory, "*.key")) {
+						keyFile.Add(MSBuildInternals.Escape(FileUtility.GetRelativePath(base.BaseDirectory, fileName)));
+					}
+				} catch {
+					// can happen for networked drives / network locations
+					break;
+				}
+				int pos = directory.LastIndexOf(Path.DirectorySeparatorChar);
+				if (pos < 0)
+					break;
+				directory = directory.Substring(0, pos);
+			}
+		}
+		
+		void KeyFileComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			var cbo = (ComboBox) sender;
+			
+			if (cbo.SelectedIndex == keyFile.Count - 1) {
+				BrowseKeyFile();
+			}
+			if (cbo.SelectedIndex == keyFile.Count - 2) {
+				CreateKeyFile();
+			}
+		}
+		
+		
+		void BrowseKeyFile()
+		{
+			string str = OptionsHelper.OpenFile("${res:SharpDevelop.FileFilter.KeyFiles} (" + KeyFileExtensions + ")|" + KeyFileExtensions + "|${res:SharpDevelop.FileFilter.AllFiles}|*.*");
+			if (!String.IsNullOrEmpty(str)) {
+				this.AssemblyOriginatorKeyFile.Value = str;
+			}                                                        
+		}
+		
+		
+		private void CreateKeyFile()
+		{
+			if (File.Exists(CreateKeyXaml.StrongNameTool)) {
+				CreateKeyXaml createKey = new CreateKeyXaml(base.BaseDirectory);
+				createKey.KeyFile = project.Name;
+				createKey.ShowDialog();
+				if (createKey.DialogResult.HasValue && createKey.DialogResult.Value)
+				{
+					SelectedKey = MSBuildInternals.Escape(createKey.KeyFile);
+				} else{
+					SelectedKey = String.Empty;
+				}
+			} else {
+				MessageService.ShowMessage("${res:Dialog.ProjectOptions.Signing.SNnotFound}");
+			}
+		}
+		
+		#endregion	
 	}
 }
