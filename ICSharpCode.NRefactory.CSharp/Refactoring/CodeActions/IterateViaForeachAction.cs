f99663b90e58fb1cb89d85c446fe7ade017c8a02
Simon Lindgren <simon.n.lindgren@gmail.com>
2012-06-11 20:00:09 +0200

    [CodeActions] Add IterateViaForeachAction.

---
+++ ICSharpCode.NRefactory.CSharp/Refactoring/CodeActions/IterateViaForeachAction.cs
@@ -0,0 +1,105 @@
+//
+// IterateViaForeachTests.cs
+//
+// Author:
+//       Simon Lindgren <simon.n.lindgren@gmail.com>
+//
+// Copyright (c) 2012 Simon Lindgren
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using ICSharpCode.NRefactory.CSharp.Refactoring;
+using ICSharpCode.NRefactory.TypeSystem;
+using System.Collections.Generic;
+using ICSharpCode.NRefactory.Semantics;
+
+namespace ICSharpCode.NRefactory.CSharp.Refactoring
+{
+	[ContextAction("Iterate via foreach", Description = "Iterates an IEnumerable with a foreach loop.")]
+	public class IterateViaForeachAction : ICodeActionProvider
+	{
+		#region ICodeActionProvider implementation
+
+		public IEnumerable<CodeAction> GetActions(RefactoringContext context)
+		{
+			CodeAction action;
+			action = ActionFromVariableInitializer(context);
+			if (action != null)
+				yield return action;
+			action = ActionFromExpressionStatement(context);
+			if (action != null)
+				yield return action;
+		}
+
+		CodeAction ActionFromVariableInitializer(RefactoringContext context)
+		{
+			var initializer = context.GetNode<VariableInitializer>();
+			if (initializer == null)
+				return null;
+			var initializerRR = context.Resolve(initializer) as LocalResolveResult;
+			if (!ImplementsInterface(initializerRR.Variable.Type, "System.Collections.IEnumerable"))
+				return null;
+			return new CodeAction(context.TranslateString("Iterate via foreach"), script => {
+				var iterator = MakeForeach(new IdentifierExpression(initializer.Name), context);
+				script.InsertBefore(context.GetNode<Statement>().NextSibling, iterator);
+				// Work around incorrect formatting when inserting relatively to
+				// a node on a different indentation level
+				script.FormatText(context.GetNode<BlockStatement>());
+			});
+		}
+
+		CodeAction ActionFromExpressionStatement(RefactoringContext context)
+		{
+			var expressionStatement = context.GetNode<ExpressionStatement>();
+			if (expressionStatement == null)
+				return null;
+			var expressionRR = context.Resolve(expressionStatement.Expression) as ResolveResult;
+			if (!ImplementsInterface(expressionRR.Type, "System.Collections.IEnumerable"))
+				return null;
+			return new CodeAction(context.TranslateString("Iterate via foreach"), script => {
+				var iterator = MakeForeach(expressionStatement.Expression.Clone(), context);
+				script.Replace(expressionStatement, iterator);
+			});
+
+		}
+
+		static AstNode MakeForeach(Expression node, RefactoringContext context)
+		{
+			return new ForeachStatement() {
+				VariableType = new SimpleType("var"),
+				VariableName = "item",
+				InExpression = node,
+				EmbeddedStatement = new BlockStatement()
+			};
+		}
+
+		bool ImplementsInterface(IType type, string typeName)
+		{
+			if (type.FullName == typeName)
+				return true;
+			foreach (var baseType in type.DirectBaseTypes) {
+				if (ImplementsInterface(baseType, typeName))
+					return true;
+			}
+			return false;
+		}
+
+		#endregion
+	}
+}
+
---
+++ ICSharpCode.NRefactory.Tests/CSharp/CodeActions/IterateViaForeachTests.cs
@@ -0,0 +1,152 @@
+//
+// IterateViaForeachTests.cs
+//
+// Author:
+//       Simon Lindgren <simon.n.lindgren@gmail.com>
+//
+// Copyright (c) 2012 Simon Lindgren
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using NUnit.Framework;
+using ICSharpCode.NRefactory.CSharp.Refactoring;
+
+namespace ICSharpCode.NRefactory.CSharp.CodeActions
+{
+	[TestFixture]
+	public class IterateViaForeachTests : ContextActionTestBase
+	{
+		[Test]
+		public void HandlesNonGenericCase()
+		{
+			Test<IterateViaForeachAction>(@"
+using System.Collections;
+class TestClass
+{
+	public void F()
+	{
+		var $list = new ArrayList ();
+	}
+}", @"
+using System.Collections;
+class TestClass
+{
+	public void F()
+	{
+		var list = new ArrayList ();
+		foreach (var item in list) {
+		}
+	}
+}");
+		}
+
+		[Test]
+		public void HandlesExpressionStatement()
+		{
+			Test<IterateViaForeachAction>(@"
+using System.Collections.Generic;
+class TestClass
+{
+	public IEnumerable<int> GetInts()
+	{
+		return new int[] { };
+	}
+
+	public void F()
+	{
+		GetInts$();
+	}
+}", @"
+using System.Collections.Generic;
+class TestClass
+{
+	public IEnumerable<int> GetInts()
+	{
+		return new int[] { };
+	}
+
+	public void F()
+	{
+		foreach (var item in GetInts ()) {
+		}
+	}
+}");
+		}
+
+		[Test]
+		public void HandlesLinqExpressionAssignment()
+		{
+			Test<IterateViaForeachAction>(@"
+using System.Collections.Generic;
+using System.Linq;
+class TestClass
+{
+	public IEnumerable<int> GetInts()
+	{
+		return new int[] { };
+	}
+
+	public void F()
+	{
+		var $filteredInts = from item in GetInts ()
+							where item > 0
+							select item;
+	}
+}", @"
+using System.Collections.Generic;
+using System.Linq;
+class TestClass
+{
+	public IEnumerable<int> GetInts()
+	{
+		return new int[] { };
+	}
+
+	public void F()
+	{
+		var filteredInts = from item in GetInts ()
+							where item > 0
+							select item;
+		foreach (var item in filteredInts) {
+		}
+	}
+}");
+		}
+
+		[Test]
+		public void IgnoresExpressionsInForeachStatement()
+		{
+			TestWrongContext<IterateViaForeachAction>(@"
+using System.Collections.Generic;
+class TestClass
+{
+	public IEnumerable<int> GetInts()
+	{
+		return new int[] { };
+	}
+
+	public void F()
+	{
+		foreach (var item in $GetInts ()) {
+		}
+	}
+}");
+		}
+	}
+}
+
