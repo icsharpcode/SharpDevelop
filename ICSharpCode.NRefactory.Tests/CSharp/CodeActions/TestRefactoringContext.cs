97185cf062b3f22429cb95785daa462874d6b067
Simon Lindgren <simon.n.lindgren@gmail.com>
2012-06-14 12:00:32 +0200

    [Refactoring] Add optional parameter expectErrors = false to ContextActonTestBase.Test().

--- ICSharpCode.NRefactory.Tests/CSharp/CodeActions/TestRefactoringContext.cs
+++ ICSharpCode.NRefactory.Tests/CSharp/CodeActions/TestRefactoringContext.cs
@@ -225,7 +225,7 @@ namespace ICSharpCode.NRefactory.CSharp.CodeActions
 				return doc.Text;
 			}
 		}
-		public static TestRefactoringContext Create (string content)
+		public static TestRefactoringContext Create (string content, bool expectErrors = false)
 		{
 			int idx = content.IndexOf ("$");
 			if (idx >= 0)
@@ -242,13 +242,19 @@ namespace ICSharpCode.NRefactory.CSharp.CodeActions
 				selectionEnd = idx2 - 2;
 				idx = selectionEnd;
 			}
-			
-			var doc = new StringBuilderDocument (content);
-			var parser = new CSharpParser ();
-			var unit = parser.Parse (content, "program.cs");
-			foreach (var error in parser.Errors)
-				Console.WriteLine (error.Message);
-			Assert.IsFalse (parser.HasErrors, "File contains parsing errors.");
+
+			var doc = new StringBuilderDocument(content);
+			var parser = new CSharpParser();
+			var unit = parser.Parse(content, "program.cs");
+			if (!expectErrors) {
+				foreach (var error in parser.Errors) {
+					Console.WriteLine(error.Message);
+				}
+				Assert.IsFalse(parser.HasErrors, "The file contains unexpected parsing errors.");
+			} else {
+				Assert.IsTrue(parser.HasErrors, "Expected parsing errors, but the file doesn't contain any.");
+			}
+
 			unit.Freeze ();
 			var parsedFile = unit.ToTypeSystem ();
 			
