11b9fd1cbc91b0d2442621ac62f1d3aa629742e0
Mansheng Yang <lightyang0@gmail.com>
2012-08-15 21:38:10 +0800

    [CodeActions]ConvertLambdaBodyExpressionToStatementAction: removed unused usings

--- ICSharpCode.NRefactory.CSharp/Refactoring/CodeActions/ConvertLambdaBodyExpressionToStatementAction.cs
+++ ICSharpCode.NRefactory.CSharp/Refactoring/CodeActions/ConvertLambdaBodyExpressionToStatementAction.cs
@@ -24,10 +24,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-using System.Linq;
-using ICSharpCode.NRefactory.CSharp.Resolver;
-using ICSharpCode.NRefactory.TypeSystem;
-
 namespace ICSharpCode.NRefactory.CSharp.Refactoring
 {
 	[ContextAction ("Converts expression of lambda body to statement",
