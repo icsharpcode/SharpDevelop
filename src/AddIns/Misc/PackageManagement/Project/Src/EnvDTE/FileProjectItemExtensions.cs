a2a0f0424020f2649ce2d0c9876704e7a9010f76
Matt Ward <ward.matt@gmail.com>
2012-08-29 21:14:48 +0100

    Add new dependent files to project browser.
    Update the project browser with any dependent files added to the project by the T4MVC template when it is run.

--- src/AddIns/Misc/PackageManagement/Project/Src/EnvDTE/FileProjectItemExtensions.cs
+++ src/AddIns/Misc/PackageManagement/Project/Src/EnvDTE/FileProjectItemExtensions.cs
@@ -2,6 +2,8 @@
 // This code is distributed under the GNU LGPL (for details please see \doc\license.txt)
 
 using System;
+using System.IO;
+using ICSharpCode.Core;
 using ICSharpCode.SharpDevelop.Project;
 
 namespace ICSharpCode.PackageManagement.EnvDTE
@@ -16,6 +18,17 @@ namespace ICSharpCode.PackageManagement.EnvDTE
 		public static bool IsDependentUpon(this FileProjectItem projectItem, FileProjectItem otherProjectItem)
 		{
 			return projectItem.DependentUpon == otherProjectItem.Include;
+		}
+		
+		public static bool IsDependentUponFileName(this FileProjectItem projectItem, string fileName)
+		{
+			return FileUtility.IsEqualFileName(projectItem.GetDependentUponFileName(), fileName);
+		}
+		
+		public static string GetDependentUponFileName(this FileProjectItem projectItem)
+		{
+			string directory = Path.GetDirectoryName(projectItem.FileName);
+			return Path.Combine(directory, projectItem.DependentUpon);
 		}
 	}
 }
