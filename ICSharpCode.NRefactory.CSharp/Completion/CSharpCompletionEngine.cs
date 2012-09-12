02ff35d1870889d49db60c59b2d13d3c9c0306fc
mkrueger <>
2012-09-12 09:58:57 +0200

    [Completion] Cleaned up some code.

--- ICSharpCode.NRefactory.CSharp/Completion/CSharpCompletionEngine.cs
+++ ICSharpCode.NRefactory.CSharp/Completion/CSharpCompletionEngine.cs
@@ -1788,7 +1788,7 @@ namespace ICSharpCode.NRefactory.CSharp.Completion
 					if (hintType is ParameterizedType && hintType.TypeParameterCount == 1 && hintType.FullName == "System.Collections.Generic.IEnumerable") {
 						var arg = ((ParameterizedType)hintType).TypeArguments.FirstOrDefault();
 						if (arg.Kind != TypeKind.TypeParameter) {
-							var array = new ArrayTypeReference(arg.ToTypeReference(), 1).Resolve(ctx);
+							var array = new ArrayType (ctx.Compilation, arg, 1);
 							wrapper.AddType(array, amb.ConvertType(array));
 						}
 					}
