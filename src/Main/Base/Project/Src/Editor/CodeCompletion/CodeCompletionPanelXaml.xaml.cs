b51c9518d32dab24399b12eb8118609071b9899b
PeterForstmeier <peter.forstmeier@t-online.de>
2012-10-22 20:45:00 +0200

    missing Files

---
+++ src/Main/Base/Project/Src/Editor/CodeCompletion/CodeCompletionPanelXaml.xaml.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Peter Forstmeier
+ * Date: 22.10.2012
+ * Time: 20:11
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+using ICSharpCode.SharpDevelop.Gui;
+
+namespace ICSharpCode.SharpDevelop.Editor.CodeCompletion
+{
+	/// <summary>
+	/// Interaction logic for CodeCompletionPanelXaml.xaml
+	/// </summary>
+	public partial class CodeCompletionPanelXaml : OptionPanel
+	{
+		public CodeCompletionPanelXaml()
+		{
+			InitializeComponent();
+		}
+	}
+}
