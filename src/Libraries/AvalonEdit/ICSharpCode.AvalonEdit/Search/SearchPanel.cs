4f623afecbc88ca852c573df966c4e7442527dce
Daniel Grunwald <daniel@danielgrunwald.de>
2012-12-22 00:37:57 +0100

    Add XmlDoc for SearchOptionsChangedEventArgs

--- src/Libraries/AvalonEdit/ICSharpCode.AvalonEdit/Search/SearchPanel.cs
+++ src/Libraries/AvalonEdit/ICSharpCode.AvalonEdit/Search/SearchPanel.cs
@@ -391,7 +391,7 @@ namespace ICSharpCode.AvalonEdit.Search
 		public event EventHandler<SearchOptionsChangedEventArgs> SearchOptionsChanged;
 		
 		/// <summary>
-		/// Raises the <see cref="SearchOptionsChangend" /> event.
+		/// Raises the <see cref="SearchOptionsChanged" /> event.
 		/// </summary>
 		protected virtual void OnSearchOptionsChanged(SearchOptionsChangedEventArgs e)
 		{
@@ -401,13 +401,34 @@ namespace ICSharpCode.AvalonEdit.Search
 		}
 	}
 	
+	/// <summary>
+	/// EventArgs for <see cref="SearchPanel.SearchOptionsChanged"/> event.
+	/// </summary>
 	public class SearchOptionsChangedEventArgs : EventArgs
 	{
+		/// <summary>
+		/// Gets the search pattern.
+		/// </summary>
 		public string SearchPattern { get; private set; }
+		
+		/// <summary>
+		/// Gets whether the search pattern should be interpreted case-sensitive.
+		/// </summary>
 		public bool MatchCase { get; private set; }
+		
+		/// <summary>
+		/// Gets whether the search pattern should be interpreted as regular expression.
+		/// </summary>
 		public bool UseRegex { get; private set; }
+		
+		/// <summary>
+		/// Gets whether the search pattern should only match whole words.
+		/// </summary>
 		public bool WholeWords { get; private set; }
 		
+		/// <summary>
+		/// Creates a new SearchOptionsChangedEventArgs instance.
+		/// </summary>
 		public SearchOptionsChangedEventArgs(string searchPattern, bool matchCase, bool useRegex, bool wholeWords)
 		{
 			this.SearchPattern = searchPattern;
