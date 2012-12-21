372153ee9d9a2b18afda492c861bced6be665865
Siegfried Pammer <siegfriedpammer@gmail.com>
2012-12-21 18:17:15 +0100

    add missing files

---
+++ src/Tools/StringResourceTool/CookieAwareWebClient.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Daniel Grunwald
+ * Date: 08.10.2005
+ * Time: 19:47
+ */
+using System;
+using System.Net;
+
+namespace StringResourceTool
+{
+	public class CookieAwareWebClient : WebClient
+	{
+		CookieContainer container;
+		
+		public CookieAwareWebClient(CookieContainer container)
+		{
+			if (container == null)
+				throw new ArgumentNullException("container");
+			this.container = container;
+		}
+	
+		protected override WebRequest GetWebRequest(Uri address)
+		{
+			WebRequest request = base.GetWebRequest(address);
+			if (request is HttpWebRequest) {
+				(request as HttpWebRequest).CookieContainer = container;
+			}
+			return request;
+		}
+	}
+}
