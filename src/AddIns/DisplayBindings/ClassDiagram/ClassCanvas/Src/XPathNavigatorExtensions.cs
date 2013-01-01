d9028a73bc888f8fa5d82851b5e04069195c3837
Matt Ward <ward.matt@gmail.com>
2013-01-01 18:42:25 +0000

    Fix FormatException in Class Diagram addin.
    Visual Studio class diagrams do not store the Collapsed xml attribute when the item is not collapsed.

---
+++ src/AddIns/DisplayBindings/ClassDiagram/ClassCanvas/Src/XPathNavigatorExtensions.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) AlphaSierraPapa for the SharpDevelop Team (for details please see \doc\copyright.txt)
+// This code is distributed under the GNU LGPL (for details please see \doc\license.txt)
+
+using System;
+using System.Xml.XPath;
+
+namespace ClassDiagram
+{
+	public static class XPathNavigatorExtensions
+	{
+		public static bool GetBooleanAttribute(this XPathNavigator navigator, string name, string namespaceUri = "")
+		{
+			string attributeValue = navigator.GetAttribute(name, namespaceUri);
+			bool result = false;
+			if (bool.TryParse(attributeValue, out result)) {
+				return true;
+			}
+			return false;
+		}
+	}
+}
