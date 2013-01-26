ae7aec8d8c1d37b85806132f1412627a481f181e
Siegfried Pammer <siegfriedpammer@gmail.com>
2013-01-26 14:59:45 +0100

    move textmarker tooltip support to AddIn tree

--- src/AddIns/DisplayBindings/AvalonEdit.AddIn/Src/CodeEditorView.cs
+++ src/AddIns/DisplayBindings/AvalonEdit.AddIn/Src/CodeEditorView.cs
@@ -247,30 +247,6 @@ namespace ICSharpCode.AvalonEdit.AddIn
 				args.LogicalPosition = pos.Value.Location;
 			}
 			
-			if (args.InDocument) {
-				int offset = args.Editor.Document.GetOffset(args.LogicalPosition);
-				
-				FoldingManager foldings = this.Adapter.GetService(typeof(FoldingManager)) as FoldingManager;
-				if (foldings != null) {
-					var foldingsAtOffset = foldings.GetFoldingsAt(offset);
-					FoldingSection collapsedSection = foldingsAtOffset.FirstOrDefault(section => section.IsFolded);
-					
-					if (collapsedSection != null) {
-						args.SetToolTip(GetTooltipTextForCollapsedSection(collapsedSection));
-					}
-				}
-				
-				TextMarkerService textMarkerService = this.Adapter.GetService(typeof(ITextMarkerService)) as TextMarkerService;
-				if (textMarkerService != null) {
-					var markersAtOffset = textMarkerService.GetMarkersAtOffset(offset);
-					ITextMarker markerWithToolTip = markersAtOffset.FirstOrDefault(marker => marker.ToolTip != null);
-					
-					if (markerWithToolTip != null) {
-						args.SetToolTip(markerWithToolTip.ToolTip);
-					}
-				}
-			}
-			
 			if (!args.Handled) {
 				// if request wasn't handled by a marker, pass it to the ToolTipRequestService
 				ToolTipRequestService.RequestToolTip(args);
@@ -366,70 +342,6 @@ namespace ICSharpCode.AvalonEdit.AddIn
 		{
 			toolTip = null;
 		}
-		
-		#region GetTooltipTextForCollapsedSection
-		string GetTooltipTextForCollapsedSection(FoldingSection foldingSection)
-		{
-			// This fixes SD-1394:
-			// Each line is checked for leading indentation whitespaces. If
-			// a line has the same or more indentation than the first line,
-			// it is reduced. If a line is less indented than the first line
-			// the indentation is removed completely.
-			//
-			// See the following example:
-			// 	line 1
-			// 		line 2
-			// 			line 3
-			//  line 4
-			//
-			// is reduced to:
-			// line 1
-			// 	line 2
-			// 		line 3
-			// line 4
-			
-			const int maxLineCount = 15;
-			
-			TextDocument document = this.Document;
-			int startOffset = foldingSection.StartOffset;
-			int endOffset = foldingSection.EndOffset;
-			
-			DocumentLine startLine = document.GetLineByOffset(startOffset);
-			DocumentLine endLine = document.GetLineByOffset(endOffset);
-			StringBuilder builder = new StringBuilder();
-			
-			DocumentLine current = startLine;
-			ISegment startIndent = TextUtilities.GetLeadingWhitespace(document, startLine);
-			int lineCount = 0;
-			while (current != endLine.NextLine && lineCount < maxLineCount) {
-				ISegment currentIndent = TextUtilities.GetLeadingWhitespace(document, current);
-				
-				if (current == startLine && current == endLine)
-					builder.Append(document.GetText(startOffset, endOffset - startOffset));
-				else if (current == startLine) {
-					if (current.EndOffset - startOffset > 0)
-						builder.AppendLine(document.GetText(startOffset, current.EndOffset - startOffset).TrimStart());
-				} else if (current == endLine) {
-					if (startIndent.Length <= currentIndent.Length)
-						builder.Append(document.GetText(current.Offset + startIndent.Length, endOffset - current.Offset - startIndent.Length));
-					else
-						builder.Append(document.GetText(current.Offset + currentIndent.Length, endOffset - current.Offset - currentIndent.Length));
-				} else {
-					if (startIndent.Length <= currentIndent.Length)
-						builder.AppendLine(document.GetText(current.Offset + startIndent.Length, current.Length - startIndent.Length));
-					else
-						builder.AppendLine(document.GetText(current.Offset + currentIndent.Length, current.Length - currentIndent.Length));
-				}
-				
-				current = current.NextLine;
-				lineCount++;
-			}
-			if (current != endLine.NextLine)
-				builder.Append("...");
-			
-			return builder.ToString();
-		}
-		#endregion
 		#endregion
 		
 		#region Ctrl+Click Go To Definition
