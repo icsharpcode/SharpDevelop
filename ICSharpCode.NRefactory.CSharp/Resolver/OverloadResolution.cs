765d917d6079b8da643a519e1455a02f3cad9f03
Mike Krüger <mkrueger@xamarin.com>
2013-02-07 09:49:59 +0100

    Revert "Revert "Implemented better way to handle extension method invocations.""
    This reverts commit c26f61e8133ce2558d93531bb771ffb0c3bb6ac7.

--- ICSharpCode.NRefactory.CSharp/Resolver/OverloadResolution.cs
+++ ICSharpCode.NRefactory.CSharp/Resolver/OverloadResolution.cs
@@ -950,8 +950,8 @@ namespace ICSharpCode.NRefactory.CSharp.Resolver
 			if (member == null)
 				throw new InvalidOperationException();
 			
-			if (this.IsExtensionMethodInvocation && member is SpecializedMethod)
-				((SpecializedMethod)member).IsExtendedExtensionMethod = true;
+			if (this.IsExtensionMethodInvocation && member is IMethod)
+				member =new InvocatedExtensionMethod ((IMethod)member);
 			return new CSharpInvocationResolveResult(
 				this.IsExtensionMethodInvocation ? new TypeResolveResult(member.DeclaringType) : targetResolveResult,
 				member,
