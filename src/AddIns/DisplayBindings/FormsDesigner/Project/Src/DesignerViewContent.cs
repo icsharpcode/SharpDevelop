ed91c296ff811c830e639aa5f235ef36afec9c75
Daniel Grunwald <daniel@danielgrunwald.de>
2013-03-26 02:10:47 +0100

    Clear property container on designer unload so that the ComboBox of selectable objects stays empty when the designer cannot be reloaded due to syntax errors.

--- src/AddIns/DisplayBindings/FormsDesigner/Project/Src/DesignerViewContent.cs
+++ src/AddIns/DisplayBindings/FormsDesigner/Project/Src/DesignerViewContent.cs
@@ -477,6 +477,7 @@ namespace ICSharpCode.FormsDesigner
 			
 			this.typeResolutionService = null;
 			this.loader = null;
+			UpdatePropertyPad();
 			
 			foreach (KeyValuePair<Type, TypeDescriptionProvider> entry in this.addedTypeDescriptionProviders) {
 				TypeDescriptor.RemoveProvider(entry.Value, entry.Key);
@@ -767,6 +768,9 @@ namespace ICSharpCode.FormsDesigner
 				if (selectionService != null) {
 					UpdatePropertyPadSelection(selectionService);
 				}
+			} else {
+				propertyContainer.Host = null;
+				propertyContainer.SelectableObjects = null;
 			}
 		}
 
