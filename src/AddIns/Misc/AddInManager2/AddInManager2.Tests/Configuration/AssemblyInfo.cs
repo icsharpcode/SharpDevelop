5c06160acf9d7f0eb1f47af4d2fe3dcf4274cf38
Matt Ward <ward.matt@gmail.com>
2013-05-22 23:08:40 +0100

    Fix build.

--- src/AddIns/Misc/AddInManager2/AddInManager2.Tests/Configuration/AssemblyInfo.cs
+++ src/AddIns/Misc/AddInManager2/AddInManager2.Tests/Configuration/AssemblyInfo.cs
@@ -12,3 +12,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0")]
