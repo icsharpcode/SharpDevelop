6dddd4eeefb01e924d61f086ae969bcf437ab45b
Matt Ward <ward.matt@gmail.com>
2013-06-09 15:41:14 +0100

    Add credential provider for NuGet feeds.
    Only supports credentials stored in NuGet.config under
    the user's profile:
    
    nuget sources update -name MyFeed -username user -password pass

---
+++ src/AddIns/Misc/PackageManagement/Project/Src/SharpDevelopCredentialProvider.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) AlphaSierraPapa for the SharpDevelop Team (for details please see \doc\copyright.txt)
+// This code is distributed under the GNU LGPL (for details please see \doc\license.txt)
+
+using System;
+using System.Net;
+using NuGet;
+
+namespace ICSharpCode.PackageManagement
+{
+	public class SharpDevelopCredentialProvider : ICredentialProvider
+	{
+		public ICredentials GetCredentials(Uri uri, IWebProxy proxy, CredentialType credentialType, bool retrying)
+		{
+			return null;
+		}
+	}
+}
+
