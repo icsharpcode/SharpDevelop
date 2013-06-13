072b3e91b02d9568dc88f883e5e08ba0274b4a5e
Daniel Grunwald <daniel@danielgrunwald.de>
2013-06-13 17:46:27 +0200

    Don't show icon in scrollbar for redundancy issues.

--- src/Main/Base/Project/Dom/IModelCollection.cs
+++ src/Main/Base/Project/Dom/IModelCollection.cs
@@ -3,8 +3,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Collections.Specialized;
 using System.Linq;
 
 namespace ICSharpCode.SharpDevelop.Dom
