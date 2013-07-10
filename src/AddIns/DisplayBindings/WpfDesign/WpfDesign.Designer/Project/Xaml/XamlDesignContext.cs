374fa8589b6b85bd24a83cda6704537d7ff94526
Tobias Gummesson <tttobbe@hotmail.com>
2013-07-10 07:14:46 -0700

    Fixed bug where list with errors in the designer was only updated if the document failed to load completely.

--- src/AddIns/DisplayBindings/WpfDesign/WpfDesign.Designer/Project/Xaml/XamlDesignContext.cs
+++ src/AddIns/DisplayBindings/WpfDesign/WpfDesign.Designer/Project/Xaml/XamlDesignContext.cs
@@ -86,8 +86,8 @@ namespace ICSharpCode.WpfDesign.Designer.Xaml
 			parserSettings.CreateInstanceCallback = this.Services.ExtensionManager.CreateInstanceWithCustomInstanceFactory;
 			parserSettings.ServiceProvider = this.Services;
 			_doc = XamlParser.Parse(xamlReader, parserSettings);
-			if(_doc==null)
-				loadSettings.ReportErrors(xamlErrorService);
+			
+			loadSettings.ReportErrors(xamlErrorService);
 			
 			_rootItem = _componentService.RegisterXamlComponentRecursive(_doc.RootElement);
 			
