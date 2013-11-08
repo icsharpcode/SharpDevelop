da5c73a6827888e76b1078ff5de418ed422d10dd
Siegfried Pammer <siegfriedpammer@gmail.com>
2013-11-08 15:56:13 +0100

    implement basic AssemblyDefinition cache

--- src/AddIns/DisplayBindings/ILSpyAddIn/ILSpyFullParseInformation.cs
+++ src/AddIns/DisplayBindings/ILSpyAddIn/ILSpyFullParseInformation.cs
@@ -2,9 +2,12 @@
 // This code is distributed under the GNU LGPL (for details please see \doc\license.txt)
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using ICSharpCode.NRefactory.CSharp;
 using ICSharpCode.NRefactory.Editor;
 using ICSharpCode.SharpDevelop.Parser;
+using Mono.Cecil;
 
 namespace ICSharpCode.ILSpyAddIn
 {
@@ -14,11 +17,14 @@ namespace ICSharpCode.ILSpyAddIn
 	public class ILSpyFullParseInformation : ParseInformation
 	{
 		SyntaxTree syntaxTree;
+		// Required to make WeakReferences work properly as cache policy.
+		AssemblyDefinition[] neededAssemblies;
 
-		public ILSpyFullParseInformation(ILSpyUnresolvedFile unresolvedFile, ITextSourceVersion parsedVersion, SyntaxTree syntaxTree)
+		public ILSpyFullParseInformation(ILSpyUnresolvedFile unresolvedFile, ITextSourceVersion parsedVersion, SyntaxTree syntaxTree, IEnumerable<AssemblyDefinition> neededAssemblies)
 			: base(unresolvedFile, parsedVersion, true)
 		{
 			this.syntaxTree = syntaxTree;
+			this.neededAssemblies = neededAssemblies.ToArray();
 		}
 		
 		public SyntaxTree SyntaxTree { get { return syntaxTree; } }
