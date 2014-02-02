32d1bab1f6a8930a43fa49dd1e2d18a0219f67a1
Matt Ward <ward.matt@gmail.com>
2014-02-02 16:04:38 +0000

    Fix FileCodeModel.CodeElements returning wrong item at index.

--- src/AddIns/Misc/PackageManagement/Project/Src/EnvDTE/CodeElementsList.cs
+++ src/AddIns/Misc/PackageManagement/Project/Src/EnvDTE/CodeElementsList.cs
@@ -24,14 +24,10 @@ using ICSharpCode.SharpDevelop.Dom;
 
 namespace ICSharpCode.PackageManagement.EnvDTE
 {
-	public class CodeElementsList<T> : MarshalByRefObject, global::EnvDTE.CodeElements, IList<T>
+	public class CodeElementsList<T> : MarshalByRefObject, global::EnvDTE.CodeElements
 		where T : global::EnvDTE.CodeElement
 	{
-		List<T> elements = new List<T>();
-		
-		public CodeElementsList()
-		{
-		}
+		readonly List<T> elements = new List<T>();
 		
 		public int Count {
 			get { return elements.Count; }
@@ -60,58 +56,16 @@ namespace ICSharpCode.PackageManagement.EnvDTE
 			return elements.Single(item => item.Name == name);
 		}
 		
-		public T this[int index] {
-			get { return elements[index]; }
-			set { elements[index] = value; }
-		}
-		
-		bool ICollection<T>.IsReadOnly {
-			get { return false; }
-		}
-		
-		public int IndexOf(T item)
+		internal void Add(T element)
 		{
-			return elements.IndexOf(item);
+			elements.Add(element);
 		}
 		
-		public void Insert(int index, T item)
+		internal void AddRange(IEnumerable<T> items)
 		{
-			elements.Insert(index, item);
-		}
-		
-		public void RemoveAt(int index)
-		{
-			elements.RemoveAt(index);
-		}
-		
-		public void Add(T item)
-		{
-			elements.Add(item);
-		}
-		
-		public void Clear()
-		{
-			elements.Clear();
-		}
-		
-		public bool Contains(T item)
-		{
-			return elements.Contains(item);
-		}
-		
-		public void CopyTo(T[] array, int arrayIndex)
-		{
-			elements.CopyTo(array, arrayIndex);
-		}
-		
-		public bool Remove(T item)
-		{
-			return elements.Remove(item);
-		}
-		
-		IEnumerator<T> IEnumerable<T>.GetEnumerator()
-		{
-			return elements.GetEnumerator();
+			foreach (T element in items) {
+				Add(element);
+			}
 		}
 	}
 }
