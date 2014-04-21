df292ae92008b7cd2aa246367be9e668867279dc
Daniel Grunwald <daniel@danielgrunwald.de>
2014-04-21 19:02:15 +0200

    When loading a portable library project and the portable library tools are not installed, display an error message directing the user to the download.

--- src/Main/Base/Project/Project/PortableLibrary/CheckPortableLibraryInstalled.cs
+++ src/Main/Base/Project/Project/PortableLibrary/CheckPortableLibraryInstalled.cs
@@ -17,9 +17,6 @@
 // DEALINGS IN THE SOFTWARE.
 
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
 using ICSharpCode.Core;
 using ICSharpCode.SharpDevelop.Gui;
 
@@ -30,15 +27,22 @@ namespace ICSharpCode.SharpDevelop.Project.PortableLibrary
 	/// </summary>
 	public class CheckPortableLibraryInstalled : AbstractCommand
 	{
+		public static string CouldNotFindToolsDescription {
+			get {
+				return StringParser.Parse(
+					"${res:PortableLibrary.CouldNotFindTools}" + Environment.NewLine + Environment.NewLine
+					+ "${res:PortableLibrary.ToolsInstallationHelp}");
+			}
+		}
+		
+		public static string DownloadUrl {
+			get { return "http://go.microsoft.com/fwlink/?LinkId=210823"; }
+		}
+		
 		public override void Run()
 		{
 			if (!ProfileList.IsPortableLibraryInstalled()) {
-				using (ToolNotFoundDialog dlg = new ToolNotFoundDialog(
-					StringParser.Parse(
-						"${res:PortableLibrary.CouldNotFindTools}" + Environment.NewLine + Environment.NewLine +
-						"${res:PortableLibrary.ToolsInstallationHelp}"),
-					"http://go.microsoft.com/fwlink/?LinkId=210823"
-				)) {
+				using (ToolNotFoundDialog dlg = new ToolNotFoundDialog(CouldNotFindToolsDescription, DownloadUrl)) {
 					// our message is long, so make the window bigger than usual
 					dlg.Width += 70;
 					dlg.Height += 70;
