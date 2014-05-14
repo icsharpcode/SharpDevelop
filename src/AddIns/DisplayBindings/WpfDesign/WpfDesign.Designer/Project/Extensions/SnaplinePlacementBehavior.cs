c825585043caa94fe741a7970728c160fa48fed1
tbulle <tbulle@server.fake>
2014-05-14 14:56:47 +0200

    Simplification of code

--- src/AddIns/DisplayBindings/WpfDesign/WpfDesign.Designer/Project/Extensions/SnaplinePlacementBehavior.cs
+++ src/AddIns/DisplayBindings/WpfDesign/WpfDesign.Designer/Project/Extensions/SnaplinePlacementBehavior.cs
@@ -116,11 +116,9 @@ namespace ICSharpCode.WpfDesign.Designer.Extensions
 				
 				if (operation.Type == PlacementType.Resize) {
 					if (info.ResizeThumbAlignment != null && info.ResizeThumbAlignment.Value.Vertical == VerticalAlignment.Top) {
-						bounds.Y += delta;
-						bounds.Height = Math.Max(0, bounds.Height - delta);
-					} else {
-						bounds.Height = Math.Max(0, bounds.Height + delta);
+						bounds.Y += delta;	
 					}
+					bounds.Height = Math.Max(0, bounds.Height - delta);
 					info.Bounds = bounds;
 				} else {
 					foreach (var item in operation.PlacedItems) {
@@ -139,11 +137,9 @@ namespace ICSharpCode.WpfDesign.Designer.Extensions
 				
 				if (operation.Type == PlacementType.Resize) {
 					if (info.ResizeThumbAlignment != null && info.ResizeThumbAlignment.Value.Horizontal == HorizontalAlignment.Left) {
-						bounds.X += delta;
-						bounds.Width = Math.Max(0, bounds.Width - delta);
-					} else {
-						bounds.Width = Math.Max(0, bounds.Width + delta);
+						bounds.X += delta;	
 					}
+					bounds.Width = Math.Max(0, bounds.Width - delta);
 					info.Bounds = bounds;
 				} else {
 					foreach (var item in operation.PlacedItems) {
