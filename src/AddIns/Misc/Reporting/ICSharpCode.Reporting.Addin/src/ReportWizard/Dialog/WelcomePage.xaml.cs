d4ff35ebbd5cc0dc5d804b1af20a6478c985f3c2
Peter Forstmeier <peter.forstmeier@t-online.de>
2014-08-15 12:11:57 +0200

    Missing

---
+++ src/AddIns/Misc/Reporting/ICSharpCode.Reporting.Addin/src/ReportWizard/Dialog/WelcomePage.xaml.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Peter Forstmeier
+ * Date: 05.08.2014
+ * Time: 20:18
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using Xceed.Wpf.Toolkit;
+
+namespace ICSharpCode.Reporting.Addin.ReportWizard.Dialog
+{
+	/// <summary>
+	/// Interaction logic for WelcomePage.xaml
+	/// </summary>
+	public partial class WelcomePage : WizardPage
+	{
+		public WelcomePage()
+		{
+			InitializeComponent();
+			_image.Source = WizardHelper.GetWizardIcon();
+		}
+	}
+}
