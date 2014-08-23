40e5ec6116300cc42d5ebbce9f28532a8787b416
Daniel Grunwald <daniel@danielgrunwald.de>
2014-08-23 16:10:32 +0200

    Fix #518 by re-using the insight window.

--- src/Libraries/AvalonEdit/ICSharpCode.AvalonEdit/CodeCompletion/OverloadInsightWindow.cs
+++ src/Libraries/AvalonEdit/ICSharpCode.AvalonEdit/CodeCompletion/OverloadInsightWindow.cs
@@ -52,7 +52,7 @@ namespace ICSharpCode.AvalonEdit.CodeCompletion
 		protected override void OnKeyDown(KeyEventArgs e)
 		{
 			base.OnKeyDown(e);
-			if (!e.Handled && this.Provider.Count > 1) {
+			if (!e.Handled && this.Provider != null && this.Provider.Count > 1) {
 				switch (e.Key) {
 					case Key.Up:
 						e.Handled = true;
