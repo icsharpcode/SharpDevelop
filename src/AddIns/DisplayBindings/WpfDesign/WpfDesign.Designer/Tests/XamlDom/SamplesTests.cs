669f892c14872e0e8d8c0fb248fce1ccabc2ee7e
jogibear9988 <jochen.kuehner@gmx.de>
2014-10-29 23:51:11 +0100

    more unit tests

--- src/AddIns/DisplayBindings/WpfDesign/WpfDesign.Designer/Tests/XamlDom/SamplesTests.cs
+++ src/AddIns/DisplayBindings/WpfDesign/WpfDesign.Designer/Tests/XamlDom/SamplesTests.cs
@@ -243,8 +243,83 @@ namespace ICSharpCode.WpfDesign.Tests.XamlDom
 </Canvas>
 </Window>");
 		}
-		
-		
-		
-	}
+
+        [Test]
+        public void Children1()
+        {
+            TestLoading(@"<Window
+    xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
+    xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml"">
+<Canvas>
+<Button />
+</Canvas>
+</Window>");
+        }
+
+	    [Test]
+	    public void Children2()
+	    {
+	        TestLoading(@"<Window
+    xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
+    xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml"">
+<Canvas>
+<Canvas.Children>
+<Button />
+</Canvas.Children>
+</Canvas>
+</Window>");
+	    }
+
+	    [Test]
+        public void Children3()
+        {
+            TestLoading(@"<Window
+    xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
+    xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml"">
+<Canvas>
+<Canvas.Children>
+<Button />
+</Canvas.Children>
+</Canvas>
+</Window>");
+        }
+
+        [Test]
+        public void ListBox1()
+        {
+            TestLoading(@"<Window
+    xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
+    xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml"">
+<Canvas>
+<ListBox>
+aa
+bb
+</ListBox>
+</Canvas>
+</Window>");
+        }
+
+
+        [Test]
+        public void CData1()
+        {
+            TestLoading(@"<Window
+    xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
+    xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml"">
+<Canvas>
+<Button>Press</Button>
+<x:Code>
+            <![CDATA[
+
+            public void ButtonOnClick(object sender, RoutedEventArgs args)
+            {
+    Button btn = sender as Button;
+    MessageBox.Show(""Button clicked"", ""Hello"");
+}
+            ]]>
+        </x:Code>
+ </Canvas>
+</Window>");
+        }
+    }
 }
