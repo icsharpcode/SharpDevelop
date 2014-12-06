11a96d9de5305949e1f68934e948146583a0f206
olegbevz <olegdb@inbox.ru>
2014-12-06 13:04:36 +0700

    add assembly info options panel

---
+++ src/Main/Base/Project/Src/Gui/Dialogs/OptionPanels/ProjectOptions/AssemblyInfo.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ICSharpCode.SharpDevelop.Gui.OptionPanels;
+
+namespace ICSharpCode.SharpDevelop.Gui.OptionPanels
+{
+	/// <summary>
+	/// Interaction logic for AssemblyInfo.xaml
+	/// </summary>
+	public partial class AssemblyInfo : ProjectOptionPanel
+	{
+		public AssemblyInfo()
+		{
+			InitializeComponent();
+		}
+	}
+}
