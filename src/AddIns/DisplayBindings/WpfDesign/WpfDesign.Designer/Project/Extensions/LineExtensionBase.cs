5ffc7056ae372111cad80f19fa7f6e03d344b485
tbulle <tbulle@server.fake>
2014-12-22 11:59:18 +0100

    Enable multiple Extension override. Lien and Polyline handling, needs controls creation

---
+++ src/AddIns/DisplayBindings/WpfDesign/WpfDesign.Designer/Project/Extensions/LineExtensionBase.cs
@@ -0,0 +1,190 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: trubra
+ * Date: 2014-12-22
+ * Time: 11:00
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using ICSharpCode.WpfDesign.Designer.Controls;
+using ICSharpCode.WpfDesign.Designer.Extensions;
+using System.Windows.Shapes;
+
+using ICSharpCode.WpfDesign;
+using ICSharpCode.WpfDesign.Adorners;
+using System.Windows;
+using System.Windows.Controls;
+using System;
+using System.Collections;
+using System.ComponentModel;
+
+namespace ICSharpCode.WpfDesign.Designer.Extensions
+{
+	/// <summary>
+	/// Description of LineExtensionBase.
+	/// </summary>
+	class Bounds
+    {
+        public double X, Y, Left, Top;
+    }
+    /// <summary>
+    /// base class for the Line, Polyline and Polygon extension classes 
+    /// </summary>
+    public class LineExtensionBase : SelectionAdornerProvider
+    {
+        
+
+        internal AdornerPanel adornerPanel;
+        internal IEnumerable resizeThumbs;
+
+        /// <summary>An array containing this.ExtendedItem as only element</summary>
+        internal readonly DesignItem[] extendedItemArray = new DesignItem[1];
+
+        internal IPlacementBehavior resizeBehavior;
+        internal PlacementOperation operation;
+        internal ChangeGroup changeGroup;
+        private Canvas _surface;
+        internal bool _isResizing;
+        private TextBlock _text;
+        //private DesignPanel designPanel;
+
+        /// <summary>
+        /// Gets whether this extension is resizing any element.
+        /// </summary>
+        public bool IsResizing
+        {
+            get { return _isResizing; }
+        }
+
+        /// <summary>
+        /// on creation add adornerlayer 
+        /// </summary>
+        public LineExtensionBase()
+        {
+            _surface = new Canvas();
+            adornerPanel = new AdornerPanel();
+            adornerPanel.Order = AdornerOrder.Foreground;
+            adornerPanel.Children.Add(_surface);
+            Adorners.Add(adornerPanel);
+        }
+
+        #region eventhandlers
+
+
+        protected void OnPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            UpdateAdornerVisibility();
+        }
+
+        protected override void OnRemove()
+        {
+            this.ExtendedItem.PropertyChanged -= OnPropertyChanged;
+            this.Services.Selection.PrimarySelectionChanged -= OnPrimarySelectionChanged;
+            DependencyPropertyDescriptor.FromProperty(FrameworkElement.HeightProperty, typeof (Shape))
+                .RemoveValueChanged(ExtendedItem.View, (s, ev) => ResetWidthHeightProperties());
+            base.OnRemove();
+        }
+
+        protected void OnPrimarySelectionChanged(object sender, EventArgs e)
+        {
+            bool isPrimarySelection = this.Services.Selection.PrimarySelection == this.ExtendedItem;
+            if (isPrimarySelection)
+                DependencyPropertyDescriptor.FromProperty(FrameworkElement.HeightProperty, typeof (Shape))
+                    .AddValueChanged(ExtendedItem.View, (s, ev) => ResetWidthHeightProperties());
+            //foreach (ResizeThumb g in adornerPanel.Children)
+            //{
+            //    g.IsPrimarySelection = isPrimarySelection;
+            //}
+        }
+
+        #endregion
+
+        protected void UpdateAdornerVisibility()
+        {
+            FrameworkElement fe = this.ExtendedItem.View as FrameworkElement;
+            foreach (ResizeThumb r in resizeThumbs)
+            {
+                bool isVisible = resizeBehavior != null &&
+                                 resizeBehavior.CanPlace(extendedItemArray, PlacementType.Resize, r.Alignment);
+                r.Visibility = isVisible ? Visibility.Visible : Visibility.Hidden;
+            }
+        }
+
+        /// <summary>
+        /// Places resize thumbs at their respective positions
+        /// and streches out thumbs which are at the center of outline to extend resizability across the whole outline
+        /// </summary>
+        /// <param name="resizeThumb"></param>
+        /// <param name="alignment"></param>
+        /// <param name="index">if using a polygon or multipoint adorner this is the index of the point in the Points array</param>
+        /// <returns></returns>
+        protected PointTrackerPlacementSupport Place(ref ResizeThumb resizeThumb, PlacementAlignment alignment, int index = -1)
+        {
+            PointTrackerPlacementSupport placement = new PointTrackerPlacementSupport(ExtendedItem.View as Shape, alignment, index);
+            return placement;
+        }
+
+        /// <summary>
+        /// forces redraw of shape
+        /// </summary>
+        protected void Invalidate()
+        {
+            Shape s = ExtendedItem.View as Shape;
+            if (s != null)
+            {
+                s.InvalidateVisual();
+                s.BringIntoView();
+            }
+        }
+
+        protected void SetSurfaceInfo(int x, int y, string s)
+        {
+            if (_text == null)
+            {
+                _text = new TextBlock();
+                _surface.Children.Add(_text);
+            }
+            AdornerPanel ap = _surface.Parent as AdornerPanel;
+
+            _surface.Width = ap.Width;
+            _surface.Height = ap.Height;
+
+            _text.Text = s;
+            Canvas.SetLeft(_text, x);
+            Canvas.SetTop(_text, y);
+
+        }
+
+        protected void HideSizeAndShowHandles()
+        {
+            SizeDisplayExtension sizeDisplay = null;
+            MarginHandleExtension marginDisplay = null;
+            foreach (var extension in ExtendedItem.Extensions)
+            {
+                if (extension is SizeDisplayExtension)
+                    sizeDisplay = extension as SizeDisplayExtension;
+                if (extension is MarginHandleExtension)
+                    marginDisplay = extension as MarginHandleExtension;
+            }
+
+            if (sizeDisplay != null)
+            {
+                sizeDisplay.HeightDisplay.Visibility = Visibility.Hidden;
+                sizeDisplay.WidthDisplay.Visibility = Visibility.Hidden;
+            }
+            if (marginDisplay != null)
+            {
+                marginDisplay.ShowHandles();
+            }
+        }
+
+        protected void ResetWidthHeightProperties()
+        {
+            ExtendedItem.Properties.GetProperty(FrameworkElement.HeightProperty).Reset();
+            ExtendedItem.Properties.GetProperty(FrameworkElement.WidthProperty).Reset();
+        }
+
+       
+    }
+}
---
+++ src/AddIns/DisplayBindings/WpfDesign/WpfDesign.Designer/Project/Extensions/PointTrackerPlacementSupport.cs
@@ -0,0 +1,93 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: trubra
+ * Date: 2014-12-22
+ * Time: 11:05
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Windows;
+using System.Windows.Shapes;
+using ICSharpCode.WpfDesign;
+using ICSharpCode.WpfDesign.Adorners;
+
+namespace ICSharpCode.WpfDesign.Designer.Extensions
+{
+	public class PointTrackerPlacementSupport : AdornerPlacement
+    {
+        private Shape shape;
+        private PlacementAlignment alignment;
+
+        public int Index
+        {
+            get; set;
+        }
+        public PointTrackerPlacementSupport(Shape s, PlacementAlignment align, int index)
+        {
+            shape = s;
+            alignment = align;
+            Index = index;
+        }
+
+
+
+        public PointTrackerPlacementSupport(Shape s, PlacementAlignment align)
+        {
+            shape = s;
+            alignment = align;
+            Index = -1;
+        }
+
+        /// <summary>
+        /// Arranges the adorner element on the specified adorner panel.
+        /// </summary>
+        public override void Arrange(AdornerPanel panel, UIElement adorner, Size adornedElementSize)
+        {
+            Point p = new Point(0, 0);
+            double thumbsize = 7;
+            double distance = thumbsize / 2;
+            if (shape as Line != null)
+            {
+                Line s = shape as Line;
+                double x, y;
+                //will give you the angle of the line if more than 180 degrees it becomes negative from 
+                Double theta = (180 / Math.PI) * Math.Atan2(s.Y2 - s.Y1, s.X2 - s.X1);
+
+                //this will give you the x offset from the line x point in parts of half the size of the thumb
+                double dx = Math.Cos(theta * (Math.PI / 180)) * distance;
+
+                //this will give you the y offset from the line y point in parts of half the size of the thumb
+                double dy = Math.Sin(theta * (Math.PI / 180)) * distance;
+                
+                if (alignment == PlacementAlignment.BottomRight)
+                {
+                    //x offset is linear 
+                    x = s.X2 - Math.Abs(theta) / (180 / thumbsize) + dx;
+                    //y offset is angular
+                    y = s.Y2 - ((.5 - Math.Sin(theta * (Math.PI / 180)) * .5) * thumbsize) + dy;
+                }
+                else
+                {
+                    x = s.X1 - ((180 - Math.Abs(theta)) / (180 / thumbsize)) - dx;
+                    y = s.Y1 - ((.5 - Math.Sin(theta * (Math.PI / 180) + Math.PI) * .5) * thumbsize) - dy;
+                }
+                p = new Point(x, y);
+
+            }
+            Polygon pg = shape as Polygon;
+            Polyline pl = shape as Polyline;
+            if (pg != null || pl != null)
+            {
+                if (Index > 0)
+                {
+                    p = pl != null ? pl.Points[Index] : pg.Points[Index];
+
+                }
+            }
+            adorner.Arrange(new Rect(p, new Size(thumbsize, thumbsize)));
+        }
+
+
+    }
+}
