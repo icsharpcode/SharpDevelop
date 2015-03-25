786f60ae9b564562932c642ce2c3627a6e063d7f
gumme <gumme@users.noreply.github.com>
2015-03-25 10:26:04 +0100

    Added comments where missing on public types.

--- src/AddIns/DisplayBindings/WpfDesign/WpfDesign.XamlDom/Project/DesignInstanceExtension.cs
+++ src/AddIns/DisplayBindings/WpfDesign/WpfDesign.XamlDom/Project/DesignInstanceExtension.cs
@@ -7,21 +7,36 @@ using System.Windows.Markup;
 namespace ICSharpCode.WpfDesign.XamlDom
 {
 	/// <summary>
-	/// A class wich Implementes the DesignInstanceExtension normaly defined in the Blend Namespace
+	/// A class which implements the DesignInstanceExtension normally defined in the Blend Namespace.
 	/// </summary>
 	public class DesignInstanceExtension : MarkupExtension
 	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="DesignInstanceExtension"/> class.
+		/// </summary>
+		/// <param name="type">The type to create.</param>
 		public DesignInstanceExtension(Type type)
 		{
 			this.Type = type;
 		}
 
+		/// <summary>
+		/// Gets or sets the type to create.
+		/// Use <see cref="IsDesignTimeCreatable"/> to specify whether an instance of your type or a designer-generated substitute type is created.
+		/// </summary>
 		public Type Type { get; set; }
 
+		/// <summary>
+		/// Gets or sets if the design instance is created from your type (true) or of a designer-generated substitute type (false). 
+		/// </summary>
 		public bool IsDesignTimeCreatable { get; set; }
 
+		/// <summary>
+		/// Gets or sets if the design instance is a list of the specified type. 
+		/// </summary>
 		public bool CreateList { get; set; }
 
+		/// <inheritdoc/>
 		public override object ProvideValue(IServiceProvider serviceProvider)
 		{
 			return null;
